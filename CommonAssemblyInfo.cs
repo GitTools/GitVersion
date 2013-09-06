b163475f2932f83921b45f1270927b08448ff775
Simon Cropp <simon.cropp@gmail.com>
2013-09-06 10:09:43 +1000

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("GitFlowVersion")]
 [assembly: AssemblyProduct("GitFlowVersion")]
-[assembly: AssemblyVersion("0.1.8")]
-[assembly: AssemblyFileVersion("0.1.8")]
+[assembly: AssemblyVersion("0.1.9")]
+[assembly: AssemblyFileVersion("0.1.9")]
