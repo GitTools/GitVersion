37f7e2a0ef1b153f356bb8ff1ed5c30e915237dd
Simon Cropp <simon.cropp@gmail.com>
2013-09-11 11:42:39 +1000

    update gitflow, better unit tests

--- Tests/FeatureBranchTests.cs
+++ Tests/FeatureBranchTests.cs
@@ -9,7 +9,7 @@ public class FeatureBranchTests
     {
         //this scenario should redirect to the develop finder since there is no diff btw this
         // branch and the develop branch
-        var version = FinderWrapper.FindVersionForCommit("c50179a2c77843245ace262b51b08af7b3b7f8fe", "a_pull_request");
+        var version = FinderWrapper.FindVersionForCommit("c50179a2c77843245ace262b51b08af7b3b7f8fe", "featureWithNoCommits");
         Assert.AreEqual(0, version.Major);
         Assert.AreEqual(3, version.Minor, "Minor should be master.Minor+1");
         Assert.AreEqual(0, version.Patch);
@@ -20,24 +20,35 @@ public class FeatureBranchTests
     [Test]
     public void Feature_branch_with_1_commit()
     {
-        var version = FinderWrapper.FindVersionForCommit("f1c44cb2a8fe960b6638c487cb6d70092fdaa432", "a_pull_request");
+        var version = FinderWrapper.FindVersionForCommit("c33e4999a623340f028e7a55b898ea911736eb88", "featureWithOneCommit");
         Assert.AreEqual(0, version.Major);
         Assert.AreEqual(3, version.Minor, "Minor should be master.Minor+1");
         Assert.AreEqual(0, version.Patch);
         Assert.AreEqual(Stage.Feature, version.Stage);
-        Assert.AreEqual("c50179a2", version.Suffix);
+        Assert.AreEqual("c50179a2", version.Suffix, "Suffix should be the develop commit it was branched from");
         Assert.AreEqual(0, version.PreRelease, "Prerelease is always 0 for feature branches");
     }
 
     [Test]
     public void Feature_branch_with_2_commits()
     {
-        var version = FinderWrapper.FindVersionForCommit("72cbf7b589e3edf3b0e781624471446c2a49037e", "a_pull_request");
+        var version = FinderWrapper.FindVersionForCommit("9fc0dee434901e882db8e9b997d966e19880e164", "featureWithTwoCommits");
         Assert.AreEqual(0, version.Major);
         Assert.AreEqual(3, version.Minor, "Minor should be master.Minor+1");
         Assert.AreEqual(0, version.Patch);
         Assert.AreEqual(Stage.Feature, version.Stage);
-        Assert.AreEqual("c50179a2", version.Suffix);
+        Assert.AreEqual("c50179a2", version.Suffix, "Suffix should be the develop commit it was branched from");
+        Assert.AreEqual(0, version.PreRelease, "Prerelease is always 0 for feature branches");
+    }
+    [Test]
+    public void Feature_branch_with_2_commits_but_building_an_commit()
+    {
+        var version = FinderWrapper.FindVersionForCommit("22abb6aab037a3cb969c3a7642607748dc94f3cb", "featureWithTwoCommits");
+        Assert.AreEqual(0, version.Major);
+        Assert.AreEqual(3, version.Minor, "Minor should be master.Minor+1");
+        Assert.AreEqual(0, version.Patch);
+        Assert.AreEqual(Stage.Feature, version.Stage);
+        Assert.AreEqual("c50179a2", version.Suffix, "Suffix should be the develop commit it was branched from");
         Assert.AreEqual(0, version.PreRelease, "Prerelease is always 0 for feature branches");
     }
 }
