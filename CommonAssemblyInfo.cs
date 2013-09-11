598feba6180e3bfe91d2f74c4ad112ed8dc21423
Simon Cropp <simon.cropp@gmail.com>
2013-09-11 12:18:03 +1000

    version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("GitFlowVersion")]
 [assembly: AssemblyProduct("GitFlowVersion")]
-[assembly: AssemblyVersion("0.1.15")]
-[assembly: AssemblyFileVersion("0.1.15")]
+[assembly: AssemblyVersion("0.1.16")]
+[assembly: AssemblyFileVersion("0.1.16")]
