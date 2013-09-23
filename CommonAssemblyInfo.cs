4db5fc1cf53acd11537dde0dd3cec27062a7e39e
Andreas Öhlund <andreasohlund2@gmail.com>
2013-09-23 13:28:55 +0200

    Bumped to 0.1.23

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("GitFlowVersion")]
 [assembly: AssemblyProduct("GitFlowVersion")]
-[assembly: AssemblyVersion("0.1.22")]
-[assembly: AssemblyFileVersion("0.1.22")]
+[assembly: AssemblyVersion("0.1.23")]
+[assembly: AssemblyFileVersion("0.1.23")]
 [assembly: InternalsVisibleTo("Tests")]
