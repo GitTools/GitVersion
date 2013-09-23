557d4baadc09cf89a094c74520bfe20f35fd5ba5
Andreas Öhlund <andreasohlund2@gmail.com>
2013-09-23 10:24:28 +0200

    Fixed a stupid mistake where the TC output was missing a hashtag

--- GitFlowVersion/TeamCity.cs
+++ GitFlowVersion/TeamCity.cs
@@ -80,6 +80,6 @@ public class TeamCity
 
     static string GenerateBuildParameter(string name, string value)
     {
-        return string.Format("#teamcity[setParameter name='GitFlowVersion.{0}' value='{1}']",name,value);
+        return string.Format("##teamcity[setParameter name='GitFlowVersion.{0}' value='{1}']",name,value);
     }
 }
