777b5f5ac2977bf8b7a88a3cbe96be3d14668e57
Simon Cropp <simon.cropp@gmail.com>
2013-09-27 14:09:20 +1000

    remove fody version
    much faster as a prebuild task

--- GitFlowVersionFody/CecilExtensions.cs
+++
@@ -1,9 +0,0 @@
-using Mono.Cecil;
-
-public static class CecilExtensions
-{
-    public static bool IsStrongNamed(this ModuleDefinition moduleDefinition)
-    {
-        return !moduleDefinition.Assembly.Name.PublicKey.IsEmpty();
-    }
-}
