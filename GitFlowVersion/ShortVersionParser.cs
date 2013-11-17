55948a465f32ffc4b1eddc6fb24c27568fd4ddce
nulltoken <emeric.fermas@gmail.com>
2013-11-17 12:11:21 +0100

    Refactor ShortVersionParser.TryParseMajorMinor()

--- GitFlowVersion/ShortVersionParser.cs
+++ GitFlowVersion/ShortVersionParser.cs
@@ -28,33 +28,19 @@ namespace GitFlowVersion
             }
 
         }
+
         public static bool TryParseMajorMinor(string versionString, out int major, out int minor)
         {
-            major = 0;
-            minor = 0;
             int patch;
-            var strings = versionString.Split('.');
-            if (strings.Length != 3)
-            {
-                return false;
-            }
-            if (!int.TryParse(strings[0], out major))
-            {
-                return false;
-            }
 
-            if (!int.TryParse(strings[1], out minor))
-            {
-                return false;
-            }
-
-            if (!int.TryParse(strings[2], out patch))
+            if (!TryParse(versionString, out major, out minor, out patch))
             {
                 return false;
             }
 
             return patch == 0;
         }
+
         public static bool TryParse(string versionString, out int major, out int minor, out int patch)
         {
             major = 0;
