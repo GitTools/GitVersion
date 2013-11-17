e67b56be72428cace9db4766e2b16ad828c9f8b2
nulltoken <emeric.fermas@gmail.com>
2013-11-17 12:04:57 +0100

    Improve ShortVersionParser.TryParseMajorMinor() test coverage

--- Tests/ShortVersionParserTests.cs
+++ Tests/ShortVersionParserTests.cs
@@ -29,4 +29,22 @@ public class ShortVersionParserTests
         Assert.AreEqual(2, minor);
         Assert.AreEqual(3, patch);
     }
+
+    [Test]
+    public void Major_minorTry()
+    {
+        int minor;
+        int major;
+        var result = ShortVersionParser.TryParseMajorMinor("1.2.3", out major, out minor);
+        Assert.IsFalse(result);
+
+        result = ShortVersionParser.TryParseMajorMinor("1.2", out major, out minor);
+        Assert.IsFalse(result);
+
+        result = ShortVersionParser.TryParseMajorMinor("1.2.0", out major, out minor);
+        Assert.IsTrue(result);
+
+        Assert.AreEqual(1, major);
+        Assert.AreEqual(2, minor);
+    }
 }
