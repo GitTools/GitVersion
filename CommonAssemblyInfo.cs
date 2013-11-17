282b13df76d830c3ffa4f2b18f0497e7885075ad
Simon Cropp <simon.cropp@gmail.com>
2013-11-17 22:40:04 +1100

    version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("GitFlowVersion")]
 [assembly: AssemblyProduct("GitFlowVersion")]
-[assembly: AssemblyVersion("0.10.2")]
-[assembly: AssemblyFileVersion("0.10.2")]
+[assembly: AssemblyVersion("0.10.3")]
+[assembly: AssemblyFileVersion("0.10.3")]
 [assembly: InternalsVisibleTo("Tests")]
