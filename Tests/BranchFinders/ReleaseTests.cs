1e392b9e9071bb492610a05f46d0c73c8682f197
nulltoken <emeric.fermas@gmail.com>
2013-12-06 17:10:38 +0100

    Make ReleaseTests run against a concrete test repository

--- Tests/BranchFinders/ReleaseTests.cs
+++ Tests/BranchFinders/ReleaseTests.cs
@@ -1,219 +1,154 @@
-using FluentDate;
-using FluentDateTimeOffset;
 using GitFlowVersion;
+using LibGit2Sharp;
 using NUnit.Framework;
+using Tests.Helpers;
 
 [TestFixture]
-public class ReleaseTests
+public class ReleaseTests : Lg2sHelperBase
 {
     [Test]
     public void No_commits()
     {
-        var branchingCommit = new MockCommit
-                              {
-                                  MessageEx = "branching commit",
-                              CommitterEx = 1.Seconds().Ago().ToSignature()
-                              };
-        var releaseBranch = new MockBranch("release-0.3.0")
-                            {
-                                branchingCommit,
-                            };
-        var finder = new ReleaseVersionFinder
-                     {
-                         ReleaseBranch = releaseBranch,
-                         Commit = branchingCommit,
-                         Repository = new MockRepository
-                                      {
-                                          Branches = new MockBranchCollection
-                                                     {
-                                                         new MockBranch("develop")
-                                                         {
-                                                             branchingCommit
-                                                         },
-                                                         releaseBranch
-                                                     },
-                                          Tags = new MockTagCollection
-                                                 {
-                                                     new MockTag
-                                                     {
-                                                         TargetEx = branchingCommit,
-                                                         NameEx = "0.3.0-alpha5"
-                                                     }
-                                                 }
-                                      },
-                     };
-        var version = finder.FindVersion();
-        Assert.AreEqual(0, version.Version.Major);
-        Assert.AreEqual(3, version.Version.Minor);
-        Assert.AreEqual(0, version.Version.Patch);
-        Assert.AreEqual(Stability.Alpha, version.Version.Stability);
-        Assert.AreEqual(BranchType.Release, version.BranchType);
-        Assert.AreEqual(5, version.Version.PreReleasePartOne, "PreReleasePartOne should be set to 5 from the tag");
-        Assert.IsNull(version.Version.PreReleasePartTwo, "PreReleasePartTwo null since there is no commits");
+        var repoPath = Clone(ASBMTestRepoWorkingDirPath);
+        using (var repo = new Repository(repoPath))
+        {
+            const string branchName = "release-0.3.0";
+
+            var branchingCommit = repo.Branches["develop"].Tip;
+            var releaseBranch = repo.Branches.Add(branchName, branchingCommit);
+
+            var sign = Constants.SignatureNow();
+            repo.Tags.Add("0.3.0-alpha5", branchingCommit, sign, "release");
+
+            var finder = new ReleaseVersionFinder
+            {
+                Repository = repo,
+                Commit = branchingCommit,
+                ReleaseBranch = releaseBranch,
+            };
+
+            var version = finder.FindVersion();
+            Assert.AreEqual(0, version.Version.Major);
+            Assert.AreEqual(3, version.Version.Minor);
+            Assert.AreEqual(0, version.Version.Patch);
+            Assert.AreEqual(Stability.Alpha, version.Version.Stability);
+            Assert.AreEqual(BranchType.Release, version.BranchType);
+            Assert.AreEqual(5, version.Version.PreReleasePartOne, "PreReleasePartOne should be set to 5 from the tag");
+            Assert.IsNull(version.Version.PreReleasePartTwo, "PreReleasePartTwo null since there is no commits");
+        }
     }
 
     [Test]
     public void First_commit()
     {
-        var branchingCommit = new MockCommit
-                              {
-                                  MessageEx = "branching commit",
-                                  CommitterEx = 2.Seconds().Ago().ToSignature()
-                              };
-        var firstCommit = new MockCommit
-                          {
-                              MessageEx = "first commit on release",
-                              CommitterEx = 1.Seconds().Ago().ToSignature()
-                          };
-        var releaseBranch = new MockBranch("release-0.5.0")
-                            {
-                                firstCommit,
-                                branchingCommit,
-                            };
-        var finder = new ReleaseVersionFinder
-                     {
-                         ReleaseBranch = releaseBranch,
-                         Commit = firstCommit,
-                         Repository = new MockRepository
-                                      {
-                                          Branches = new MockBranchCollection
-                                                     {
-                                                         new MockBranch("develop")
-                                                         {
-                                                             branchingCommit
-                                                         },
-                                                         releaseBranch
-                                                     },
-                                          Tags = new MockTagCollection
-                                                 {
-                                                     new MockTag
-                                                     {
-                                                         TargetEx = branchingCommit,
-                                                         NameEx = "0.5.0-alpha5"
-                                                     }
-                                                 }
-                                      },
-                     };
-        var version = finder.FindVersion();
-        Assert.AreEqual(0, version.Version.Major);
-        Assert.AreEqual(5, version.Version.Minor);
-        Assert.AreEqual(0, version.Version.Patch);
-        Assert.AreEqual(Stability.Alpha, version.Version.Stability);
-        Assert.AreEqual(BranchType.Release, version.BranchType);
-        Assert.AreEqual(5, version.Version.PreReleasePartOne, "PreReleasePartOne should be set to 5 from the tag");
-        Assert.AreEqual(1, version.Version.PreReleasePartTwo, "PreReleasePartTwo should be set to 1 since there is 1 commit");
+        var repoPath = Clone(ASBMTestRepoWorkingDirPath);
+        using (var repo = new Repository(repoPath))
+        {
+            const string branchName = "release-0.5.0";
+
+            var branchingCommit = repo.Branches["develop"].Tip;
+            repo.Branches.Add(branchName, branchingCommit).Checkout();
+
+            AddOneCommitToHead(repo, "first commit on release");
+
+            var releaseBranch = repo.Branches[branchName];
+            var firstCommit = releaseBranch.Tip;
+
+            var sign = Constants.SignatureNow();
+            repo.Tags.Add("0.5.0-alpha5", branchingCommit, sign, "release");
+
+            var finder = new ReleaseVersionFinder
+            {
+                Repository = repo,
+                Commit = firstCommit,
+                ReleaseBranch = releaseBranch,
+            };
+
+            var version = finder.FindVersion();
+            Assert.AreEqual(0, version.Version.Major);
+            Assert.AreEqual(5, version.Version.Minor);
+            Assert.AreEqual(0, version.Version.Patch);
+            Assert.AreEqual(Stability.Alpha, version.Version.Stability);
+            Assert.AreEqual(BranchType.Release, version.BranchType);
+            Assert.AreEqual(5, version.Version.PreReleasePartOne, "PreReleasePartOne should be set to 5 from the tag");
+            Assert.AreEqual(1, version.Version.PreReleasePartTwo, "PreReleasePartTwo should be set to 1 since there is 1 commit");
+        } 
     }
 
     [Test]
     public void Second_commit()
     {
-        var branchingCommit = new MockCommit
-                              {
-                                  MessageEx = "branching commit",
-                              CommitterEx = 3.Seconds().Ago().ToSignature()
-                              };
-        var secondCommit = new MockCommit
-                           {
-                               MessageEx = "second commit on release",
-                              CommitterEx = 1.Seconds().Ago().ToSignature()
-                           };
-        var firstCommit = new MockCommit
-                         {
-                             MessageEx = "first commit on release",
-                              CommitterEx = 2.Seconds().Ago().ToSignature()
-                         };
-        var releaseBranch = new MockBranch("release-0.4.0")
-                            {
-                                secondCommit,
-                                firstCommit,
-                                branchingCommit,
-                            };
-        var finder = new ReleaseVersionFinder
-                     {
-                         ReleaseBranch = releaseBranch,
-                         Commit = secondCommit,
-                         Repository = new MockRepository
-                                      {
-                                          Branches = new MockBranchCollection
-                                                     {
-                                                         new MockBranch("develop")
-                                                         {
-                                                             branchingCommit
-                                                         },
-                                                         releaseBranch
-                                                     },
-                                          Tags = new MockTagCollection
-                                                 {
-                                                     new MockTag
-                                                     {
-                                                         TargetEx = branchingCommit,
-                                                         NameEx = "0.4.0-alpha5"
-                                                     }
-                                                 }
-                                      },
-                     };
-        var version = finder.FindVersion();
-        Assert.AreEqual(0, version.Version.Major);
-        Assert.AreEqual(4, version.Version.Minor);
-        Assert.AreEqual(0, version.Version.Patch);
-        Assert.AreEqual(Stability.Alpha, version.Version.Stability);
-        Assert.AreEqual(BranchType.Release, version.BranchType);
-        Assert.AreEqual(5, version.Version.PreReleasePartOne, "PreReleasePartOne should be set to 5 from the tag");
-        Assert.AreEqual(2, version.Version.PreReleasePartTwo, "PreReleasePartTwo should be set to 3 since there is 2 commits on the branch");
+        var repoPath = Clone(ASBMTestRepoWorkingDirPath);
+        using (var repo = new Repository(repoPath))
+        {
+            const string branchName = "release-0.4.0";
+
+            var branchingCommit = repo.Branches["develop"].Tip;
+            repo.Branches.Add(branchName, branchingCommit).Checkout();
+
+            AddOneCommitToHead(repo, "first commit on release");
+            AddOneCommitToHead(repo, "second commit on release");
+
+            var releaseBranch = repo.Branches[branchName];
+            var secondCommit = releaseBranch.Tip;
+
+            var sign = Constants.SignatureNow();
+            repo.Tags.Add("0.4.0-alpha5", branchingCommit, sign, "release");
+
+            var finder = new ReleaseVersionFinder
+            {
+                Repository = repo,
+                Commit = secondCommit,
+                ReleaseBranch = releaseBranch,
+            };
+
+            var version = finder.FindVersion();
+            Assert.AreEqual(0, version.Version.Major);
+            Assert.AreEqual(4, version.Version.Minor);
+            Assert.AreEqual(0, version.Version.Patch);
+            Assert.AreEqual(Stability.Alpha, version.Version.Stability);
+            Assert.AreEqual(BranchType.Release, version.BranchType);
+            Assert.AreEqual(5, version.Version.PreReleasePartOne, "PreReleasePartOne should be set to 5 from the tag");
+            Assert.AreEqual(2, version.Version.PreReleasePartTwo, "PreReleasePartTwo should be set to 3 since there is 2 commits on the branch");
+        }
     }
 
 
     [Test]
     public void Override_stage_using_tag()
     {
+        var repoPath = Clone(ASBMTestRepoWorkingDirPath);
+        using (var repo = new Repository(repoPath))
+        {
+            const string branchName = "release-0.4.0";
 
-        var branchingCommit = new MockCommit
-                              {
-                                  MessageEx = "branching commit",
-                              CommitterEx = 2.Seconds().Ago().ToSignature()
-                              };
-        var firstCommit = new MockCommit
-                          {
-                              MessageEx = "first commit on release",
-                              CommitterEx = 1.Seconds().Ago().ToSignature()
-                          };
-        var releaseBranch = new MockBranch("release-0.4.0")
-                            {
-                                firstCommit,
-                                branchingCommit,
-                            };
-        var finder = new ReleaseVersionFinder
-                     {
-                         ReleaseBranch = releaseBranch,
-                         Commit = firstCommit,
-                         Repository = new MockRepository
-                                      {
-                                          Branches = new MockBranchCollection
-                                                     {
-                                                         new MockBranch("develop")
-                                                         {
-                                                             branchingCommit
-                                                         },
-                                                         releaseBranch
-                                                     },
-                                          Tags = new MockTagCollection
-                                                 {
-                                                     new MockTag
-                                                     {
-                                                         TargetEx = branchingCommit,
-                                                         NameEx = "0.4.0-RC4"
-                                                     }
-                                                 }
-                                      },
-                     };
-        var version = finder.FindVersion();
-        //tag: 0.4.0-RC1 => 
-        Assert.AreEqual(0, version.Version.Major);
-        Assert.AreEqual(4, version.Version.Minor);
-        Assert.AreEqual(0, version.Version.Patch);
-        Assert.AreEqual(Stability.ReleaseCandidate, version.Version.Stability);
-        Assert.AreEqual(BranchType.Release, version.BranchType);
-        Assert.AreEqual(4, version.Version.PreReleasePartOne);
+            var branchingCommit = repo.Branches["develop"].Tip;
+            repo.Branches.Add(branchName, branchingCommit).Checkout();
+
+            AddOneCommitToHead(repo, "first commit on release");
+
+            var releaseBranch = repo.Branches[branchName];
+            var firstCommit = releaseBranch.Tip;
+
+            var sign = Constants.SignatureNow();
+            repo.Tags.Add("0.4.0-RC4", branchingCommit, sign, "release");
+
+            var finder = new ReleaseVersionFinder
+            {
+                Repository = repo,
+                Commit = firstCommit,
+                ReleaseBranch = releaseBranch,
+            };
+
+            var version = finder.FindVersion();
+            //tag: 0.4.0-RC1 => 
+            Assert.AreEqual(0, version.Version.Major);
+            Assert.AreEqual(4, version.Version.Minor);
+            Assert.AreEqual(0, version.Version.Patch);
+            Assert.AreEqual(Stability.ReleaseCandidate, version.Version.Stability);
+            Assert.AreEqual(BranchType.Release, version.BranchType);
+            Assert.AreEqual(4, version.Version.PreReleasePartOne);
+        }
     }
 
         //TODO:
