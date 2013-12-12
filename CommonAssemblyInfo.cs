8ccd51c37f7b3181c1c12f8cec4c27ec38ac85c6
Andreas Öhlund <andreasohlund2@gmail.com>
2013-12-12 08:19:12 +0100

    Releasing 0.12.0

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("GitFlowVersion")]
 [assembly: AssemblyProduct("GitFlowVersion")]
-[assembly: AssemblyVersion("0.11.0")]
-[assembly: AssemblyFileVersion("0.11.0")]
+[assembly: AssemblyVersion("0.12.0")]
+[assembly: AssemblyFileVersion("0.12.0")]
 [assembly: InternalsVisibleTo("Tests")]
