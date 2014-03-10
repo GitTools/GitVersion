07c72e01a6c06ea2a36eb9277c4633fca2052492
Andreas Öhlund <andreasohlund2@gmail.com>
2014-03-10 15:37:28 +0100

    Releasing 0.20.0
    Hopefully final manual bump before we start dogfooding

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("GitVersion")]
 [assembly: AssemblyProduct("GitVersion")]
-[assembly: AssemblyVersion("0.19.0")]
-[assembly: AssemblyFileVersion("0.19.0")]
+[assembly: AssemblyVersion("0.20.0")]
+[assembly: AssemblyFileVersion("0.20.0")]
 [assembly: InternalsVisibleTo("Tests")]
