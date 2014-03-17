314cf17521ecc198c8633e7c03b30d4c6ad9c59f
Geert van Horrik <geert@catenalogic.com>
2014-03-17 15:20:01 +0100

    Changed tabs to spaces in HelpWriter

--- GitVersion/HelpWriter.cs
+++ GitVersion/HelpWriter.cs
@@ -11,12 +11,12 @@ namespace GitVersion
 
 GitVersion [path] [/l logFilePath]
 
-	path    The directory containing .git. If not defined current directory is used.
+    path    The directory containing .git. If not defined current directory is used.
     /url    Url to remote git repository.
-    /b	    Name of the branch to use on the remote repository, must be used in combination with /url.
-    /u	    Username in case authentication is required.
-    /p	    Password in case authentication is required.
-	/l	    Path to logfile.
+    /b      Name of the branch to use on the remote repository, must be used in combination with /url.
+    /u      Username in case authentication is required.
+    /p      Password in case authentication is required.
+    /l      Path to logfile.
 ";
             Console.Write(message);
         }
