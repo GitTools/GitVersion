8cb9bcd2364261ea6130646907104d570c45c8a3
Jake Ginnivan <jake.ginnivan@readify.net>
2014-03-19 21:33:24 +0000

    Migrated most of GitHubFlows acceptance tests across

---
+++ AcceptanceTests/Helpers/GitHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using LibGit2Sharp;
+
+namespace GitHubFlowVersion.AcceptanceTests.Helpers
+{
+    public static class GitHelper
+    {
+        public static Commit MakeACommit(this IRepository repository)
+        {
+            var randomFile = Path.Combine(repository.Info.WorkingDirectory, Guid.NewGuid().ToString());
+            File.WriteAllText(randomFile, string.Empty);
+            repository.Index.Stage(randomFile);
+            return repository.Commit("Test Commit", new Signature("Test User", "test@email.com", DateTimeOffset.UtcNow));
+        }
+
+        public static Commit[] MakeCommits(this IRepository repository, int numCommitsToMake)
+        {
+            return Enumerable.Range(1, numCommitsToMake)
+                .Select(x => repository.MakeACommit())
+                .ToArray();
+        }
+
+        public static Tag MakeATaggedCommit(this IRepository repository, string tag)
+        {
+            var commit = repository.MakeACommit();
+            var existingTag = repository.Tags.SingleOrDefault(t=>t.Name == tag);
+            if (existingTag != null)
+                return existingTag;
+            return repository.Tags.Add(tag, commit);
+        }
+    }
+}
---
+++ AcceptanceTests/ToFileSpecification.cs
@@ -0,0 +1,44 @@
+﻿//using System.Collections.Generic;
+//using System.IO;
+//using System.Web.Script.Serialization;
+//using GitHubFlowVersion.AcceptanceTests.Helpers;
+//using Xunit;
+
+//namespace GitHubFlowVersion.AcceptanceTests
+//{
+//    public class ToFileSpecification : RepositorySpecification
+//    {
+//        private string _tempFile;
+//        private ExecutionResults _result;
+//        private const string TaggedVersion = "1.2.3";
+
+//        public void GivenARepositoryWithATaggedCommit()
+//        {
+//            Repository.MakeATaggedCommit(TaggedVersion);
+//            Repository.MakeACommit();
+//        }
+
+//        public void AndGivenThereIsANextVersionTxtFile()
+//        {
+//            Repository.AddNextVersionTxtFile(TaggedVersion);
+//        }
+
+//        public void WhenGitHubFlowVersionIsExecutedWithToFileOptionSet()
+//        {
+//            _tempFile = Path.Combine(PathHelper.GetTempPath(), "ToFileTest.json");
+//            _result = GitVersionHelper.ExecuteIn(RepositoryPath, toFile: _tempFile);
+//        }
+
+//        public void ThenProcessExitedWithoutError()
+//        {
+//            _result.AssertExitedSuccessfully();
+//        }
+
+//        public void ThenVariablesShouldBeWrittenToOutputFile()
+//        {
+//            var output = File.ReadAllText(_tempFile);
+//            var variables = (Dictionary<string, object>)new JavaScriptSerializer().DeserializeObject(output);
+//            Assert.Equal("1.2.4+001", (string)variables["GitHubFlowVersion_FullSemVer"]);
+//        }
+//    }
+//}
