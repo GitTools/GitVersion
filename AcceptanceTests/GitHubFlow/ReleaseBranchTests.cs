1477188163b1325a8abca831847358366bb789e3
Jake Ginnivan <jake.ginnivan@readify.net>
2014-05-09 06:34:12 +0100

    Added test

--- AcceptanceTests/GitHubFlow/ReleaseBranchTests.cs
+++ AcceptanceTests/GitHubFlow/ReleaseBranchTests.cs
@@ -26,5 +26,25 @@
                 result.OutputVariables[VariableProvider.FullSemVer].ShouldBe("2.0.0-beta.1+5");
             }
         }
+
+        [Fact]
+        public void WhenReleaseBranchIsMergedIntoMasterVersionIsTakenWithIt()
+        {
+            using (var fixture = new EmptyRepositoryFixture())
+            {
+                const string TaggedVersion = "1.0.3";
+                fixture.Repository.MakeATaggedCommit(TaggedVersion);
+                fixture.Repository.MakeCommits(5);
+                fixture.Repository.CreateBranch("release-2.0.0");
+                fixture.Repository.Checkout("release-2.0.0");
+                fixture.Repository.Checkout("master");
+                fixture.Repository.MergeNoFF("release-2.0.0", Constants.SignatureNow());
+
+                var result = GitVersionHelper.ExecuteIn(fixture.RepositoryPath);
+
+                result.ExitCode.ShouldBe(0);
+                result.OutputVariables[VariableProvider.FullSemVer].ShouldBe("2.0.0+6");
+            }
+        }
     }
 }
