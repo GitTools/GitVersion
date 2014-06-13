10a14fac4abd5f73a58b4fe7b8dcb3b0b18516b4
Alexander Groß <agross@therightstuff.de>
2014-06-13 09:36:55 +0200

    Creating a singleton per arguments passed to git_version

--- GitVersionExe/GemAssets/lib/git_version.rb
+++ GitVersionExe/GemAssets/lib/git_version.rb
@@ -2,6 +2,11 @@ require 'git_version/parser'
 
 module GitVersion
   def git_version(args = nil)
-    Parser.new(args)
+    parsers.fetch(args) { |a| parsers[a] = Parser.new(a) }
+  end
+
+  private
+  def parsers
+    @parsers ||= {}
   end
 end
