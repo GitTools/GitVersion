d3947f1bda5c934e5513df9942cff101cf5855dc
Simon Cropp <simon.cropp@gmail.com>
2014-06-30 19:21:07 +0100

    create beta version

--- GitVersionTask/AssemblyInfo.cs
+++ GitVersionTask/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("GitVersionTask")]
 [assembly: AssemblyProduct("GitVersionTask")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0-beta0001")]
