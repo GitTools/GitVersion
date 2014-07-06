e1d76da6569d59255e3b9bbeb67dd7a7e014bb3f
Simon Cropp <simon.cropp@gmail.com>
2014-07-06 12:32:35 +1000

    fix error message for GitVersionTask
    we dont rely on fody anymore

--- GitVersionTask/VersionAndBranchFinder.cs
+++ GitVersionTask/VersionAndBranchFinder.cs
@@ -13,7 +13,7 @@
             {
                 var message =
                     "No .git directory found in provided solution path. This means the assembly may not be versioned correctly. " +
-                    "To fix this warning either clone the repository using git or remove the `GitVersion.Fody` nuget package. " +
+                    "To fix this warning either clone the repository using git or remove the `GitVersionTask` nuget package. " +
                     "To temporarily work around this issue add a AssemblyInfo.cs with an appropriate `AssemblyVersionAttribute`." +
                     "If it is detected that this build is occurring on a CI server an error may be thrown.";
                 Logger.WriteWarning(message);
