9b1eafbf9499c486ce1a3b9851695c5581e4be44
Jake Ginnivan <jake@ginnivan.net>
2014-08-08 09:10:08 +0100

    Bumped version to 1.2.0 and fixed issue with duplicate command line NuGet package

--- GitVersionCore/AssemblyInfo.cs
+++ GitVersionCore/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("GitVersionCore")]
 [assembly: AssemblyProduct("GitVersion")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
 [assembly: InternalsVisibleTo("Tests")]
 [assembly: InternalsVisibleTo("GitVersion")]
 [assembly: InternalsVisibleTo("AcceptanceTests")]
