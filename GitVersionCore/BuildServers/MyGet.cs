7be161feb2e17718b1846792b86a05037e0ed726
Maarten Balliauw <maarten@maartenballiauw.be>
2014-10-28 11:37:32 +0100

    Added MyGet Build Services support

---
+++ GitVersionCore/BuildServers/MyGet.cs
@@ -0,0 +1,64 @@
+﻿namespace GitVersion
+{
+    using System;
+
+    public class MyGet : BuildServerBase
+    {
+        Authentication authentication;
+
+        public MyGet(Authentication authentication)
+        {
+            this.authentication = authentication;
+        }
+
+        public override bool CanApplyToCurrentContext()
+        {
+            var buildRunner = Environment.GetEnvironmentVariable("BuildRunner");
+
+            return !string.IsNullOrEmpty(buildRunner)
+                && buildRunner.Equals("MyGet", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public override void PerformPreProcessingSteps(string gitDirectory)
+        {
+            if (string.IsNullOrEmpty(gitDirectory))
+            {
+                throw new WarningException("Failed to find .git directory on agent.");
+            }
+
+            GitHelper.NormalizeGitDirectory(gitDirectory, authentication);
+        }
+
+        public override string[] GenerateSetParameterMessage(string name, string value)
+        {
+            return new[]
+            {
+                string.Format("##teamcity[setParameter name='GitVersion.{0}' value='{1}']", name, EscapeValue(value)),
+                string.Format("##teamcity[setParameter name='system.GitVersion.{0}' value='{1}']", name, EscapeValue(value))
+            };
+        }
+
+        public override string GenerateSetVersionMessage(string versionToUseForBuildNumber)
+        {
+            return string.Format("##teamcity[buildNumber '{0}']", EscapeValue(versionToUseForBuildNumber));
+        }
+
+        static string EscapeValue(string value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            // List of escape values from http://confluence.jetbrains.com/display/TCD8/Build+Script+Interaction+with+TeamCity
+
+            value = value.Replace("|", "||");
+            value = value.Replace("'", "|'");
+            value = value.Replace("[", "|[");
+            value = value.Replace("]", "|]");
+            value = value.Replace("\r", "|r");
+            value = value.Replace("\n", "|n");
+
+            return value;
+        }
+    }
+}
---
+++ GitVersionTask.Tests/BuildServers/MyGetTests.cs
@@ -0,0 +1,26 @@
+﻿using GitVersion;
+using NUnit.Framework;
+
+[TestFixture]
+public class MyGetTests
+{
+    [Test]
+    public void Develop_branch()
+    {
+        var authentication = new Authentication();
+        var versionBuilder = new MyGet(authentication);
+        var message = versionBuilder.GenerateSetVersionMessage("0.0.0-Unstable4");
+        Assert.AreEqual("##teamcity[buildNumber '0.0.0-Unstable4']", message);
+    }
+
+    [Test]
+    public void EscapeValues()
+    {
+        var authentication = new Authentication();
+        var versionBuilder = new MyGet(authentication);
+        var message = versionBuilder.GenerateSetParameterMessage("Foo", "0.8.0-unstable568 Branch:'develop' Sha:'ee69bff1087ebc95c6b43aa2124bd58f5722e0cb'");
+        Assert.AreEqual("##teamcity[setParameter name='GitVersion.Foo' value='0.8.0-unstable568 Branch:|'develop|' Sha:|'ee69bff1087ebc95c6b43aa2124bd58f5722e0cb|'']", message[0]);
+        Assert.AreEqual("##teamcity[setParameter name='system.GitVersion.Foo' value='0.8.0-unstable568 Branch:|'develop|' Sha:|'ee69bff1087ebc95c6b43aa2124bd58f5722e0cb|'']", message[1]);
+    }
+
+}
