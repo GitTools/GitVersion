221c23761e3bb5c8d2b208b5342db1a2562bd8eb
Jake Ginnivan <jake@ginnivan.net>
2014-11-17 10:03:38 +0000

    Added new test scenario showing issue in #296

---
+++ GitVersionCore.Tests/IntegrationTests/GitFlow/SwitchingToGitFlowScenarios.cs
@@ -0,0 +1,20 @@
+﻿using GitVersion;
+using LibGit2Sharp;
+using NUnit.Framework;
+
+[TestFixture]
+public class SwitchingToGitFlowScenarios
+{
+    [Test]
+    public void WhenDevelopBranchedFromMasterWithLegacyVersionTags_DevelopCanUseReachableTag()
+    {
+        using (var fixture = new EmptyRepositoryFixture(new Config()))
+        {
+            fixture.Repository.MakeCommits(5);
+            fixture.Repository.MakeATaggedCommit("1.0.0.0");
+            fixture.Repository.MakeCommits(2);
+            fixture.Repository.CreateBranch("develop").Checkout();
+            fixture.AssertFullSemver("1.1.0-unstable.0+0");
+        }
+    }
+}
