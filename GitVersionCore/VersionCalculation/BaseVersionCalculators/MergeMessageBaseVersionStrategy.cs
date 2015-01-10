62b6f42062f30382709719ad5a2a4c696688f2f6
Jake Ginnivan <jake@ginnivan.net>
2015-01-10 21:28:19 +0000

    Added merge message base version strategy

---
+++ GitVersionCore/VersionCalculation/BaseVersionCalculators/MergeMessageBaseVersionStrategy.cs
@@ -0,0 +1,15 @@
+﻿namespace GitVersion.VersionCalculation.BaseVersionCalculators
+{
+    public class MergeMessageBaseVersionStrategy : BaseVersionStrategy
+    {
+        public override BaseVersion GetVersion(GitVersionContext context)
+        {
+            // TODO when this approach works, inline the other class into here
+            SemanticVersion semanticVersion;
+            if (MergeMessageParser.TryParse(context.CurrentCommit, context.Configuration, out semanticVersion))
+                return new BaseVersion(true, semanticVersion);
+
+            return null;
+        }
+    }
+}
