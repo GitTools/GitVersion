87f2519281ae70e6b17f590592464fe1e159862c
Jake Ginnivan <jake@ginnivan.net>
2015-01-10 21:10:34 +0000

    Starting on base version calculators

---
+++ GitVersionCore.Tests/VersionCalculation/BaseVersionCalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿namespace GitVersionCore.Tests.VersionCalculation
+{
+    using GitVersion;
+    using GitVersion.VersionCalculation;
+    using GitVersion.VersionCalculation.BaseVersionCalculators;
+    using GitVersionCore.Tests.VersionCalculation.Strategies;
+    using NUnit.Framework;
+    using Shouldly;
+
+    [TestFixture]
+    public class BaseVersionCalculatorTests
+    {
+        [Test]
+        public void ChoosesHighestVersionReturnedFromStrategies()
+        {
+            var context = new GitVersionContextBuilder().Build();
+            var sut = new BaseVersionCalculator(new V1Strategy(), new V2Strategy());
+
+            var baseVersion = sut.GetBaseVersion(context);
+
+            baseVersion.SemanticVersion.ToString().ShouldBe("2.0.0");
+            baseVersion.ShouldIncrement.ShouldBe(true);
+        }
+
+        class V1Strategy : BaseVersionStrategy
+        {
+            public override BaseVersion GetVersion(GitVersionContext context)
+            {
+                return new BaseVersion(false, new SemanticVersion(1));
+            }
+        }
+
+        class V2Strategy : BaseVersionStrategy
+        {
+            public override BaseVersion GetVersion(GitVersionContext context)
+            {
+                return new BaseVersion(true, new SemanticVersion(2));
+            }
+        }
+    }
+}
