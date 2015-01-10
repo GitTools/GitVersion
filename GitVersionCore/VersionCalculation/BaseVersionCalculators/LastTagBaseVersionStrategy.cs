b25220a9973dfb9c48fd8e4f2f0b5d782d85dc3d
Jake Ginnivan <jake@ginnivan.net>
2015-01-10 21:50:19 +0000

    Adding LastTagBaseVersionStrategy

---
+++ GitVersionCore/VersionCalculation/BaseVersionCalculators/LastTagBaseVersionStrategy.cs
@@ -0,0 +1,14 @@
+﻿namespace GitVersion.VersionCalculation.BaseVersionCalculators
+{
+    public class LastTagBaseVersionStrategy : BaseVersionStrategy
+    {
+        public override BaseVersion GetVersion(GitVersionContext context)
+        {
+            VersionTaggedCommit version;
+            if (new LastTaggedReleaseFinder(context).GetVersion(out version))
+                return new BaseVersion(true, version.SemVer);
+
+            return null;
+        }
+    }
+}
