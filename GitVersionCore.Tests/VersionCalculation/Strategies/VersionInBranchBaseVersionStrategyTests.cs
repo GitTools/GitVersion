513004a997456b29e2063738364a6730a9979011
Jake Ginnivan <jake@ginnivan.net>
2015-01-11 12:20:43 +0000

    Added missing base version finder

---
+++ GitVersionCore.Tests/VersionCalculation/Strategies/VersionInBranchBaseVersionStrategyTests.cs
@@ -0,0 +1,34 @@
+﻿namespace GitVersionCore.Tests.VersionCalculation.Strategies
+{
+    using GitVersion.VersionCalculation.BaseVersionCalculators;
+    using NUnit.Framework;
+    using Shouldly;
+
+    [TestFixture]
+    public class VersionInBranchBaseVersionStrategyTests
+    {
+        [Test]
+        [TestCase("release-2.0.0", "2.0.0")]
+        [TestCase("release/2.0.0", "2.0.0")]
+        [TestCase("hotfix-2.0.0", "2.0.0")]
+        [TestCase("hotfix/2.0.0", "2.0.0")]
+        [TestCase("hotfix/2.0.0", "2.0.0")]
+        [TestCase("feature/JIRA-123", null)]
+        public void CanTakeVersionFromBranchName(string branchName, string expectedBaseVersion)
+        {
+            var context = new GitVersionContextBuilder()
+                .WithBranch(branchName)
+                .AddCommit()
+                .Build();
+
+            var sut = new VersionInBranchBaseVersionStrategy();
+
+            var baseVersion = sut.GetVersion(context);
+
+            if (expectedBaseVersion == null)
+                baseVersion.ShouldBe(null);
+            else
+                baseVersion.SemanticVersion.ToString().ShouldBe(expectedBaseVersion);
+        }
+    }
+}
---
+++ GitVersionCore/VersionCalculation/BaseVersionCalculators/VersionInBranchBaseVersionStrategy.cs
@@ -0,0 +1,35 @@
+﻿namespace GitVersion.VersionCalculation.BaseVersionCalculators
+{
+    using System;
+    using System.Linq;
+
+    public class VersionInBranchBaseVersionStrategy : BaseVersionStrategy
+    {
+        public override BaseVersion GetVersion(GitVersionContext context)
+        {
+            var versionInBranch = GetVersionInBranch(context);
+            if (versionInBranch != null)
+            {
+                var firstCommitOfBranch = context.CurrentBranch.Commits.Last();
+                return new BaseVersion(false, versionInBranch.Item2, firstCommitOfBranch);
+            }
+
+            return null;
+        }
+
+        Tuple<string, SemanticVersion> GetVersionInBranch(GitVersionContext context)
+        {
+            var branchParts = context.CurrentBranch.Name.Split('/', '-');
+            foreach (var part in branchParts)
+            {
+                SemanticVersion semanticVersion;
+                if (SemanticVersion.TryParse(part, context.Configuration.GitTagPrefix, out semanticVersion))
+                {
+                    return Tuple.Create(part, semanticVersion);
+                }
+            }
+
+            return null;
+        }
+    }
+}
