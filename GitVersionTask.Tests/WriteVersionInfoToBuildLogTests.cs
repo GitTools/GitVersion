a0f8e397ad38e9b2f5b211b18dfe8b250d990bb0
Henning Moe <hem@isys.no>
2015-03-24 09:25:16 +0100

    Add integration test for #406.

---
+++ GitVersionTask.Tests/WriteVersionInfoToBuildLogTests.cs
@@ -0,0 +1,22 @@
+﻿
+namespace GitVersionTask.Tests
+{
+    using System.IO;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class WriteVersionInfoToBuildLogTests
+    {
+        [Test]
+        public void UsingInvalidGitDirectory_ThrowsDirectoryNotFoundException()
+        {
+            var task = new WriteVersionInfoToBuildLog
+            {
+                BuildEngine = new MockBuildEngine(),
+                SolutionDirectory = Path.GetTempPath()
+            };
+
+            Assert.That(task.InnerExecute, Throws.InstanceOf<DirectoryNotFoundException>());
+        }
+    }
+}
