1a7a2c7ecf9183855bad4a0b531d9be7b0d5440c
Jake Ginnivan <jake@ginnivan.net>
2015-04-23 21:00:52 +0100

    Dynamic repo is now updated properly when it exists

--- GitVersionExe.Tests/GitPreparerTests.cs
+++ GitVersionExe.Tests/GitPreparerTests.cs
@@ -79,6 +79,57 @@ public class GitPreparerTests
     }
 
     [Test]
+    public void UpdatesExistingDynamicRepository()
+    {
+        var repoName = Guid.NewGuid().ToString();
+        var tempPath = Path.GetTempPath();
+        var tempDir = Path.Combine(tempPath, repoName);
+        Directory.CreateDirectory(tempDir);
+        string dynamicRepositoryPath = null;
+
+        try
+        {
+            using (var mainRepositoryFixture = new EmptyRepositoryFixture(new Config()))
+            {
+                mainRepositoryFixture.Repository.MakeCommits(5);
+                mainRepositoryFixture.Repository.CreateFileAndCommit("TestFile.txt");
+
+                mainRepositoryFixture.Repository.CreateBranch(SpecificBranchName);
+
+                var arguments = new Arguments
+                {
+                    TargetPath = tempDir,
+                    TargetUrl = mainRepositoryFixture.RepositoryPath
+                };
+
+                // Copy contents into working directory
+                File.Copy(Path.Combine(mainRepositoryFixture.RepositoryPath, "TestFile.txt"), Path.Combine(tempDir, "TestFile.txt"));
+                arguments.TargetBranch = "master";
+
+                var gitPreparer = new GitPreparer(arguments);
+                gitPreparer.InitialiseDynamicRepositoryIfNeeded();
+                dynamicRepositoryPath = gitPreparer.GetDotGitDirectory();
+
+                var newCommit = mainRepositoryFixture.Repository.MakeACommit();
+                gitPreparer.InitialiseDynamicRepositoryIfNeeded();
+
+                using (var repository = new Repository(dynamicRepositoryPath))
+                {
+                    mainRepositoryFixture.DumpGraph();
+                    repository.DumpGraph();
+                    repository.Commits.ShouldContain(c => c.Sha == newCommit.Sha);
+                }
+            }
+        }
+        finally
+        {
+            Directory.Delete(tempDir, true);
+            if (dynamicRepositoryPath != null)
+                DeleteHelper.DeleteGitRepository(dynamicRepositoryPath);
+        }
+    }
+
+    [Test]
     public void PicksAnotherDirectoryNameWhenDynamicRepoFolderTaken()
     {
         var repoName = Guid.NewGuid().ToString();
