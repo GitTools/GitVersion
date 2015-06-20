6aa60b61bcd46005c3f9080487cb438a1f32af23
Jake Ginnivan <jake@ginnivan.net>
2015-06-20 10:57:04 +0100

    Added timing to indent scopes

--- GitVersionCore/Logger.cs
+++ GitVersionCore/Logger.cs
@@ -1,6 +1,7 @@
 namespace GitVersion
 {
     using System;
+    using System.Globalization;
 
     public static class Logger
     {
@@ -17,12 +18,13 @@ namespace GitVersion
 
         public static IDisposable IndentLog(string operationDescription)
         {
+            var start = DateTime.Now;
             indent = indent + "  ";
             WriteInfo("Begin: " + operationDescription);
             return new ActionDisposable(() =>
             {
                 indent = indent.Substring(0, indent.Length - 2);
-                WriteInfo("End: " + operationDescription);
+                WriteInfo(string.Format(CultureInfo.InvariantCulture, "End: {0} (Took: {1:N}ms)", operationDescription, DateTime.Now.Subtract(start).TotalMilliseconds));
             });
         }
 
@@ -35,7 +37,7 @@ namespace GitVersion
 
         static Action<string> LogMessage(Action<string> logAction, string level)
         {
-            return s => logAction(string.Format("{0}{1} [{2:MM/dd/yy H:mm:ss:ff}] {3}", indent, level, DateTime.Now, s));
+            return s => logAction(string.Format(CultureInfo.InvariantCulture, "{0}{1} [{2:MM/dd/yy H:mm:ss:ff}] {3}", indent, level, DateTime.Now, s));
         }
 
         public static void Reset()
