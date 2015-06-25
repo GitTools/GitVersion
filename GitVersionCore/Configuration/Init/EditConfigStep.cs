b0d49df75ee8e83c228fa58e6ee91a4450acb8e9
Jake Ginnivan <jake@ginnivan.net>
2015-06-25 21:16:56 +0100

    Added set next version via gitversion init

--- GitVersionCore/Configuration/Init/EditConfigStep.cs
+++ GitVersionCore/Configuration/Init/EditConfigStep.cs
@@ -21,16 +21,20 @@ namespace GitVersion.Configuration.Init
                     steps.Enqueue(new PickBranchingStrategyStep());
                     return StepResult.Ok();
 
-                case "3":
+                case "4":
+                    steps.Enqueue(new SetNextVersion());
+                    return StepResult.Ok();
+
+                case "4":
                     steps.Enqueue(new ConfigureBranches());
                     return StepResult.Ok();
-                case "4":
+                case "5":
                     steps.Enqueue(new GlobalModeSetting(new EditConfigStep(), false));
                     return StepResult.Ok();
-                case "5":
+                case "6":
                     steps.Enqueue(new AssemblyVersioningSchemeSetting());
                     return StepResult.Ok();
-                case "6":
+                case "7":
                     steps.Enqueue(new SetupBuildScripts());
                     return StepResult.Ok();
             }
@@ -46,15 +50,46 @@ namespace GitVersion.Configuration.Init
 
 2) Run getting started wizard
 
-3) Branch specific configuration
-4) Branch Increment mode (per commit/after tag) (Current: {0})
-5) Assembly versioning scheme (Current: {1})
-6) Setup build scripts", config.VersioningMode, config.AssemblyVersioningScheme);
+3) Set next version number
+4) Branch specific configuration
+5) Branch Increment mode (per commit/after tag) (Current: {0})
+6) Assembly versioning scheme (Current: {1})
+7) Setup build scripts", config.VersioningMode, config.AssemblyVersioningScheme);
         }
 
         protected override string DefaultResult
         {
             get { return null; }
+        }
+    }
+
+    public class SetNextVersion : ConfigInitWizardStep
+    {
+        protected override StepResult HandleResult(string result, Queue<ConfigInitWizardStep> steps, Config config, string workingDirectory, IFileSystem fileSystem)
+        {
+            if (string.IsNullOrEmpty(result))
+            {
+                steps.Enqueue(new EditConfigStep());
+                return StepResult.Ok();
+            }
+
+            SemanticVersion semVer;
+            if (!SemanticVersion.TryParse(result, string.Empty, out semVer))
+                return StepResult.InvalidResponseSelected();
+
+            config.NextVersion = semVer.ToString("t");
+            steps.Enqueue(new EditConfigStep());
+            return StepResult.Ok();
+        }
+
+        protected override string GetPrompt(Config config, string workingDirectory, IFileSystem fileSystem)
+        {
+            return @"What would you like to set the next version to (enter nothing to cancel)?";
+        }
+
+        protected override string DefaultResult
+        {
+            get { throw new System.NotImplementedException(); }
         }
     }
 }
