fbc342a5c498807ca4623d8538701c25321f0106
Jake Ginnivan <jake@ginnivan.net>
2015-07-20 12:08:02 +0800

    Fixed build to point at new paths

--- build/Build.cmd
+++ build/Build.cmd
@@ -1,12 +1,10 @@
 @echo on
 
 set framework=v4.0.30319
+set src=%~dp0..\src\
 
-"%~dp0.nuget\nuget.exe" restore
+"%src%.nuget\nuget.exe" restore %src%
 
-"%SystemDrive%\Windows\Microsoft.NET\Framework\%framework%\MSBuild.exe" "%~dp0GitVersion.sln"
+"%SystemDrive%\Windows\Microsoft.NET\Framework\%framework%\MSBuild.exe" "%src%GitVersion.sln"
 
-mkdir "%~dp0GitVersionExe\bin\Intermediate"
-cp "%~dp0GitVersionExe\bin\Debug\GitVersion.exe" "%~dp0GitVersionExe\bin\Intermediate\GitVersion.exe"
-
-"%~dp0GitVersionExe\bin\Intermediate\GitVersion.exe" /l console /output buildserver /updateAssemblyInfo /proj "%~dp0GitVersion.sln"
+"%%~dp0NuGetCommandLineBuild\tools\GitVersion.exe" /l console /output buildserver /updateAssemblyInfo /proj "%src%GitVersion.sln"
