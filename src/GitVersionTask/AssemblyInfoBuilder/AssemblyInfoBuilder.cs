c53fab7199b37642475fb6c028ef4cda33a25043
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-07-21 16:38:44 +0200

    Add GitVersionInformationAttribute to generated assembly info files to make reflection of the contained version information easier.

--- src/GitVersionTask/AssemblyInfoBuilder/AssemblyInfoBuilder.cs
+++ src/GitVersionTask/AssemblyInfoBuilder/AssemblyInfoBuilder.cs
@@ -18,6 +18,7 @@ using System.Reflection;
 [assembly: AssemblyFileVersion(""{1}"")]
 [assembly: AssemblyInformationalVersion(""{2}"")]
 [assembly: ReleaseDate(""{3}"")]
+[assembly: GitVersionInformation()]
 
 [System.Runtime.CompilerServices.CompilerGenerated]
 sealed class ReleaseDateAttribute : System.Attribute
@@ -36,26 +37,48 @@ static class GitVersionInformation
 {4}
 }}
 
-
-",
+[System.Runtime.CompilerServices.CompilerGenerated]
+sealed class GitVersionInformationAttribute : System.Attribute
+{{
+{5}
+}}",
                                          vars.AssemblySemVer,
                                          vars.MajorMinorPatch + ".0",
                                          vars.InformationalVersion,
                                          vars.CommitDate,
-                                         GenerateVariableMembers(v))
+                                         GenerateStaticVariableMembers(v),
+                                         GenerateAttributeVariableMembers(v))
             .Trim();
 
         return assemblyInfo;
     }
 
 
-    static string GenerateVariableMembers(IList<KeyValuePair<string, string>> vars)
+    static string GenerateStaticVariableMembers(IList<KeyValuePair<string, string>> vars)
     {
         var members = new StringBuilder();
         for (var i = 0; i < vars.Count; i++)
         {
             var variable = vars[i];
             members.AppendFormat("    public static string {0} = \"{1}\";", variable.Key, variable.Value);
+
+            if (i < vars.Count - 1)
+            {
+                members.AppendLine();
+            }
+        }
+
+        return members.ToString();
+    }
+
+
+    static string GenerateAttributeVariableMembers(IList<KeyValuePair<string, string>> vars)
+    {
+        var members = new StringBuilder();
+        for (var i = 0; i < vars.Count; i++)
+        {
+            var variable = vars[i];
+            members.AppendFormat("    public string {0} {{ get {{ return \"{1}\"; }} }}", variable.Key, variable.Value);
 
             if (i < vars.Count - 1)
             {
