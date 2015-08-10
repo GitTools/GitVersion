944e7ed0a1806292c737daa329acd8b72dc83454
Oren Novotny <oren@novotny.org>
2015-08-10 09:08:13 -0400

    Add support for VSO Agent

---
+++ src/GitVersionCore/BuildServers/VsoAgent.cs
@@ -0,0 +1,27 @@
+﻿namespace GitVersion
+{
+    using System;
+
+    public class VsoAgent : BuildServerBase
+    {
+        public override bool CanApplyToCurrentContext()
+        { 
+            return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("TF_BUILD")); 
+        }
+
+        public override string[] GenerateSetParameterMessage(string name, string value)
+        {
+            return new[]
+            {
+                string.Format("##vso[task.setvariable variable=GitVersion.{0};]{1}", name, value)
+            };
+        }
+
+        public override string GenerateSetVersionMessage(string versionToUseForBuildNumber)
+        {
+            // Note: the VSO agent does not yet support updating the build display number from a variable
+            // this is here for later when they do but might need adjusting
+            return string.Format("##vso[task.setvariable variable=GitBuildNumber;]{0}", versionToUseForBuildNumber);
+        }
+    }
+}
