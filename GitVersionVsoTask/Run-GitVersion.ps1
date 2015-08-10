328a48aa74d68a9adcb99bb7e12f0c326e0904de
Oren Novotny <oren@novotny.org>
2015-08-10 12:20:42 -0400

    Initial stab at VSO build step

---
+++ GitVersionVsoTask/Run-GitVersion.ps1
@@ -0,0 +1,31 @@
+param (
+    [string] [Parameter(Mandatory = $false)]
+    $updateAssemblyInfo,
+    [string] [Parameter(Mandatory = $false)]
+    $additionalArguments
+)
+
+Write-Verbose 'Entering Run-GitVersion.ps1'
+
+# Import the Task.Common dll that has all the cmdlets we need for Build
+import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+# Returns a path to GitVersion.exe
+function Get-PathToGitVersionExe() { 
+ 	$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.ScriptBlock.File 
+ 	$targetPath = Join-Path -Path $PSScriptRoot -ChildPath "GitVersion.exe"  
+ 	return $targetPath 
+} 
+
+
+## Execution starts here
+
+
+$updateAssms = Convert-String $updateAssemblyInfo Boolean
+
+# Call GitVersion.exe
+$gvPath = Get-PathToGitVersionExe
+Write-Output "Path to GitVersion.exe = $gvPath"
+Invoke-Tool -Path $gvPath -Arguments "/output buildserver /updateAssemblyInfo $updateAssms $additionalArguments"
+
+
