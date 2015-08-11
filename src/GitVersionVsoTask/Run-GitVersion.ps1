f2096c4def771cbeb6778661307253d9fae8db2c
Oren Novotny <oren@novotny.org>
2015-08-11 10:46:11 -0400

    ensure local branch info is available

--- src/GitVersionVsoTask/Run-GitVersion.ps1
+++ src/GitVersionVsoTask/Run-GitVersion.ps1
@@ -25,10 +25,25 @@ $updateAssms = Convert-String $updateAssemblyInfo Boolean
 
 $loc = $($env:BUILD_SOURCESDIRECTORY)
 $branch = $($env:BUILD_SOURCEBRANCH)
+$commitId = $($env:BUILD_SOURCEVERSION)
+
+$localBranch = $branch.Trim().Replace("refs/heads/", "")
+
+Set-Location $loc
+
+# VSO checks out the commit as a detached HEAD
+# loop through and checkout all branches and the one we want
+# then reset hard to get to the desired commit id
+foreach ($remoteBranch in . git branch -r) {
+  . git checkout $remoteBranch.Trim().Replace("origin/", "") 2>&1 | write-host
+
+}  
+. git checkout $localBranch 2>&1 | write-host 
+. git reset --hard $commitId 2>&1 | write-host
 
 # Call GitVersion.exe
 $gvPath = Get-PathToGitVersionExe
 Write-Output "Path to GitVersion.exe = $gvPath"
-Invoke-Tool -Path $gvPath -Arguments "`"$loc`" /output buildserver /nofetch /b $branch /updateAssemblyInfo $updateAssms $additionalArguments"
+Invoke-Tool -Path $gvPath -Arguments "`"$loc`" /output buildserver /nofetch /updateAssemblyInfo $updateAssms $additionalArguments"
 
 
