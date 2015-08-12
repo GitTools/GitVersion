845cf33e4b1791a638594a4ef542090b19af9ce8
Oren Novotny <oren@novotny.org>
2015-08-12 08:23:02 -0400

    Use Shouldy

--- src/GitVersionCore.Tests/BuildServers/VsoAgentTests.cs
+++ src/GitVersionCore.Tests/BuildServers/VsoAgentTests.cs
@@ -1,5 +1,6 @@
 ﻿using GitVersion;
 using NUnit.Framework;
+using Shouldly;
 
 [TestFixture]
 public class VsoAgentTests
@@ -11,7 +12,7 @@ public class VsoAgentTests
         var vsVersion = versionBuilder.GenerateSetVersionMessage("0.0.0-Unstable4");
         //  Assert.AreEqual("##vso[task.setvariable variable=GitBuildNumber;]0.0.0-Unstable4", vsVersion);
 
-        Assert.Null(vsVersion);
+        vsVersion.ShouldBe(null);
     }
 
     [Test]
@@ -19,8 +20,8 @@ public class VsoAgentTests
     {
         var versionBuilder = new VsoAgent();
         var vsVersion = versionBuilder.GenerateSetParameterMessage("Foo", "0.8.0-unstable568 Branch:'develop' Sha:'ee69bff1087ebc95c6b43aa2124bd58f5722e0cb'");
-        Assert.AreEqual("##vso[task.setvariable variable=GitVersion.Foo;]0.8.0-unstable568 Branch:'develop' Sha:'ee69bff1087ebc95c6b43aa2124bd58f5722e0cb'", vsVersion[0]);
-        
+
+        vsVersion[0].ShouldBe("##vso[task.setvariable variable=GitVersion.Foo;]0.8.0-unstable568 Branch:'develop' Sha:'ee69bff1087ebc95c6b43aa2124bd58f5722e0cb'");
     }
 
 }
