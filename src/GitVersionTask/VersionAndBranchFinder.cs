305dd58365cf44e4c74ebb998be95f5ba51b3cc8
Simon Cropp <simon.cropp@gmail.com>
2015-08-20 17:41:24 +1000

    fix version cache key in msbuild task
    fixes #488

--- src/GitVersionTask/VersionAndBranchFinder.cs
+++ src/GitVersionTask/VersionAndBranchFinder.cs
@@ -27,17 +27,20 @@ public static class VersionAndBranchFinder
         var gitDir = GitDirFinder.TreeWalkForDotGitDir(directory);
         using (var repo = RepositoryLoader.GetRepo(gitDir))
         {
-            var ticks = DirectoryDateFinder.GetLastDirectoryWrite(directory);
-            var key = string.Format("{0}:{1}:{2}", repo.Head.CanonicalName, repo.Head.Tip.Sha, ticks);
+            var ticks = DirectoryDateFinder.GetLastDirectoryWrite(gitDir);
+            var key = string.Format("{0}:{1}:{2}",gitDir, repo.Head.CanonicalName, repo.Head.Tip.Sha);
 
+            Logger.WriteInfo("CacheKey: " + key );
             CachedVersion result;
             if (versionCacheVersions.TryGetValue(key, out result))
             {
                 if (result.Timestamp != ticks)
                 {
-                    Logger.WriteInfo("Change detected. flushing cache.");
+                    Logger.WriteInfo(string.Format("Change detected. Flushing cache. OldTimeStamp: {0}. NewTimeStamp: {1}", result.Timestamp, ticks));
                     result.VersionVariables = ExecuteCore.ExecuteGitVersion(fileSystem, null, null, authentication, null, noFetch, directory, null);
+                    result.Timestamp = ticks;
                 }
+                Logger.WriteInfo("Returning version from cache");
                 return result.VersionVariables;
             }
             Logger.WriteInfo("Version not in cache. Calculating version.");
