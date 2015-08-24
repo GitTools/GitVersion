8be67b436b701d4262ca2e0edc8cc6867f965c19
Marijn van der Zee <marijn@serraict.com>
2015-08-24 18:09:00 +0200

    Remove build server options that are not used

--- src/GitVersionExe/Options/InjectBuildServerOptions.cs
+++ src/GitVersionExe/Options/InjectBuildServerOptions.cs
@@ -8,23 +8,12 @@
         HelpText = "Inject gitversion variables as environment variables in your build server job.")]
     class InjectBuildServerOptions : LoggingOptions
     {
-        [Option(HelpText = "Autodetect the build server, defaults to true.", Default = true)]
-        public bool AutoDetect { get; set; }
-
-        [Option(HelpText = "The name of the buildserver to use in case auto-detect is false. " +
-                           "One of TeamCity, AppVeyor, ContinuaCi, MyGet, VsoBuild, Jenkins.")]
-        public string BuildServerName { get; set; }
-
-
         [Usage(ApplicationAlias = "GitVersion")]
         public static IEnumerable<Example> Examples
         {
             get
             {
                 yield return new Example("Normal scenario, will detect build server automatically", new InjectBuildServerOptions());
-                yield return new Example("Specific build server, will run the specified build server integration", 
-                      UnParserSettings.WithGroupSwitchesOnly(),
-                    new InjectBuildServerOptions { AutoDetect = false, BuildServerName = "Jenkins" });
             }
         }
     }
