65cff08fb33b651d4ade87c8db52c15453ddb3a5
Ronny <ronny@intellisearch.com>
2015-10-11 16:21:32 +0200

    Added scenario for branches with slashes in the name.

--- src/GitVersionCore.Tests/IntegrationTests/OtherBranchScenarios.cs
+++ src/GitVersionCore.Tests/IntegrationTests/OtherBranchScenarios.cs
@@ -5,18 +5,32 @@ using NUnit.Framework;
 [TestFixture]
 public class OtherBranchScenarios
 {
-    [Test]
-    public void CanTakeVersionFromReleaseBranch()
-    {
-        using (var fixture = new EmptyRepositoryFixture(new Config()))
-        {
-            const string TaggedVersion = "1.0.3";
-            fixture.Repository.MakeATaggedCommit(TaggedVersion);
-            fixture.Repository.MakeCommits(5);
-            fixture.Repository.CreateBranch("alpha-2.0.0");
-            fixture.Repository.Checkout("alpha-2.0.0");
+	[Test]
+	public void CanTakeVersionFromReleaseBranch()
+	{
+		using (var fixture = new EmptyRepositoryFixture(new Config()))
+		{
+			const string TaggedVersion = "1.0.3";
+			fixture.Repository.MakeATaggedCommit(TaggedVersion);
+			fixture.Repository.MakeCommits(5);
+			fixture.Repository.CreateBranch("alpha-2.0.0");
+			fixture.Repository.Checkout("alpha-2.0.0");
 
-            fixture.AssertFullSemver("2.0.0-alpha.1+0");
-        }
-    }
+			fixture.AssertFullSemver("2.0.0-alpha.1+0");
+		}
+	}
+	[Test]
+	public void BranchesWithIllegalCharsShouldNotBeUsedInVersionNames()
+	{
+		using (var fixture = new EmptyRepositoryFixture(new Config()))
+		{
+			const string TaggedVersion = "1.0.3";
+			fixture.Repository.MakeATaggedCommit(TaggedVersion);
+			fixture.Repository.MakeCommits(5);
+			fixture.Repository.CreateBranch("issue/m/github-569");
+			fixture.Repository.Checkout("issue/m/github-569");
+
+			fixture.AssertFullSemver("1.0.4-issue-m-github-569.1+5");
+		}
+	}
 }
