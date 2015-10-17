e91b74194aa22e383175d50ccdcef47fbbf42c15
Jake Ginnivan <jake@ginnivan.net>
2015-10-17 16:00:41 +0800

    Fix some r# errors

--- src/GitVersionCore.Tests/TestableVersionVariables.cs
+++ src/GitVersionCore.Tests/TestableVersionVariables.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GitVersionCore.Tests
+﻿namespace GitVersionCore.Tests
 {
     using GitVersion;
+
     class TestableVersionVariables : VersionVariables
     {
-        public TestableVersionVariables(string major = "", string minor = "", string patch = "", string buildMetaData = "", string buildMetaDataPadded = "", string fullBuildMetaData = "", string branchName = "", string sha = "", string majorMinorPatch = "", string semVer = "", string legacySemVer = "", string legacySemVerPadded = "", string fullSemVer = "", string assemblySemVer = "", string preReleaseTag = "", string preReleaseTagWithDash = "", string informationalVersion = "", string commitDate = "") : base(major, minor, patch, buildMetaData, buildMetaDataPadded, fullBuildMetaData, branchName, sha, majorMinorPatch, semVer, legacySemVer, legacySemVerPadded, fullSemVer, assemblySemVer, preReleaseTag, preReleaseTagWithDash, informationalVersion, commitDate)
+        public TestableVersionVariables(string major = "", string minor = "", string patch = "", string buildMetaData = "", string buildMetaDataPadded = "", string fullBuildMetaData = "", string branchName = "", string sha = "", string majorMinorPatch = "", string semVer = "", string legacySemVer = "", string legacySemVerPadded = "", string fullSemVer = "", string assemblySemVer = "", string preReleaseTag = "", string preReleaseTagWithDash = "", string informationalVersion = "", string commitDate = "", string nugetVersion = "", string nugetVersionV2 = null) : base(major, minor, patch, buildMetaData, buildMetaDataPadded, fullBuildMetaData, branchName, sha, majorMinorPatch, semVer, legacySemVer, legacySemVerPadded, fullSemVer, assemblySemVer, preReleaseTag, preReleaseTagWithDash, informationalVersion, commitDate, nugetVersion, nugetVersionV2)
         {
         }
     }
--- src/GitVersionCore/BuildServers/VsoAgent.cs
+++ src/GitVersionCore/BuildServers/VsoAgent.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Linq;
-    using System.Text;
     using System.Text.RegularExpressions;
 
     public class VsoAgent : BuildServerBase
