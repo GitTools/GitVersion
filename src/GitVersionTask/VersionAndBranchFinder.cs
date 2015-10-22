c20abdb7a70625f1505fcd6bad2b4fe198b4e4be
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-10-22 15:24:43 +0200

    Do scoped `IndentLog` instead of `WriteInfo` before and after.

--- src/GitVersionTask/VersionAndBranchFinder.cs
+++ src/GitVersionTask/VersionAndBranchFinder.cs
@@ -52,61 +52,66 @@ public static class VersionAndBranchFinder
 
     static VersionVariables LoadVersionVariablesFromDiskCache(string key, string directory, Authentication authentication, bool noFetch, IFileSystem fileSystem, string gitDir, long ticks)
     {
-        string cacheKey;
-        using (var sha1 = SHA1.Create())
+        using (Logger.IndentLog("Loading version variables from disk cache"))
         {
-            // Make a shorter key by hashing, to avoid having to long cache filename.
-            cacheKey = BitConverter.ToString(sha1.ComputeHash(Encoding.UTF8.GetBytes(key))).Replace("-", "");
-        }
-
-        var cacheDir = Path.Combine(gitDir, "gitversion_cache");
-        if (!fileSystem.Exists(cacheDir))
-        {
-            Logger.WriteInfo("Creating directory for cache in " + cacheDir);
-            fileSystem.CreateDirectory(cacheDir);
-        }
-
-        var cacheFileName = Path.Combine(cacheDir, cacheKey);
-        VersionVariables vv = null;
-        if (fileSystem.Exists(cacheFileName))
-        {
-            try
+            string cacheKey;
+            using (var sha1 = SHA1.Create())
             {
-                using (var stream = fileSystem.OpenRead(cacheFileName))
-                using (var sr = new StreamReader(stream))
-                {
-                    Logger.WriteInfo("Deserializing version variables from cache file " + cacheFileName);
-                    vv = VersionVariables.FromDictionary(new Deserializer().Deserialize<Dictionary<string, string>>(sr));
-                    Logger.WriteInfo("Deserializing cache file done.");
-                }
+                // Make a shorter key by hashing, to avoid having to long cache filename.
+                cacheKey = BitConverter.ToString(sha1.ComputeHash(Encoding.UTF8.GetBytes(key))).Replace("-", "");
             }
-            catch (Exception ex)
+
+            var cacheDir = Path.Combine(gitDir, "gitversion_cache");
+            if (!fileSystem.Exists(cacheDir))
             {
-                Logger.WriteWarning("Unable to read cache file " + cacheFileName + ", deleting it.");
-                Logger.WriteInfo(ex.ToString());
+                Logger.WriteInfo("Creating directory for cache in " + cacheDir);
+                fileSystem.CreateDirectory(cacheDir);
+            }
+
+            var cacheFileName = Path.Combine(cacheDir, cacheKey);
+            VersionVariables vv = null;
+            if (fileSystem.Exists(cacheFileName))
+            {
                 try
                 {
-                    fileSystem.Delete(cacheFileName);
+                    using (var stream = fileSystem.OpenRead(cacheFileName))
+                    using (var sr = new StreamReader(stream))
+                    {
+                        using (Logger.IndentLog("Deserializing version variables from cache file " + cacheFileName))
+                        {
+                            vv = VersionVariables.FromDictionary(new Deserializer().Deserialize<Dictionary<string, string>>(sr));
+                        }
+                    }
                 }
-                catch (Exception deleteEx)
+                catch (Exception ex)
                 {
-                    Logger.WriteWarning(string.Format("Unable delete corrupted version cache file {0}. Got {1} exception.", cacheFileName, deleteEx.GetType().FullName));
+                    Logger.WriteWarning("Unable to read cache file " + cacheFileName + ", deleting it.");
+                    Logger.WriteInfo(ex.ToString());
+                    try
+                    {
+                        fileSystem.Delete(cacheFileName);
+                    }
+                    catch (Exception deleteEx)
+                    {
+                        Logger.WriteWarning(string.Format("Unable delete corrupted version cache file {0}. Got {1} exception.", cacheFileName, deleteEx.GetType().FullName));
+                    }
                 }
             }
-        }
-        if (vv == null)
-        {
-            vv = ExecuteCore.ExecuteGitVersion(fileSystem, null, null, authentication, null, noFetch, directory, null);
-
-            using (var stream = fileSystem.OpenWrite(cacheFileName))
-            using (var sw = new StreamWriter(stream))
+            if (vv == null)
             {
-                Logger.WriteInfo("Storing version variables to cache file " + cacheFileName);
-                var serializer = new Serializer();
-                serializer.Serialize(sw, vv.ToDictionary(x => x.Key, x => x.Value));
-                Logger.WriteInfo("Serialization of cache file done.");
+                vv = ExecuteCore.ExecuteGitVersion(fileSystem, null, null, authentication, null, noFetch, directory, null);
+
+                using (var stream = fileSystem.OpenWrite(cacheFileName))
+                using (var sw = new StreamWriter(stream))
+                {
+                    using (Logger.IndentLog("Storing version variables to cache file " + cacheFileName))
+                    {
+                        var serializer = new Serializer();
+                        serializer.Serialize(sw, vv.ToDictionary(x => x.Key, x => x.Value));
+                    }
+                }
             }
+            return vv;
         }
-        return vv;
     }
 }
