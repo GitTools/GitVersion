cd4c47441928899d08a4956a6422166b658688cd
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-11-10 14:02:09 +0100

    R# cleanup

--- src/GitVersionCore/Helpers/FileSystem.cs
+++ src/GitVersionCore/Helpers/FileSystem.cs
@@ -10,45 +10,54 @@ namespace GitVersion.Helpers
             File.Copy(from, to, overwrite);
         }
 
+
         public void Move(string @from, string to)
         {
             File.Move(from, to);
         }
+
 
         public bool Exists(string file)
         {
             return File.Exists(file);
         }
 
+
         public void Delete(string path)
         {
             File.Delete(path);
         }
+
 
         public string ReadAllText(string path)
         {
             return File.ReadAllText(path);
         }
 
+
         public void WriteAllText(string file, string fileContents)
         {
             File.WriteAllText(file, fileContents);
         }
+
 
         public IEnumerable<string> DirectoryGetFiles(string directory, string searchPattern, SearchOption searchOption)
         {
             return Directory.GetFiles(directory, searchPattern, searchOption);
         }
 
+
         public Stream OpenWrite(string path)
         {
             return File.OpenWrite(path);
         }
 
+
         public Stream OpenRead(string path)
         {
             return File.OpenRead(path);
         }
+
 
         public void CreateDirectory(string path)
         {
