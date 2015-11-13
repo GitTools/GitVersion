ed5edd648bad13ed0cae6e77b6a58b98c2d6ccd4
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-11-13 15:21:43 +0100

    Make the `VersionAndBranchFinder.VersionCacheVersions` dictionary internal and internals visible to GitVersionTask.Tests so we can reset the in-memory cache for testing. Not very elegant, but that's `static` for you. :-/

--- src/GitVersionTask/AssemblyInfo.cs
+++ src/GitVersionTask/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("GitVersionTask")]
 [assembly: AssemblyProduct("GitVersionTask")]
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
+[assembly: InternalsVisibleTo("GitVersionTask.Tests")]
