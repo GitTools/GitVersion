b7e76d45d4d3dabaa919c3378e79a2e24ef8b3f8
GaveUp <gaveup@gmx.net>
2016-05-23 08:49:29 -0500

    Add support for TFS 2013.

--- src/GitVersionCore/BuildServers/VsoAgent.cs
+++ src/GitVersionCore/BuildServers/VsoAgent.cs
@@ -1,51 +1,58 @@
 ﻿namespace GitVersion
 {
-    using System;
-    using System.Linq;
-    using System.Text.RegularExpressions;
+	using System;
+	using System.Linq;
+	using System.Text.RegularExpressions;
 
-    public class VsoAgent : BuildServerBase
-    {
-        public override bool CanApplyToCurrentContext()
-        {
-            return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("TF_BUILD"));
-        }
+	public class VsoAgent : BuildServerBase
+	{
+		public override bool CanApplyToCurrentContext()
+		{
+			return !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("TF_BUILD"));
+		}
 
-        public override string[] GenerateSetParameterMessage(string name, string value)
-        {
-            return new[]
-            {
-                string.Format("##vso[task.setvariable variable=GitVersion.{0};]{1}", name, value)
-            };
-        }
+		public override string[] GenerateSetParameterMessage(string name, string value)
+		{
+			return new[]
+			{
+				string.Format("##vso[task.setvariable variable=GitVersion.{0};]{1}", name, value)
+			};
+		}
 
-        public override string GetCurrentBranch(bool usingDynamicRepos)
-        {
-            return Environment.GetEnvironmentVariable("BUILD_SOURCEBRANCH");
-        }
+		public override string GetCurrentBranch(bool usingDynamicRepos)
+		{
+			if (Environment.GetEnvironmentVariable("BUILD_SOURCEBRANCH") != null)
+				return Environment.GetEnvironmentVariable("BUILD_SOURCEBRANCH");
 
-        public override bool PreventFetch()
-        {
-            return true;
-        }
+			//TFS2013 Support.  Must parse branch from TF_BUILD_SOURCEGETVERSION.
+			if (Environment.GetEnvironmentVariable("TF_BUILD_SOURCEGETVERSION") != null)
+				return Regex.Replace(Environment.GetEnvironmentVariable("TF_BUILD_SOURCEGETVERSION") ?? "", @".*(refs\/heads\/.*?):.*", "$1");
 
-        public override string GenerateSetVersionMessage(VersionVariables variables)
-        {
-            // For VSO, we'll get the Build Number and insert GitVersion variables where
-            // specified
-            var buildNum = Environment.GetEnvironmentVariable("BUILD_BUILDNUMBER");
+			return null;
+		}
 
-            var newBuildNum = variables.Aggregate(buildNum, (current, kvp) =>
-                current.RegexReplace(string.Format(@"\$\(GITVERSION_{0}\)", kvp.Key), kvp.Value ?? string.Empty, RegexOptions.IgnoreCase));
+		public override bool PreventFetch()
+		{
+			return true;
+		}
 
-            // If no variable substitution has happened, use FullSemVer
-            if (buildNum == newBuildNum)
-            {
-                var buildNumber = variables.FullSemVer.EndsWith("+0") ? variables.FullSemVer.Substring(0, variables.FullSemVer.Length - 2) : variables.FullSemVer;
-                return string.Format("##vso[build.updatebuildnumber]{0}", buildNumber);
-            }
+		public override string GenerateSetVersionMessage(VersionVariables variables)
+		{
+			// For VSO, we'll get the Build Number and insert GitVersion variables where
+			// specified
+			var buildNum = Environment.GetEnvironmentVariable("BUILD_BUILDNUMBER") ?? Environment.GetEnvironmentVariable("TF_BUILD_BUILDNUMBER");
 
-            return string.Format("##vso[build.updatebuildnumber]{0}", newBuildNum);
-        }
-    }
+			var newBuildNum = variables.Aggregate(buildNum, (current, kvp) =>
+					current.RegexReplace(string.Format(@"\$\(GITVERSION_{0}\)", kvp.Key), kvp.Value ?? string.Empty, RegexOptions.IgnoreCase));
+
+			// If no variable substitution has happened, use FullSemVer
+			if (buildNum == newBuildNum)
+			{
+				var buildNumber = variables.FullSemVer.EndsWith("+0") ? variables.FullSemVer.Substring(0, variables.FullSemVer.Length - 2) : variables.FullSemVer;
+				return string.Format("##vso[build.updatebuildnumber]{0}", buildNumber);
+			}
+
+			return string.Format("##vso[build.updatebuildnumber]{0}", newBuildNum);
+		}
+	}
 }
