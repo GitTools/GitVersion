335bf474f37abbc9e9c28210e1a6e80ef0a10948
Ruben Mamo <RubenMamo@kpmg.com.mt>
2016-05-26 16:53:45 +0200

    Added additional logging to help with TravisCI debugging

--- src/GitVersionCore/ExecuteCore.cs
+++ src/GitVersionCore/ExecuteCore.cs
@@ -43,7 +43,8 @@ namespace GitVersion
             //    },
             //    Directory = workingDirectory
             //});
-            Logger.WriteInfo(string.Format("Project root is: " + projectRoot));
+            Logger.WriteInfo(string.Format("Project root is: {0}", projectRoot));
+            Logger.WriteInfo(string.Format("DotGit directory is: {0}", dotGitDirectory));
             if (string.IsNullOrEmpty(dotGitDirectory) || string.IsNullOrEmpty(projectRoot))
             {
                 // TODO Link to wiki article
