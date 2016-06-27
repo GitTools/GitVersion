45590b5202e2658c25da4524f07efd91519474ca
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2016-06-27 12:56:38 +0200

    Improve the language discovery heuristics of AssemblyInfoBuilder
    Improve how the AssemblyInfoBuilder discovers what language is being compiled
    by not looking for an `AssemblyInfo.*` file, but instead finding the first
    *.vb og *.cs file and deducing the langauge from there.
    
    Also adds a few more guards and tests for AssemblyInfoBuilder to make it more
    robust and easier to understand if it breaks.

--- src/GitVersionTask/AssemblyInfoBuilder/AssemblyInfoBuilder.cs
+++ src/GitVersionTask/AssemblyInfoBuilder/AssemblyInfoBuilder.cs
@@ -1,37 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using GitTools;
 using GitVersion;
 using Microsoft.Build.Framework;
-using System.IO;
-using GitTools;
 
 public abstract class AssemblyInfoBuilder
 {
-    private static readonly Dictionary<string, Type> assemblyInfoBuilders = new Dictionary<string, Type>()
+    private static readonly Dictionary<string, Type> assemblyInfoBuilders = new Dictionary<string, Type>
     {
-        { ".cs", typeof(CSharpAssemblyInfoBuilder) },
-        { ".vb", typeof(VisualBasicAssemblyInfoBuilder) }
+        {".cs", typeof(CSharpAssemblyInfoBuilder)},
+        {".vb", typeof(VisualBasicAssemblyInfoBuilder)}
         // TODO: Missing FSharpAssemblyInfoBuilder
     };
 
+    public abstract string AssemblyInfoExtension { get; }
+
     public static AssemblyInfoBuilder GetAssemblyInfoBuilder(IEnumerable<ITaskItem> compileFiles)
     {
+        if (compileFiles == null)
+        {
+            throw new ArgumentNullException("compileFiles");
+        }
+
         Type builderType;
 
-        var assemblyInfoExtension = compileFiles.Select(x => x.ItemSpec)
-            .Where(compileFile => compileFile.Contains("AssemblyInfo"))
-            .Select(Path.GetExtension).FirstOrDefault();
+        var assemblyInfoExtension = compileFiles
+            .Select(x => x.ItemSpec)
+            .Select(Path.GetExtension)
+            // TODO: While it works, this seems like a bad way to discover the language is being compiled. @asbjornu
+            .FirstOrDefault(extension => assemblyInfoBuilders.ContainsKey(extension.ToLowerInvariant()));
 
-        if (assemblyInfoBuilders.TryGetValue(assemblyInfoExtension, out builderType))
+        if (assemblyInfoExtension != null && assemblyInfoBuilders.TryGetValue(assemblyInfoExtension, out builderType))
         {
             return Activator.CreateInstance(builderType) as AssemblyInfoBuilder;
         }
 
         throw new WarningException("Unable to determine which AssemblyBuilder required to generate GitVersion assembly information");
     }
-
-    public abstract string AssemblyInfoExtension { get; }
 
     public abstract string GetAssemblyInfoText(VersionVariables vars, string rootNamespace);
 }
