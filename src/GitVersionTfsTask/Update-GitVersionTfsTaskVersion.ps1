a3c2adf6a2651619a0ef4e959deae5bba93a1d7c
Pascal Berger <pascal.berger@ioz.ch>
2016-07-14 21:38:58 +0200

    Throw exception if script for patching TFS task version is called with invalid arguments.

--- src/GitVersionTfsTask/Update-GitVersionTfsTaskVersion.ps1
+++ src/GitVersionTfsTask/Update-GitVersionTfsTaskVersion.ps1
@@ -5,6 +5,24 @@ param (
 [string] $patch
 )
 
+if ([string]::IsNullOrWhiteSpace($filePath)) {
+    throw "File path needs to be provided."
+}
+
+if ([string]::IsNullOrWhiteSpace($major)) {
+    throw "Major version number needs to be provided."
+}
+
+if ([string]::IsNullOrWhiteSpace($minor)) {
+    throw "Minor version number needs to be provided."
+}
+
+if ([string]::IsNullOrWhiteSpace($patch)) {
+    throw "Patch level needs to be provided."
+}
+
+Write-Host "Set version in '$filePath' to $major.$minor.$patch"
+
 # Get the task.json as a powershell object
 $task = Get-Content -Raw -Path $filePath | ConvertFrom-Json
 
