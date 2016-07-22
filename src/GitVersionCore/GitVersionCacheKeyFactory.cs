fa64cef1fc4db2c051da6395f0a2f04a46483bd1
Tim Uy <tim@loqu8.com>
2016-07-22 07:50:05 -0700

    suggestions from DanielRose + make sure to close file after reading using ReadAllText

--- src/GitVersionCore/GitVersionCacheKeyFactory.cs
+++ src/GitVersionCore/GitVersionCacheKeyFactory.cs
@@ -104,17 +104,11 @@
                         // Perform whatever action is required in your scenario.
                         System.IO.FileInfo fi = new System.IO.FileInfo(file);
                         result.Add(fi.Name);
-
-                        var reader = fi.OpenText();
-                        result.Add(reader.ReadToEnd());
-
+                        result.Add(File.ReadAllText(file));
                         //Logger.WriteInfo(string.Format("{0}: {1}, {2}", fi.Name, fi.Length, fi.CreationTime));
                     }
-                    catch (System.IO.FileNotFoundException e)
+                    catch (IOException e)
                     {
-                        // If file was deleted by a separate application
-                        //  or thread since the call to TraverseTree()
-                        // then just continue.
                         Logger.WriteError(e.Message);
                         continue;
                     }
