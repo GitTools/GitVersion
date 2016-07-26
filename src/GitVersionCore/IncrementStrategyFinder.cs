9756b9dbb94868608f89fe73acdf8e24d0e8d8c6
MikeLarah <mike.larah@endjin.com>
2016-07-26 12:29:13 +0100

    Exclude base commit when getting intermediate commits. #974

--- src/GitVersionCore/IncrementStrategyFinder.cs
+++ src/GitVersionCore/IncrementStrategyFinder.cs
@@ -97,11 +97,11 @@
             var found = false;
             foreach (var commit in intermediateCommitCache)
             {
-                if (commit.Sha == baseCommit.Sha)
-                    found = true;
-
                 if (found)
                     yield return commit;
+
+                if (commit.Sha == baseCommit.Sha)
+                    found = true;
             }
         }
 
