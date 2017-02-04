c980b5c5dcaafc423b15b8705f055dfe83e88453
Cyprien Autexier <Cyprien Autexier>
2017-02-04 12:49:12 +0100

    remove workaround as it should not be required.

--- src/GitVersionTfsTask/Tasks/GitVersion/GitVersion.ts
+++ src/GitVersionTfsTask/Tasks/GitVersion/GitVersion.ts
@@ -2,35 +2,6 @@ import tl = require('vsts-task-lib/task');
 import path = require('path');
 import q = require('q');
 
-var variables = [
-    "GitVersion_Major",
-    "GitVersion_Minor",
-    "GitVersion_Patch",
-    "GitVersion_PreReleaseTag",
-    "GitVersion_PreReleaseTagWithDash",
-    "GitVersion_PreReleaseLabel",
-    "GitVersion_PreReleaseNumber",
-    "GitVersion_BuildMetaData",
-    "GitVersion_BuildMetaDataPadded",
-    "GitVersion_FullBuildMetaData",
-    "GitVersion_MajorMinorPatch",
-    "GitVersion_SemVer",
-    "GitVersion_LegacySemVer",
-    "GitVersion_LegacySemVerPadded",
-    "GitVersion_AssemblySemVer",
-    "GitVersion_FullSemVer",
-    "GitVersion_InformationalVersion",
-    "GitVersion_BranchName",
-    "GitVersion_Sha",
-    "GitVersion_NuGetVersionV2",
-    "GitVersion_NuGetVersion",
-    "GitVersion_NuGetPreReleaseTagV2",
-    "GitVersion_NuGetPreReleaseTag",
-    "GitVersion_CommitsSinceVersionSource",
-    "GitVersion_CommitsSinceVersionSourcePadded",
-    "GitVersion_CommitDate"
-]
-
 var updateAssemblyInfo = tl.getBoolInput('updateAssemblyInfo');
 var updateAssemblyInfoFilename = tl.getInput('updateAssemblyInfoFilename');
 var additionalArguments = tl.getInput('additionalArguments');
@@ -75,13 +46,6 @@ if (!gitVersionPath) {
         if (result) {
             tl.setResult(tl.TaskResult.Failed, "An error occured during GitVersion execution")
         } else {
-            // workaround gitversion to make sure variable are available in all vsts build contexts
-            for(var i = 0; i < variables.length; i++) {
-                var variableName = variables[i];
-
-                tl.setVariable(variableName, tl.getVariable(variableName));
-            }
-
             tl.setResult(tl.TaskResult.Succeeded, "GitVersion executed successfully")
         }
     }
