d6ece816f124048183330ad295b48168be0c52e5
Cyprien Autexier <Cyprien Autexier>
2017-02-04 15:21:40 +0100

    CreateVsix update

--- src/GitVersionTfsTask/Create-Vsix.ps1
+++ src/GitVersionTfsTask/Create-Vsix.ps1
@@ -5,7 +5,10 @@ param (
 Push-Location $taskFolder
 Try
 {
-    & tfx extension create --manifest-globs manifest.json
+    & npm install
+    & npm run clean
+    & npm run build
+    & npm run package -- --version "$Env:GitVersion_SemVer" --noversiontransform
 }
 Finally
 {
