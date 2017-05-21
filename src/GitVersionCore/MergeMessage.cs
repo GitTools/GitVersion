bd16d241fbbdb54a81e98f728b350da293576fe2
Jake Ginnivan <jake@ginnivan.net>
2017-05-21 12:56:41 +0800

    Introduced MergeMessage class
    This class parses the merge message and exposes all the useful information, like is it a pull request, what was the source and target branch etc.

---
+++ src/GitVersionCore/MergeMessage.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace GitVersion
+{
+    class MergeMessage
+    {
+        static Regex parseMergeMessage = new Regex(
+            @"^Merge (branch|tag) '(?<Branch>[^']*)'",
+            RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        static Regex parseGitHubPullMergeMessage = new Regex(
+            @"^Merge pull request #(?<PullRequestNumber>\d*) (from|in) (?<Source>.*)",
+            RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        static Regex smartGitMergeMessage = new Regex(
+            @"^Finish (?<Branch>.*)",
+            RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private string mergeMessage;
+        private Config config;
+
+        public MergeMessage(string mergeMessage, Config config)
+        {
+            this.mergeMessage = mergeMessage;
+            this.config = config;
+
+            var lastIndexOf = mergeMessage.LastIndexOf("into", StringComparison.OrdinalIgnoreCase);
+            if (lastIndexOf != -1)
+            {
+                // If we have into in the merge message the rest should be the target branch
+                TargetBranch = mergeMessage.Substring(lastIndexOf + 5);
+            }
+
+            MergedBranch = ParseBranch();
+            
+            // Remove remotes and branch prefixes like release/ feature/ hotfix/ etc
+            var toMatch = Regex.Replace(MergedBranch, @"^(\w+[-/])*", "", RegexOptions.IgnoreCase);
+            toMatch = Regex.Replace(toMatch, $"^{config.TagPrefix}", "");
+            // We don't match if the version is likely an ip (i.e starts with http://)
+            var versionMatch = new Regex(@"^(?<!://)\d+\.\d+(\.*\d+)*");
+            var version = versionMatch.Match(toMatch);
+
+            if (version.Success)
+            {
+                SemanticVersion val;
+                if (SemanticVersion.TryParse(version.Value, config.TagPrefix, out val))
+                {
+                    Version = val;
+                }
+            }
+        }
+
+        private string ParseBranch()
+        {
+            var match = parseMergeMessage.Match(mergeMessage);
+            if (match.Success)
+            {
+                return match.Groups["Branch"].Value;
+            }
+
+            match = smartGitMergeMessage.Match(mergeMessage);
+            if (match.Success)
+            {
+                return match.Groups["Branch"].Value;
+            }
+
+            match = parseGitHubPullMergeMessage.Match(mergeMessage);
+            if (match.Success)
+            {
+                IsMergedPullRequest = true;
+                int pullNumber;
+                if (int.TryParse(match.Groups["PullRequestNumber"].Value, out pullNumber))
+                {
+                    PullRequestNumber = pullNumber;
+                }
+                var from = match.Groups["Source"].Value;
+                // We could remove/separate the remote name at this point?
+                return from;
+            }
+
+            return "";
+        }
+
+        public string TargetBranch { get; }
+        public string MergedBranch { get; }
+        public bool IsMergedPullRequest { get; private set; }
+        public int? PullRequestNumber { get; private set; }
+        public SemanticVersion Version { get; }
+    }
+}
--- src/GitVersionCore/VersionCalculation/BaseVersionCalculators/MergeMessageBaseVersionStrategy.cs
+++ src/GitVersionCore/VersionCalculation/BaseVersionCalculators/MergeMessageBaseVersionStrategy.cs
@@ -1,9 +1,7 @@
 ﻿namespace GitVersion.VersionCalculation.BaseVersionCalculators
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text.RegularExpressions;
     using LibGit2Sharp;
 
     /// <summary>
@@ -21,7 +19,7 @@
                 .SelectMany(c =>
                 {
                     SemanticVersion semanticVersion;
-                    if (TryParse(c, context.Configuration, out semanticVersion))
+                    if (TryParse(c, context, out semanticVersion))
                     {
                         var shouldIncrement = !context.Configuration.PreventIncrementForMergedBranchVersion;
                         return new[]
@@ -34,35 +32,21 @@
             return baseVersions;
         }
 
-        static bool TryParse(Commit mergeCommit, EffectiveConfiguration configuration, out SemanticVersion semanticVersion)
+        static bool TryParse(Commit mergeCommit, GitVersionContext context, out SemanticVersion semanticVersion)
         {
-            semanticVersion = Inner(mergeCommit, configuration);
+            semanticVersion = Inner(mergeCommit, context);
             return semanticVersion != null;
         }
 
-        static SemanticVersion Inner(Commit mergeCommit, EffectiveConfiguration configuration)
+        static SemanticVersion Inner(Commit mergeCommit, GitVersionContext context)
         {
             if (mergeCommit.Parents.Count() < 2)
             {
                 return null;
             }
 
-            var commitMessage = mergeCommit.Message;
-            var lastIndexOf = commitMessage.LastIndexOf("into", StringComparison.OrdinalIgnoreCase);
-            if (lastIndexOf != -1)
-                commitMessage = commitMessage.Substring(0, lastIndexOf);
-
-            //TODO: Make the version prefixes customizable
-            var possibleVersions = Regex.Matches(commitMessage, @"^.*?(([rR]elease|[hH]otfix|[aA]lpha)-|-v|/|/v|'|Finish )(?<PossibleVersions>(?<!://)\d+\.\d+(\.*\d+)*)")
-                .Cast<Match>()
-                .Select(m => m.Groups["PossibleVersions"].Value);
-
-            return possibleVersions
-                .Select(part =>
-                {
-                    SemanticVersion v;
-                    return SemanticVersion.TryParse(part, configuration.GitTagPrefix, out v) ? v : null;
-                }).FirstOrDefault(v => v != null);
+            var mergeMessage = new MergeMessage(mergeCommit.Message, context.FullConfiguration);
+            return mergeMessage.Version;
         }
     }
 }
