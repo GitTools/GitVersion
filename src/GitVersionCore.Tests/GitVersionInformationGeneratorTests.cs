9f6354559102e0b275d58e25896efd5af455ff70
Brandon Ording <bording@gmail.com>
2017-07-07 00:32:33 -0400

    Add initial GitVersionInformationGenerator implementation

---
+++ src/GitVersionCore.Tests/GitVersionInformationGeneratorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitVersion;
+using NUnit.Framework;
+
+namespace GitVersionCore.Tests
+{
+    [TestFixture]
+    public class GitVersionInformationGeneratorTests
+    {
+        [Test]
+        public void ShouldCreateFile()
+        {
+            var fileSystem = new TestFileSystem();
+            var directory = Path.GetTempPath();
+            var fileName = "GitVersionInformation.g.cs";
+            var fullPath = Path.Combine(directory, fileName);
+            var variables = VariableProvider.GetVariablesFor(SemanticVersion.Parse("1.0.0", "v"), new TestEffectiveConfiguration(), false);
+
+            var generator = new GitVersionInformationGenerator(fileName, directory, variables, fileSystem);
+
+            generator.Generate();
+
+            var fileContents = fileSystem.ReadAllText(fullPath);
+        }
+    }
+}
---
+++ src/GitVersionCore/GitVersionInformationResources/GitVersionInformationGenerator.cs
@@ -0,0 +1,51 @@
+﻿namespace GitVersion
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using Helpers;
+
+    public class GitVersionInformationGenerator
+    {
+        string fileName;
+        string directory;
+        VersionVariables variables;
+        IFileSystem fileSystem;
+
+        TemplateManager templateManager;
+
+        public GitVersionInformationGenerator(string fileName, string directory, VersionVariables variables, IFileSystem fileSystem)
+        {
+            this.fileName = fileName;
+            this.directory = directory;
+            this.variables = variables;
+            this.fileSystem = fileSystem;
+
+            templateManager = new TemplateManager(TemplateType.GitVersionInformationResources);
+        }
+
+        public void Generate()
+        {
+            var filePath = Path.Combine(directory, fileName);
+
+            string originalFileContents = null;
+
+            if (File.Exists(filePath))
+            {
+                originalFileContents = fileSystem.ReadAllText(filePath);
+            }
+
+            var template = templateManager.GetTemplateFor(fileName);
+            var addFormat = templateManager.GetAddFormatFor(Path.GetExtension(fileName));
+
+            var members = string.Join(Environment.NewLine, variables.Select(v => string.Format("    " + addFormat, v.Key, v.Value)));
+
+            var fileContents = string.Format(template, members);
+
+            if (fileContents != originalFileContents)
+            {
+                fileSystem.WriteAllText(filePath, fileContents);
+            }
+        }
+    }
+}
