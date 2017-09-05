f906d9a7e0befcc4090b1abbd130a24a7ecff966
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2017-09-05 12:11:54 +0200

    Made other exceptions inherit GitVersionException for consistency

--- src/GitVersionCore/Configuration/GitVersionConfigurationException.cs
+++ src/GitVersionCore/Configuration/GitVersionConfigurationException.cs
@@ -1,10 +1,19 @@
 namespace GitVersion
 {
     using System;
+    using System.Runtime.Serialization;
 
-    public class GitVersionConfigurationException : Exception
+    [Serializable]
+    public class GitVersionConfigurationException : GitVersionException
     {
-        public GitVersionConfigurationException(string msg) : base(msg)
+        public GitVersionConfigurationException(string msg)
+            : base(msg)
+        {
+        }
+
+
+        protected GitVersionConfigurationException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
--- src/GitVersionCore/Configuration/OldConfigurationException.cs
+++ src/GitVersionCore/Configuration/OldConfigurationException.cs
@@ -4,15 +4,16 @@ namespace GitVersion
     using System.Runtime.Serialization;
 
     [Serializable]
-    public class OldConfigurationException : Exception
+    public class OldConfigurationException : GitVersionException
     {
-        public OldConfigurationException(string message) : base(message)
+        public OldConfigurationException(string message)
+            : base(message)
         {
         }
 
-        protected OldConfigurationException(
-            SerializationInfo info,
-            StreamingContext context) : base(info, context)
+
+        protected OldConfigurationException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
