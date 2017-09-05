a636d3271c00e47759eed5e027bd5c3f929360d9
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2017-09-05 12:10:21 +0200

    Added GitVersionException to wrap LibGit2Sharp.NotFoundException when the repository might be a shallow clone

---
+++ src/GitVersionCore/GitVersionException.cs
@@ -0,0 +1,31 @@
+﻿namespace GitVersion
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class GitVersionException : ApplicationException
+    {
+        public GitVersionException()
+        {
+        }
+
+
+        public GitVersionException(string message)
+            : base(message)
+        {
+        }
+
+
+        public GitVersionException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+
+        protected GitVersionException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
