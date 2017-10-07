043ba85c12d4e826d7efa14c51169767538d091a
Brandon Ording <bording@gmail.com>
2017-10-07 18:42:09 -0400

    Update ShouldCreateFile test

--- src/GitVersionCore.Tests/GitVersionInformationGeneratorTests.cs
+++ src/GitVersionCore.Tests/GitVersionInformationGeneratorTests.cs
@@ -6,26 +6,47 @@ using System.Text;
 using System.Threading.Tasks;
 using GitVersion;
 using NUnit.Framework;
+using Shouldly;
 
 namespace GitVersionCore.Tests
 {
     [TestFixture]
     public class GitVersionInformationGeneratorTests
     {
-        [Test]
-        public void ShouldCreateFile()
+        [SetUp]
+        public void SetLoggers()
+        {
+            ShouldlyConfiguration.ShouldMatchApprovedDefaults.LocateTestMethodUsingAttribute<TestCaseAttribute>();
+        }
+
+        [TestCase("cs")]
+        [TestCase("fs")]
+        [TestCase("vb")]
+        [Category("NoMono")]
+        [Description("Won't run on Mono due to source information not being available for ShouldMatchApproved.")]
+        public void ShouldCreateFile(string fileExtension)
         {
             var fileSystem = new TestFileSystem();
             var directory = Path.GetTempPath();
-            var fileName = "GitVersionInformation.g.cs";
+            var fileName = "GitVersionInformation.g." + fileExtension;
             var fullPath = Path.Combine(directory, fileName);
-            var variables = VariableProvider.GetVariablesFor(SemanticVersion.Parse("1.0.0", "v"), new TestEffectiveConfiguration(), false);
 
+            var semanticVersion = new SemanticVersion
+            {
+                Major = 1,
+                Minor = 2,
+                Patch = 3,
+                PreReleaseTag = "unstable4",
+                BuildMetaData = new SemanticVersionBuildMetaData(5,
+                    "feature1", "commitSha", DateTimeOffset.Parse("2014-03-06 23:59:59Z"))
+            };
+
+            var variables = VariableProvider.GetVariablesFor(semanticVersion, new TestEffectiveConfiguration(), false);
             var generator = new GitVersionInformationGenerator(fileName, directory, variables, fileSystem);
 
             generator.Generate();
 
-            var fileContents = fileSystem.ReadAllText(fullPath);
+            fileSystem.ReadAllText(fullPath).ShouldMatchApproved(c => c.SubFolder(Path.Combine("Approved", fileExtension)));
         }
     }
 }
