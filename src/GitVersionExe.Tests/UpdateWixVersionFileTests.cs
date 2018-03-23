8513d23d62fd21e62470a2fd32b6fa2af6dfb7cd
Ruh Ullah Shah <ruhullahshah88@gmail.com>
2018-03-23 18:27:57 +0100

    Added Testcases

---
+++ src/GitVersionExe.Tests/UpdateWixVersionFileTests.cs
@@ -0,0 +1,102 @@
+﻿namespace GitVersionExe.Tests
+{
+    using System.IO;
+    using System.Linq;
+    using NUnit.Framework;
+
+    using GitVersion;
+    using GitTools.Testing;
+    using System.Collections.Generic;
+    using System.Xml;
+
+    [TestFixture]
+    class UpdateWixVersionFileTests
+    {
+        private string WixVersionFileName;
+
+        [SetUp]
+        public void Setup()
+        {
+            WixVersionFileName = WixVersionFileUpdater.GetWixVersionFileName();
+        }
+
+        [Test]
+        public void WixVersionFileCreationTest()
+        {
+            using (var fixture = new EmptyRepositoryFixture())
+            {
+                fixture.MakeATaggedCommit("1.2.3");
+                fixture.MakeACommit();
+
+                GitVersionHelper.ExecuteIn(fixture.RepositoryPath, arguments: " /updatewixversionfile");
+                Assert.IsTrue(File.Exists(Path.Combine(fixture.RepositoryPath, WixVersionFileName)));
+            }
+        }
+
+        [Test]
+        public void WixVersionFileVarCountTest()
+        {
+            //Make sure we have captured all the version variables by count in the Wix version file
+            using (var fixture = new EmptyRepositoryFixture())
+            {
+                fixture.MakeATaggedCommit("1.2.3");
+                fixture.MakeACommit();
+
+                var gitVersionExecutionResults = GitVersionHelper.ExecuteIn(fixture.RepositoryPath, arguments: null);
+                VersionVariables vars = gitVersionExecutionResults.OutputVariables;
+
+                GitVersionHelper.ExecuteIn(fixture.RepositoryPath, arguments: " /updatewixversionfile");
+
+                var gitVersionVarsInWix = GetGitVersionVarsInWixFile(Path.Combine(fixture.RepositoryPath, WixVersionFileName));
+                var gitVersionVars = VersionVariables.AvailableVariables;
+
+                Assert.AreEqual(gitVersionVars.Count(), gitVersionVarsInWix.Count);
+            }
+        }
+
+        [Test]
+        public void WixVersionFileContentTest()
+        {
+            using (var fixture = new EmptyRepositoryFixture())
+            {
+                fixture.MakeATaggedCommit("1.2.3");
+                fixture.MakeACommit();
+
+                var gitVersionExecutionResults = GitVersionHelper.ExecuteIn(fixture.RepositoryPath, arguments: null);
+                VersionVariables vars = gitVersionExecutionResults.OutputVariables;
+
+                GitVersionHelper.ExecuteIn(fixture.RepositoryPath, arguments: " /updatewixversionfile");
+
+                var gitVersionVarsInWix = GetGitVersionVarsInWixFile(Path.Combine(fixture.RepositoryPath, WixVersionFileName));
+                var gitVersionVars = VersionVariables.AvailableVariables;
+
+                foreach (var variable in gitVersionVars)
+                {
+                    string value = null;
+                    vars.TryGetValue(variable, out value);
+                    //Make sure the variable is present in the Wix file
+                    Assert.IsTrue(gitVersionVarsInWix.ContainsKey(variable));
+                    //Make sure the values are equal
+                    Assert.AreEqual(value, gitVersionVarsInWix[variable]);
+                }
+            }
+        }
+
+        private Dictionary<string, string> GetGitVersionVarsInWixFile(string file)
+        {
+            var gitVersionVarsInWix = new Dictionary<string, string>();
+            using (var reader = new XmlTextReader(file))
+            {
+                while (reader.Read())
+                {
+                    if (reader.Name == "define")
+                    {
+                        string[] component = reader.Value.Split('=');
+                        gitVersionVarsInWix[component[0]] = component[1].TrimStart('"').TrimEnd('"');
+                    }
+                }
+            }
+            return gitVersionVarsInWix;
+        }
+    }
+}
