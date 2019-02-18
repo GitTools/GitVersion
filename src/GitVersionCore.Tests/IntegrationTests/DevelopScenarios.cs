22686d1a2048d7c778d6b1c3f5ba09ef5bcb3356
Ruh Ullah Shah <ruhullah.shah@rohde-schwarz.com>
2019-02-18 16:25:40 +0100

    Fixed failing testcases, updated the docs to reflect the changes in the regex

--- src/GitVersionCore.Tests/IntegrationTests/DevelopScenarios.cs
+++ src/GitVersionCore.Tests/IntegrationTests/DevelopScenarios.cs
@@ -1,4 +1,4 @@
-﻿using GitTools.Testing;
+using GitTools.Testing;
 using GitVersion;
 using GitVersionCore.Tests;
 using LibGit2Sharp;
@@ -201,7 +201,7 @@ public class DevelopScenarios : TestBase
             fixture.Repository.MakeACommit();
             fixture.Repository.MergeNoFF("develop");
 
-            fixture.AssertFullSemver("1.1.0-x.1+3");
+            fixture.AssertFullSemver("1.0.1-x.1+3");
         }
     }
 }
