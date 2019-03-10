9c74cd7330fce9c398f8bfd024833a2d63aec1e8
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2019-03-11 00:02:55 +0200

    Making GitVersionTask.Tests compilable again.

--- src/GitVersionTask.Tests/GetVersionTaskTests.cs
+++ src/GitVersionTask.Tests/GetVersionTaskTests.cs
@@ -10,12 +10,12 @@ public class GetVersionTaskTests : TestBase
     [Test]
     public void OutputsShouldMatchVariableProvider()
     {
-        var taskProperties = typeof( GetVersion.Output )
+        var taskProperties = typeof(GetVersion.Output)
             .GetProperties()
-            .Select( p => p.Name );
+            .Select(p => p.Name);
 
         var variablesProperties = VersionVariables.AvailableVariables;
 
-        taskProperties.ShouldBe( variablesProperties, ignoreOrder: true );
+        taskProperties.ShouldBe(variablesProperties, ignoreOrder: true);
     }
 }
