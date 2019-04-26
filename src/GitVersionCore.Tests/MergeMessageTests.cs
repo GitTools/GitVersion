f353dd8782cfcc91bdaf8de492fdbe08d896dc4c
Kieranties <Kieran@Kieranties.com>
2019-04-26 09:14:43 +0100

    CR: Whitespace correction

--- src/GitVersionCore.Tests/MergeMessageTests.cs
+++ src/GitVersionCore.Tests/MergeMessageTests.cs
@@ -55,13 +55,13 @@ namespace GitVersionCore.Tests
 
         private static readonly object[] MergeMessages =
         {
-             new object[] { "Merge branch 'feature/one'", "feature/one", null, null },
-             new object[] { "Merge branch 'origin/feature/one'", "origin/feature/one", null, null },
-             new object[] { "Merge tag 'v4.0.0' into master", "v4.0.0", "master", new SemanticVersion(4) },
-             new object[] { "Merge tag 'V4.0.0' into master", "V4.0.0", "master", new SemanticVersion(4) },
-             new object[] { "Merge branch 'feature/4.1/one'", "feature/4.1/one", null, new SemanticVersion(4, 1) },
-             new object[] { "Merge branch 'origin/4.1/feature/one'", "origin/4.1/feature/one", null, new SemanticVersion(4, 1) },
-             new object[] { "Merge tag 'v://10.10.10.10' into master", "v://10.10.10.10", "master", null }
+            new object[] { "Merge branch 'feature/one'", "feature/one", null, null },
+            new object[] { "Merge branch 'origin/feature/one'", "origin/feature/one", null, null },
+            new object[] { "Merge tag 'v4.0.0' into master", "v4.0.0", "master", new SemanticVersion(4) },
+            new object[] { "Merge tag 'V4.0.0' into master", "V4.0.0", "master", new SemanticVersion(4) },
+            new object[] { "Merge branch 'feature/4.1/one'", "feature/4.1/one", null, new SemanticVersion(4, 1) },
+            new object[] { "Merge branch 'origin/4.1/feature/one'", "origin/4.1/feature/one", null, new SemanticVersion(4, 1) },
+            new object[] { "Merge tag 'v://10.10.10.10' into master", "v://10.10.10.10", "master", null }
          };
 
         [TestCaseSource(nameof(MergeMessages))]
@@ -85,13 +85,13 @@ namespace GitVersionCore.Tests
 
         private static readonly object[] GitHubPullPullMergeMessages =
         {
-             new object[] { "Merge pull request #1234 from feature/one", "feature/one", null, null, 1234 },
-             new object[] { "Merge pull request #1234 in feature/one", "feature/one", null, null, 1234  },
-             new object[] { "Merge pull request #1234 in v4.0.0", "v4.0.0", null, new SemanticVersion(4), 1234  },
-             new object[] { "Merge pull request #1234 from origin/feature/one", "origin/feature/one", null, null, 1234  },
-             new object[] { "Merge pull request #1234 in feature/4.1/one", "feature/4.1/one", null, new SemanticVersion(4,1), 1234  },
-             new object[] { "Merge pull request #1234 in V://10.10.10.10", "V://10.10.10.10", null, null, 1234 },
-             new object[] { "Merge pull request #1234 from feature/one into dev", "feature/one", "dev", null, 1234  }
+            new object[] { "Merge pull request #1234 from feature/one", "feature/one", null, null, 1234 },
+            new object[] { "Merge pull request #1234 in feature/one", "feature/one", null, null, 1234  },
+            new object[] { "Merge pull request #1234 in v4.0.0", "v4.0.0", null, new SemanticVersion(4), 1234  },
+            new object[] { "Merge pull request #1234 from origin/feature/one", "origin/feature/one", null, null, 1234  },
+            new object[] { "Merge pull request #1234 in feature/4.1/one", "feature/4.1/one", null, new SemanticVersion(4,1), 1234  },
+            new object[] { "Merge pull request #1234 in V://10.10.10.10", "V://10.10.10.10", null, null, 1234 },
+            new object[] { "Merge pull request #1234 from feature/one into dev", "feature/one", "dev", null, 1234  }
          };
 
         [TestCaseSource(nameof(GitHubPullPullMergeMessages))]
@@ -116,16 +116,16 @@ namespace GitVersionCore.Tests
 
         private static readonly object[] BitBucketPullMergeMessages =
         {
-             new object[] { "Merge pull request #1234 from feature/one from feature/two to dev", "feature/two", "dev", null, 1234  },
-             new object[] { "Merge pull request #1234 in feature/one from feature/two to dev", "feature/two", "dev", null, 1234 },
-             new object[] { "Merge pull request #1234 in v4.0.0 from v4.1.0 to dev", "v4.1.0", "dev", new SemanticVersion(4,1), 1234  },
-             new object[] { "Merge pull request #1234 from origin/feature/one from origin/feature/4.2/two to dev", "origin/feature/4.2/two", "dev", new SemanticVersion(4,2), 1234  },
-             new object[] { "Merge pull request #1234 in feature/4.1/one from feature/4.2/two to dev", "feature/4.2/two", "dev", new SemanticVersion(4,2), 1234  },
-             new object[] { "Merge pull request #1234 from feature/one from feature/two to master" , "feature/two", "master", null, 1234 },
-             new object[] { "Merge pull request #1234 in V4.1.0 from V://10.10.10.10 to dev", "V://10.10.10.10", "dev", null, 1234 },
-             //TODO: Investigate successful bitbucket merge messages that may be invalid
-             // Regex has double 'from/in from' section.  Is that correct?
-             new object[] { "Merge pull request #1234 from feature/one from v4.0.0 to master", "v4.0.0", "master", new SemanticVersion(4), 1234  }
+            new object[] { "Merge pull request #1234 from feature/one from feature/two to dev", "feature/two", "dev", null, 1234  },
+            new object[] { "Merge pull request #1234 in feature/one from feature/two to dev", "feature/two", "dev", null, 1234 },
+            new object[] { "Merge pull request #1234 in v4.0.0 from v4.1.0 to dev", "v4.1.0", "dev", new SemanticVersion(4,1), 1234  },
+            new object[] { "Merge pull request #1234 from origin/feature/one from origin/feature/4.2/two to dev", "origin/feature/4.2/two", "dev", new SemanticVersion(4,2), 1234  },
+            new object[] { "Merge pull request #1234 in feature/4.1/one from feature/4.2/two to dev", "feature/4.2/two", "dev", new SemanticVersion(4,2), 1234  },
+            new object[] { "Merge pull request #1234 from feature/one from feature/two to master" , "feature/two", "master", null, 1234 },
+            new object[] { "Merge pull request #1234 in V4.1.0 from V://10.10.10.10 to dev", "V://10.10.10.10", "dev", null, 1234 },
+            //TODO: Investigate successful bitbucket merge messages that may be invalid
+            // Regex has double 'from/in from' section.  Is that correct?
+            new object[] { "Merge pull request #1234 from feature/one from v4.0.0 to master", "v4.0.0", "master", new SemanticVersion(4), 1234  }
          };
 
         [TestCaseSource(nameof(BitBucketPullMergeMessages))]
@@ -151,21 +151,21 @@ namespace GitVersionCore.Tests
 
         private static readonly object[] SmartGitMergeMessages =
         {
-             new object[] { "Finish feature/one", "feature/one", null, null },
-             new object[] { "Finish origin/feature/one", "origin/feature/one", null, null },
-             new object[] { "Finish v4.0.0", "v4.0.0", null, new SemanticVersion(4) },
-             new object[] { "Finish feature/4.1/one", "feature/4.1/one", null, new SemanticVersion(4, 1) },
-             new object[] { "Finish origin/4.1/feature/one", "origin/4.1/feature/one", null, new SemanticVersion(4, 1) },
-             new object[] { "Finish V://10.10.10.10", "V://10.10.10.10", null, null },
-             new object[] { "Finish V4.0.0 into master", "V4.0.0", "master", new SemanticVersion(4) }
+            new object[] { "Finish feature/one", "feature/one", null, null },
+            new object[] { "Finish origin/feature/one", "origin/feature/one", null, null },
+            new object[] { "Finish v4.0.0", "v4.0.0", null, new SemanticVersion(4) },
+            new object[] { "Finish feature/4.1/one", "feature/4.1/one", null, new SemanticVersion(4, 1) },
+            new object[] { "Finish origin/4.1/feature/one", "origin/4.1/feature/one", null, new SemanticVersion(4, 1) },
+            new object[] { "Finish V://10.10.10.10", "V://10.10.10.10", null, null },
+            new object[] { "Finish V4.0.0 into master", "V4.0.0", "master", new SemanticVersion(4) }
          };
 
         [TestCaseSource(nameof(SmartGitMergeMessages))]
         public void ParsesSmartGitMergeMessage(
-           string message,
-           string expectedMergedBranch,
-           string expectedTargetBranch,
-           SemanticVersion expectedVersion)
+            string message,
+            string expectedMergedBranch,
+            string expectedTargetBranch,
+            SemanticVersion expectedVersion)
         {
             // Act
             var sut = new MergeMessage(message, _config);
@@ -181,21 +181,21 @@ namespace GitVersionCore.Tests
 
         private static readonly object[] RemoteTrackingMergeMessages =
         {
-             new object[] { "Merge remote-tracking branch 'feature/one' into master", "feature/one", "master", null },
-             new object[] { "Merge remote-tracking branch 'origin/feature/one' into dev", "origin/feature/one", "dev", null },
-             new object[] { "Merge remote-tracking branch 'v4.0.0' into master", "v4.0.0", "master", new SemanticVersion(4) },
-             new object[] { "Merge remote-tracking branch 'V4.0.0' into master", "V4.0.0", "master", new SemanticVersion(4) },
-             new object[] { "Merge remote-tracking branch 'feature/4.1/one' into dev", "feature/4.1/one", "dev", new SemanticVersion(4, 1) },
-             new object[] { "Merge remote-tracking branch 'origin/4.1/feature/one' into master", "origin/4.1/feature/one", "master", new SemanticVersion(4, 1) },
-             new object[] { "Merge remote-tracking branch 'v://10.10.10.10' into master", "v://10.10.10.10", "master", null }
+            new object[] { "Merge remote-tracking branch 'feature/one' into master", "feature/one", "master", null },
+            new object[] { "Merge remote-tracking branch 'origin/feature/one' into dev", "origin/feature/one", "dev", null },
+            new object[] { "Merge remote-tracking branch 'v4.0.0' into master", "v4.0.0", "master", new SemanticVersion(4) },
+            new object[] { "Merge remote-tracking branch 'V4.0.0' into master", "V4.0.0", "master", new SemanticVersion(4) },
+            new object[] { "Merge remote-tracking branch 'feature/4.1/one' into dev", "feature/4.1/one", "dev", new SemanticVersion(4, 1) },
+            new object[] { "Merge remote-tracking branch 'origin/4.1/feature/one' into master", "origin/4.1/feature/one", "master", new SemanticVersion(4, 1) },
+            new object[] { "Merge remote-tracking branch 'v://10.10.10.10' into master", "v://10.10.10.10", "master", null }
          };
 
         [TestCaseSource(nameof(RemoteTrackingMergeMessages))]
         public void ParsesRemoteTrackingMergeMessage(
-           string message,
-           string expectedMergedBranch,
-           string expectedTargetBranch,
-           SemanticVersion expectedVersion)
+            string message,
+            string expectedMergedBranch,
+            string expectedTargetBranch,
+            SemanticVersion expectedVersion)
         {
             // Act
             var sut = new MergeMessage(message, _config);
@@ -211,25 +211,25 @@ namespace GitVersionCore.Tests
 
         private static readonly object[] ParsesTfsEnglishUSMergeMessages =
         {
-             new object[] { "Merge feature/one to master", "feature/one", "master", null },
-             new object[] { "Merge v://10.10.10.10 to master", "v://10.10.10.10", "master", null },
-             new object[] { "Merge feature/one to v://10.10.10.10", "feature/one", "v://10.10.10.10", null },
-             new object[] { "Merge V4.0.0 to master", "V4.0.0", "master", new SemanticVersion(4) },
-             new object[] { "Merge feature/4.1/one to master", "feature/4.1/one", "master", new SemanticVersion(4, 1) }
+            new object[] { "Merge feature/one to master", "feature/one", "master", null },
+            new object[] { "Merge v://10.10.10.10 to master", "v://10.10.10.10", "master", null },
+            new object[] { "Merge feature/one to v://10.10.10.10", "feature/one", "v://10.10.10.10", null },
+            new object[] { "Merge V4.0.0 to master", "V4.0.0", "master", new SemanticVersion(4) },
+            new object[] { "Merge feature/4.1/one to master", "feature/4.1/one", "master", new SemanticVersion(4, 1) }
          };
 
         [TestCaseSource(nameof(ParsesTfsEnglishUSMergeMessages))]
         public void ParsesTfsEnglishUSMessage(
-          string message,
-          string expectedMergedBranch,
-          string expectedTargetBranch,
-          SemanticVersion expectedVersion)
+            string message,
+            string expectedMergedBranch,
+            string expectedTargetBranch,
+            SemanticVersion expectedVersion)
         {
             // Act
             var sut = new MergeMessage(message, _config);
 
             // Assert
-              sut.MatchDefinition.ShouldBe("TfsMergeMessageEnglishUS");
+            sut.MatchDefinition.ShouldBe("TfsMergeMessageEnglishUS");
             sut.TargetBranch.ShouldBe(expectedTargetBranch);
             sut.MergedBranch.ShouldBe(expectedMergedBranch);
             sut.IsMergedPullRequest.ShouldBeFalse();
@@ -237,7 +237,7 @@ namespace GitVersionCore.Tests
             sut.Version.ShouldBe(expectedVersion);
         }
 
-private static readonly object[] ParsesTfsGermanDEMergeMessages =
+        private static readonly object[] ParsesTfsGermanDEMergeMessages =
         {
             new object[] { "Zusammengeführter PR \"1234\": feature/one mit master mergen", "feature/one", "master", null, 1234 },
             new object[] { "Zusammengeführter PR \"1234\": v://10.10.10.10 mit master mergen", "v://10.10.10.10", "master", null, 1234 },
@@ -248,11 +248,11 @@ private static readonly object[] ParsesTfsGermanDEMergeMessages =
 
         [TestCaseSource(nameof(ParsesTfsGermanDEMergeMessages))]
         public void ParseTfsGermanDEMessage(
-          string message,
-          string expectedMergedBranch,
-          string expectedTargetBranch,
-          SemanticVersion expectedVersion,
-          int? expectedPullRequestNumber)
+            string message,
+            string expectedMergedBranch,
+            string expectedTargetBranch,
+            SemanticVersion expectedVersion,
+            int? expectedPullRequestNumber)
         {
             // Act
             var sut = new MergeMessage(message, _config);
@@ -268,9 +268,9 @@ private static readonly object[] ParsesTfsGermanDEMergeMessages =
 
         private static readonly object[] InvalidMergeMessages =
         {
-           new object[] { "Merge pull request # from feature/one", "", null, null, null },
-           new object[] { "Merge pull request # in feature/one from feature/two to master" , "", null, null, null },
-           new object[] { "Zusammengeführter PR : feature/one mit master mergen", "", null, null, null }
+            new object[] { "Merge pull request # from feature/one", "", null, null, null },
+            new object[] { "Merge pull request # in feature/one from feature/two to master" , "", null, null, null },
+            new object[] { "Zusammengeführter PR : feature/one mit master mergen", "", null, null, null }
         };
 
         [TestCaseSource(nameof(InvalidMergeMessages))]
