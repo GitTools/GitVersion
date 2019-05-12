b244ac3e3a7d55a46fb30a8bf356f96e0abde38c
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2019-05-12 18:01:35 +0300

    Migrating from UtilPack to NuGetUtils, Stage 2: MSBuild infrastructure-related changes. Also forgot to add one .cs file from previous commit.

---
+++ src/GitVersionTask/TaskLogger.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+
+class TaskLogger
+{
+    private readonly TextWriter stdout;
+    private readonly TextWriter stderr;
+
+    public TaskLogger(
+        TextWriter paramStdout = null,
+        TextWriter paramStderr = null
+        )
+    {
+        this.stdout = paramStdout ?? Console.Out;
+        this.stderr = paramStderr ?? Console.Error;
+    }
+
+    public void LogWarning(string message)
+    {
+        this.stdout.WriteLine(message);
+    }
+
+    public void LogInfo(string message)
+    {
+        this.stdout.WriteLine(message);
+    }
+
+    public void LogError(string message)
+    {
+        this.stderr.WriteLine(message);
+    }
+}
