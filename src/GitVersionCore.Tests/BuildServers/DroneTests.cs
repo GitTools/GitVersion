e781749eab176fecdd455c16b30dfe151e569d89
Vadim Hatsura <vadimgatsura@gmail.com>
2019-05-21 11:47:37 +0300

    implement unit tests

--- src/GitVersionCore.Tests/BuildServers/DroneTests.cs
+++ src/GitVersionCore.Tests/BuildServers/DroneTests.cs
@@ -46,5 +46,106 @@ namespace GitVersionCore.Tests.BuildServers
             // Assert
             result.ShouldBeFalse();
         }
+
+        [Test]
+        public void GetCurrentBranch_ShouldDroneBranch_InCaseOfPush()
+        {
+            // Arrange
+            const string droneBranch = "droneBranch";
+
+            Environment.SetEnvironmentVariable("DRONE_PULL_REQUEST", "");
+            Environment.SetEnvironmentVariable("DRONE_BRANCH", droneBranch);
+
+            var buildServer = new Drone();
+
+            // Act
+            var result = buildServer.GetCurrentBranch(false);
+
+            // Assert
+            result.ShouldBe(droneBranch);
+        }
+
+        [Test]
+        public void GetCurrentBranch_ShouldUseDroneSourceBranch_InCaseOfPullRequestAndNonEmptyDroneSourceBranch()
+        {
+            // Arrange
+            const string droneSourceBranch = "droneSourceBranch";
+            Environment.SetEnvironmentVariable("DRONE_PULL_REQUEST", "1");
+            Environment.SetEnvironmentVariable("DRONE_SOURCE_BRANCH", droneSourceBranch);
+
+            var buildServer = new Drone();
+
+            // Act
+            var result = buildServer.GetCurrentBranch(false);
+
+            // Assert
+            result.ShouldBe(droneSourceBranch);
+        }
+
+        [Test]
+        public void GetCurrentBranch_ShouldUseSourceBranchFromCiCommitRefSpec_InCaseOfPullRequestAndEmptyDroneSourceBranch()
+        {
+            // Arrange
+            const string droneSourceBranch = "droneSourceBranch";
+            const string droneDestinationBranch = "droneDestinationBranch";
+
+            string ciCommitRefSpec = $"{droneSourceBranch}:{droneDestinationBranch}";
+
+            Environment.SetEnvironmentVariable("DRONE_PULL_REQUEST", "1");
+            Environment.SetEnvironmentVariable("DRONE_SOURCE_BRANCH", "");
+            Environment.SetEnvironmentVariable("CI_COMMIT_REFSPEC", ciCommitRefSpec);
+
+            var buildServer = new Drone();
+
+            // Act
+            var result = buildServer.GetCurrentBranch(false);
+
+            // Assert
+            result.ShouldBe(droneSourceBranch);
+        }
+
+        [Test]
+        public void GetCurrentBranch_ShouldUseDroneBranch_InCaseOfPullRequestAndEmptyDroneSourceBranchAndCiCommitRefSpec()
+        {
+            // Arrange
+            const string droneBranch = "droneBranch";
+
+            Environment.SetEnvironmentVariable("DRONE_PULL_REQUEST", "1");
+            Environment.SetEnvironmentVariable("DRONE_SOURCE_BRANCH", "");
+            Environment.SetEnvironmentVariable("CI_COMMIT_REFSPEC", "");
+            Environment.SetEnvironmentVariable("DRONE_BRANCH", droneBranch);
+
+            var buildServer = new Drone();
+
+            // Act
+            var result = buildServer.GetCurrentBranch(false);
+
+            // Assert
+            result.ShouldBe(droneBranch);
+        }
+
+        [Test]
+        public void GetCurrentBranch_ShouldUseDroneBranch_InCaseOfPullRequestAndEmptyDroneSourceBranchAndInvalidFormatOfCiCommitRefSpec()
+        {
+            // Arrange
+            const string droneBranch = "droneBranch";
+            const string droneSourceBranch = "droneSourceBranch";
+            const string droneDestinationBranch = "droneDestinationBranch";
+
+            string ciCommitRefSpec = $"{droneSourceBranch};{droneDestinationBranch}";
+
+            Environment.SetEnvironmentVariable("DRONE_PULL_REQUEST", "1");
+            Environment.SetEnvironmentVariable("DRONE_SOURCE_BRANCH", "");
+            Environment.SetEnvironmentVariable("CI_COMMIT_REFSPEC", ciCommitRefSpec);
+            Environment.SetEnvironmentVariable("DRONE_BRANCH", droneBranch);
+
+            var buildServer = new Drone();
+
+            // Act
+            var result = buildServer.GetCurrentBranch(false);
+
+            // Assert
+            result.ShouldBe(droneBranch);
+        }
     }
 }
