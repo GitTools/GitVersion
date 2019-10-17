4a9fa96aaf76121e79bd4907e165d4f7e47d6257
Artur Stolear <arturcic@gmail.com>
2019-10-17 23:19:40 +0300

    Add Microsoft.Extensions.DependencyInjection

--- src/GitVersionExe/Program.cs
+++ src/GitVersionExe/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using GitVersion.Common;
 using GitVersion.Configuration;
 using GitVersion.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using Console = System.Console;
 using Environment = GitVersion.Common.Environment;
 
@@ -17,24 +18,19 @@ namespace GitVersion
             var exitCode = 1;
             if (arguments != null)
             {
-                var fileSystem = new FileSystem();
-                var environment = new Environment();
+                var services = ConfigureServices(arguments);
 
-                try
+                using (var serviceProvider = services.BuildServiceProvider())
                 {
-                    var log = new Log { Verbosity = arguments.Verbosity };
-
-                    var configFileLocator = string.IsNullOrWhiteSpace(arguments.ConfigFile)
-                        ? (IConfigFileLocator) new DefaultConfigFileLocator(fileSystem, log)
-                        : new NamedConfigFileLocator(arguments.ConfigFile, fileSystem, log);
-
-                    var app = new GitVersionApplication(fileSystem, environment, log, configFileLocator);
-
-                    exitCode = app.Run(arguments);
-                }
-                catch (Exception exception)
-                {
-                    Console.Error.WriteLine(exception.Message);
+                    try
+                    {
+                        var app = serviceProvider.GetService<IGitVersionRunner>();
+                        exitCode = app.Run(arguments);
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.Error.WriteLine(exception.Message);
+                    }
                 }
             }
 
@@ -44,6 +40,33 @@ namespace GitVersion
             }
 
             System.Environment.Exit(exitCode);
+        }
+
+        private static IServiceCollection ConfigureServices(Arguments arguments)
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<IFileSystem, FileSystem>();
+            services.AddSingleton<IEnvironment, Environment>();
+            services.AddSingleton<IHelpWriter, HelpWriter>();
+            services.AddSingleton<IVersionWriter, VersionWriter>();
+            services.AddSingleton<ILog>(new Log { Verbosity = arguments.Verbosity });
+            services.AddSingleton(sp => ConfigFileLocator(sp, arguments));
+            services.AddSingleton<IGitVersionRunner, GitVersionRunner>();
+
+            return services;
+        }
+
+        private static IConfigFileLocator ConfigFileLocator(IServiceProvider sp, Arguments arguments)
+        {
+            var fileSystem = sp.GetService<IFileSystem>();
+            var log = sp.GetService<ILog>();
+
+            var configFileLocator = string.IsNullOrWhiteSpace(arguments.ConfigFile)
+                ? (IConfigFileLocator) new DefaultConfigFileLocator(fileSystem, log)
+                : new NamedConfigFileLocator(arguments.ConfigFile, fileSystem, log);
+
+            return configFileLocator;
         }
 
         private static Arguments ParseArguments(string[] args)
