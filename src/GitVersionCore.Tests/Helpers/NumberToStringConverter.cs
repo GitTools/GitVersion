1cb5f6d7aceb7736ead5b3a6c1b037a35437e554
Joseph Petersen <josephp90@gmail.com>
2020-01-22 09:27:00 +0100

    add number converter

---
+++ src/GitVersionCore.Tests/Helpers/NumberToStringConverter.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GitVersionCore.Tests.Helpers
+{
+    public class NumberToStringConverter : JsonConverter<object>
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeof(string) == typeToConvert;
+        }
+        public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Number)
+            {
+                return reader.GetString();
+            }
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                return reader.GetString();
+            }
+
+            using var document = JsonDocument.ParseValue(ref reader);
+            return document.RootElement.Clone().ToString();
+        }
+
+        public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue( value.ToString());
+        }
+    }
+}
--- src/GitVersionExe.Tests/ExecutionResults.cs
+++ src/GitVersionExe.Tests/ExecutionResults.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using GitVersion.OutputVariables;
+using GitVersionCore.Tests.Helpers;
 
 namespace GitVersionExe.Tests
 {
@@ -23,8 +24,10 @@ namespace GitVersionExe.Tests
             get
             {
                 var regex = new Regex(@"\{(.|\s)*\}");
+                var jsonSerializerOptions = new JsonSerializerOptions();
+                jsonSerializerOptions.Converters.Add(new NumberToStringConverter());
                 var output = regex.Match(Output).Value;
-                var outputVariables = JsonSerializer.Deserialize<Dictionary<string, string>>(output);
+                var outputVariables = JsonSerializer.Deserialize<Dictionary<string, string>>(output, jsonSerializerOptions);
                 return VersionVariables.FromDictionary(outputVariables);
             }
         }
