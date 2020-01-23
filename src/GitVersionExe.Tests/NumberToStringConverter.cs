fecd4b6451b1d71eaf55fcd5ff46e7e41f2dcfbd
Joseph Petersen <josephp90@gmail.com>
2020-01-23 06:05:27 +0100

    use a string converter

--- src/GitVersionExe.Tests/NumberToStringConverter.cs
+++ src/GitVersionExe.Tests/NumberToStringConverter.cs
@@ -4,19 +4,15 @@ using System.Text.Json.Serialization;
 
 namespace GitVersionExe.Tests
 {
-    public class NumberToStringConverter : JsonConverter<object>
+    public class NumberToStringConverter : JsonConverter<string>
     {
         public override bool CanConvert(Type typeToConvert)
         {
             return typeof(string) == typeToConvert;
         }
-        public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            if (reader.TokenType == JsonTokenType.Number)
-            {
-                return reader.GetString();
-            }
-            if (reader.TokenType == JsonTokenType.String)
+            if (reader.TokenType == JsonTokenType.Number || reader.TokenType == JsonTokenType.String)
             {
                 return reader.GetString();
             }
@@ -25,9 +21,9 @@ namespace GitVersionExe.Tests
             return document.RootElement.Clone().ToString();
         }
 
-        public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue( value.ToString());
+            writer.WriteStringValue(value);
         }
     }
 }
