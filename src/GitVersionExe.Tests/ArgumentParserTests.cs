5d2b42f50a494b0476e0fc6a23f701c050b76186
Inok <club0174@gmail.com>
2020-02-11 02:25:30 +0300

    #2089 fix parsing of command line switch arguments

--- src/GitVersionExe.Tests/ArgumentParserTests.cs
+++ src/GitVersionExe.Tests/ArgumentParserTests.cs
@@ -328,11 +328,57 @@ namespace GitVersionExe.Tests
         }
 
         [Test]
+        public void OtherArgumentsCanBeParsedBeforeNonormalize()
+        {
+            var arguments = argumentParser.ParseArguments("targetpath -nonormalize");
+            arguments.TargetPath.ShouldBe("targetpath");
+            arguments.NoNormalize.ShouldBe(true);
+        }
+
+        [Test]
+        public void OtherArgumentsCanBeParsedBeforeNocache()
+        {
+            var arguments = argumentParser.ParseArguments("targetpath -nocache");
+            arguments.TargetPath.ShouldBe("targetpath");
+            arguments.NoCache.ShouldBe(true);
+        }
+
+        [Test]
         public void OtherArgumentsCanBeParsedAfterNofetch()
         {
             var arguments = argumentParser.ParseArguments("-nofetch -proj foo.sln");
             arguments.NoFetch.ShouldBe(true);
             arguments.Proj.ShouldBe("foo.sln");
+        }
+
+        [Test]
+        public void OtherArgumentsCanBeParsedAfterNonormalize()
+        {
+            var arguments = argumentParser.ParseArguments("-nonormalize -proj foo.sln");
+            arguments.NoNormalize.ShouldBe(true);
+            arguments.Proj.ShouldBe("foo.sln");
+        }
+
+        [Test]
+        public void OtherArgumentsCanBeParsedAfterNocache()
+        {
+            var arguments = argumentParser.ParseArguments("-nocache -proj foo.sln");
+            arguments.NoCache.ShouldBe(true);
+            arguments.Proj.ShouldBe("foo.sln");
+        }
+
+        [TestCase("-nofetch -nonormalize -nocache")]
+        [TestCase("-nofetch -nocache -nonormalize")]
+        [TestCase("-nocache -nofetch -nonormalize")]
+        [TestCase("-nocache -nonormalize -nofetch")]
+        [TestCase("-nonormalize -nocache -nofetch")]
+        [TestCase("-nonormalize -nofetch -nocache")]
+        public void SeveralSwitchesCanBeParsed(string commandLineArgs)
+        {
+            var arguments = argumentParser.ParseArguments(commandLineArgs);
+            arguments.NoCache.ShouldBe(true);
+            arguments.NoNormalize.ShouldBe(true);
+            arguments.NoFetch.ShouldBe(true);
         }
 
         [Test]
