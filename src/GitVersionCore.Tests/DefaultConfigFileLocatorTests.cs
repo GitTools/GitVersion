e8327e82a3f4cceb7f0d3d06c0e8338f16de38dd
Artur Stolear <arturcic@gmail.com>
2020-02-26 10:48:59 +0200

    remove obsolete configs

--- src/GitVersionCore.Tests/DefaultConfigFileLocatorTests.cs
+++ src/GitVersionCore.Tests/DefaultConfigFileLocatorTests.cs
@@ -41,41 +41,7 @@ namespace GitVersionCore.Tests
             ShouldlyConfiguration.ShouldMatchApprovedDefaults.LocateTestMethodUsingAttribute<TestAttribute>();
         }
 
-        [TestCase(DefaultRepoPath)]
-        [TestCase(DefaultWorkingPath)]
-        public void WarnOnExistingGitVersionConfigYamlFile(string path)
-        {
-            SetupConfigFileContent(string.Empty, DefaultConfigFileLocator.ObsoleteFileName, path);
-
-            var output = WithDefaultConfigFileLocator(configFileLocator =>
-            {
-                configFileLocator.Verify(workingPath, repoPath);
-            });
-
-            var configFileDeprecatedWarning = $"{DefaultConfigFileLocator.ObsoleteFileName}' is deprecated, use '{DefaultConfigFileLocator.DefaultFileName}' instead";
-            output.Contains(configFileDeprecatedWarning).ShouldBe(true);
-        }
-
-        [TestCase(DefaultRepoPath)]
-        [TestCase(DefaultWorkingPath)]
-        public void WarnOnAmbiguousConfigFilesAtTheSameProjectRootDirectory(string path)
-        {
-            SetupConfigFileContent(string.Empty, DefaultConfigFileLocator.ObsoleteFileName, path);
-            SetupConfigFileContent(string.Empty, DefaultConfigFileLocator.DefaultFileName, path);
-
-            var output = WithDefaultConfigFileLocator(configFileLocator =>
-            {
-                configFileLocator.Verify(workingPath, repoPath);
-            });
-
-            var configFileDeprecatedWarning = $"Ambiguous config files at '{path}'";
-            output.Contains(configFileDeprecatedWarning).ShouldBe(true);
-        }
-
         [TestCase(DefaultConfigFileLocator.DefaultFileName, DefaultConfigFileLocator.DefaultFileName)]
-        [TestCase(DefaultConfigFileLocator.DefaultFileName, DefaultConfigFileLocator.ObsoleteFileName)]
-        [TestCase(DefaultConfigFileLocator.ObsoleteFileName, DefaultConfigFileLocator.DefaultFileName)]
-        [TestCase(DefaultConfigFileLocator.ObsoleteFileName, DefaultConfigFileLocator.ObsoleteFileName)]
         public void ThrowsExceptionOnAmbiguousConfigFileLocation(string repoConfigFile, string workingConfigFile)
         {
             var repositoryConfigFilePath = SetupConfigFileContent(string.Empty, repoConfigFile, repoPath);
@@ -104,25 +70,6 @@ namespace GitVersionCore.Tests
             });
 
             output.Length.ShouldBe(0);
-        }
-
-        [Test]
-        public void WarnOnObsoleteIsDevelopBranchConfigurationSetting()
-        {
-            const string text = @"
-assembly-versioning-scheme: MajorMinorPatch
-branches:
-  master:
-    tag: beta
-    is-develop: true";
-
-            var exception = Should.Throw<OldConfigurationException>(() =>
-            {
-                LegacyConfigNotifier.Notify(new StringReader(text));
-            });
-
-            const string expectedMessage = "'is-develop' is deprecated, use 'tracks-release-branches' instead.";
-            exception.Message.ShouldContain(expectedMessage);
         }
 
         private string SetupConfigFileContent(string text, string fileName, string path)
