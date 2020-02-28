bdfa509609fb45a9e082bb272dd7d096574f7df4
Artur Stolear <arturcic@gmail.com>
2020-02-28 09:16:46 +0200

    (build) use GitVersion release as source

--- nuspec/chocolateyInstall.ps1
+++ nuspec/chocolateyInstall.ps1
@@ -1,1 +1,16 @@
-Generate-BinFile "gv" "$packageFolder\Tools\GitVersion.exe"
+$version = "5.1.3"
+$packageName = "GitVersion.Portable"
+$url32 = "https://github.com/GitTools/GitVersion/releases/download/$version/gitversion-windows-$version.tar.gz";
+
+$packageArgs = @{
+    packageName    = $packageName
+    url            = $url32
+    unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+}
+
+$toolLocation = "$env:ChocolateyInstall\lib\$packageName\"
+Install-ChocolateyZipPackage @packageArgs
+$File = Get-ChildItem -File -Path $toolLocation -Filter *.tar
+Get-ChocolateyUnzip -fileFullPath $File.FullName -destination $toolLocation
+
+Generate-BinFile "gitversion" "$toolLocation\GitVersion.exe"
