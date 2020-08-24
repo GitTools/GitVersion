f6bc10b11f2ae5218bbeb3fc448b9510fa721aa9
Artur Stolear <arturcic@gmail.com>
2020-08-24 12:19:46 +0300

    cleanup

--- src/GitVersionCore.Tests/IntegrationTests/OtherScenarios.cs
+++ src/GitVersionCore.Tests/IntegrationTests/OtherScenarios.cs
@@ -1,9 +1,5 @@
-using System.Collections.Generic;
 using System.Linq;
 using GitTools.Testing;
-using GitVersion;
-using GitVersion.Model.Configuration;
-using GitVersion.VersionCalculation;
 using GitVersionCore.Tests.Helpers;
 using LibGit2Sharp;
 using NUnit.Framework;
