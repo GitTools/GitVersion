e65d3adb2d12bb716119ec566125418a85aca504
Artur <arturcic@gmail.com>
2020-12-25 18:44:10 +0100

    use the TaskLoggingHelper from Microsoft.Build.Utilities.Core

--- src/GitVersion.MsBuild/GitVersionTaskBase.cs
+++ src/GitVersion.MsBuild/GitVersionTaskBase.cs
@@ -1,4 +1,5 @@
 using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 
 namespace GitVersion.MsBuild
 {
--- src/GitVersion.MsBuild/Helpers/MsBuildAppender.cs
+++ src/GitVersion.MsBuild/Helpers/MsBuildAppender.cs
@@ -1,5 +1,6 @@
 using System;
 using GitVersion.Logging;
+using Microsoft.Build.Utilities;
 
 namespace GitVersion.MsBuild
 {
