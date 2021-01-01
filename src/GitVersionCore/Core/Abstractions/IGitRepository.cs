3651e581ec0233b4a12ba0fad3217b2875d61b2f
Artur <arturcic@gmail.com>
2021-01-01 16:20:54 +0100

    remove unused IRepository fields/methods from IGitRepository

--- src/GitVersionCore/Core/Abstractions/IGitRepository.cs
+++ src/GitVersionCore/Core/Abstractions/IGitRepository.cs
@@ -1,9 +1,20 @@
+using System;
 using LibGit2Sharp;
 
 namespace GitVersion
 {
-    public interface IGitRepository : IRepository
+    public interface IGitRepository : IDisposable
     {
         IGitRepositoryCommands Commands { get; }
+        ObjectDatabase ObjectDatabase { get; }
+        IQueryableCommitLog Commits { get; }
+        Branch Head { get; }
+        BranchCollection Branches { get; }
+        TagCollection Tags { get; }
+        ReferenceCollection Refs { get; }
+        Diff Diff { get; }
+        RepositoryInformation Info { get; }
+        Network Network { get; }
+        RepositoryStatus RetrieveStatus();
     }
 }
