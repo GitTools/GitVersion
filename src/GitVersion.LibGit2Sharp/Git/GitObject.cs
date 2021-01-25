3b9300a95cb63f25dd5af6e3cf36dde7241c6d04
Artur <arturcic@gmail.com>
2021-01-25 12:29:57 +0100

    added IGitObject

---
+++ src/GitVersion.LibGit2Sharp/Git/GitObject.cs
@@ -0,0 +1,28 @@
+using GitVersion.Helpers;
+
+namespace GitVersion
+{
+    internal class GitObject : IGitObject
+    {
+        private static readonly LambdaEqualityHelper<IGitObject> equalityHelper = new(x => x.Id);
+        private static readonly LambdaKeyComparer<IGitObject, string> comparerHelper = new(x => x.Sha);
+
+        internal GitObject(LibGit2Sharp.GitObject innerGitObject)
+        {
+            Id = new ObjectId(innerGitObject.Id);
+            Sha = innerGitObject.Sha;
+        }
+
+        public int CompareTo(IGitObject other) => comparerHelper.Compare(this, other);
+        public bool Equals(IGitObject other) => equalityHelper.Equals(this, other);
+        public override bool Equals(object obj) => Equals((obj as IGitObject)!);
+        public override int GetHashCode() => equalityHelper.GetHashCode(this);
+        public override string ToString()
+        {
+            return Id.ToString(7);
+        }
+
+        public IObjectId Id { get; }
+        public string Sha { get; }
+    }
+}
--- src/GitVersionCore/Git/ICommit.cs
+++ src/GitVersionCore/Git/ICommit.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 
 namespace GitVersion
 {
-    public interface ICommit : IEquatable<ICommit>, IComparable<ICommit>
+    public interface ICommit : IEquatable<ICommit>, IComparable<ICommit>, IGitObject
     {
         IEnumerable<ICommit> Parents { get; }
-        string Sha { get; }
-        IObjectId Id { get; }
         DateTimeOffset When { get; }
         string Message { get; }
     }
