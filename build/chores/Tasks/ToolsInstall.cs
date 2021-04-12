126f05c6855d0d974d83f15c08bf82192895eca9
Artur <arturcic@gmail.com>
2021-04-12 15:49:47 +0200

    (build) added tool install task to install tools to a local folder

---
+++ build/chores/Tasks/ToolsInstall.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Cake.Common.Tools.DotNetCore;
+using Cake.Frosting;
+using Cake.Json;
+using Common.Utilities;
+using Newtonsoft.Json.Linq;
+
+namespace Chores.Tasks
+{
+    [TaskName(nameof(ToolsInstall))]
+    [TaskDescription("Install dotnet global tools to 'tools' folder")]
+    public class ToolsInstall : FrostingTask<BuildContext>
+    {
+        public override void Run(BuildContext context)
+        {
+            var json = context.ParseJsonFromFile("dotnet-tools.json");
+            var jToken = json["tools"];
+            if (jToken == null) return;
+
+            var tools = jToken.Select(x => (JProperty)x).ToDictionary(x => x.Name, x => x.Value["version"]?.Value<string>());
+            foreach (var (toolName, version) in tools)
+            {
+                context.DotNetCoreTool($"tool update --tool-path {CommonPaths.ToolsDirectory} --version {version} {toolName}");
+            }
+        }
+    }
+}
