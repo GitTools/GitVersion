9234a6d0b082e28239fda8df4450b161ad65db12
Artur <arturcic@gmail.com>
2021-04-13 22:38:50 +0200

    (build) cleanup

--- build/common/Utilities/Extensions.cs
+++ build/common/Utilities/Extensions.cs
@@ -2,40 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Cake.Core.IO;
 using Cake.Frosting;
 
 namespace Common.Utilities
 {
     public static class Extensions
     {
-        public static IEnumerable<Type> FindAllDerivedTypes<T>(this Assembly assembly) =>
-            assembly.FindAllDerivedTypes(typeof(T));
-
         public static IEnumerable<Type> FindAllDerivedTypes(this Assembly assembly, Type baseType) =>
             from type in assembly.GetExportedTypes()
             let info = type.GetTypeInfo()
             where baseType.IsAssignableFrom(type) && info.IsClass && !info.IsAbstract
             select type;
-
-        public static string GetTaskName(this IFrostingTask task)
-        {
-            if (task is null)
-            {
-                throw new ArgumentNullException(nameof(task));
-            }
-
-            return task.GetType().GetTaskName();
-        }
-
-        public static string GetTaskDescription(this IFrostingTask task)
-        {
-            if (task is null)
-            {
-                throw new ArgumentNullException(nameof(task));
-            }
-
-            return task.GetType().GetTaskDescription();
-        }
 
         public static string GetTaskDescription(this Type task)
         {
@@ -58,6 +36,8 @@ namespace Common.Utilities
             var attribute = task.GetCustomAttribute<TaskNameAttribute>();
             return attribute != null ? attribute.Name : task.Name;
         }
+        public static DirectoryPath Combine(this string path, string segment) => DirectoryPath.FromString(path).Combine(segment);
+        public static FilePath CombineWithFilePath(this string path, string segment) => DirectoryPath.FromString(path).CombineWithFilePath(segment);
     }
 
 }
