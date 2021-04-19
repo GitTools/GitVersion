0997a689f0e1c3176593d0ea9585d686c4909f44
Artur <arturcic@gmail.com>
2021-04-19 15:03:29 +0200

    (build) run package target

---
+++ build/chores/Startup.cs
@@ -0,0 +1,16 @@
+using System;
+using Cake.Frosting;
+using Common.Lifetime;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chores
+{
+    public class Startup : IFrostingStartup
+    {
+        public void Configure(IServiceCollection services)
+        {
+            services.UseLifetime<BuildLifetime>();
+            services.UseTaskLifetime<BuildTaskLifetime>();
+        }
+    }
+}
