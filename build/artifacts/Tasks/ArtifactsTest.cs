786614132ed7ec4ddb4e44444bfb2ea0cd24a8fc
Artur <arturcic@gmail.com>
2021-05-18 22:18:12 +0200

    (build) added ArtifactsTest to test packages

---
+++ build/artifacts/Tasks/ArtifactsTest.cs
@@ -0,0 +1,20 @@
+﻿using Cake.Frosting;
+using Common.Utilities;
+
+namespace Artifacts.Tasks
+{
+    [TaskName(nameof(ArtifactsMsBuildCoreTest))]
+    [TaskDescription("Tests packages in docker container")]
+    [IsDependentOn(typeof(ArtifactsDotnetToolTest))]
+    [IsDependentOn(typeof(ArtifactsMsBuildCoreTest))]
+    public class ArtifactsTest : FrostingTask<BuildContext>
+    {
+        public override bool ShouldRun(BuildContext context)
+        {
+            var shouldRun = true;
+            shouldRun &= context.ShouldRun(context.IsDockerOnLinux, "ArtifactsTest works only on Docker on Linux agents.");
+
+            return shouldRun;
+        }
+    }
+}
