1faf5834b1529e1012ba72d2d50e6564f05cecd1
Darwin <DarwinJS@users.noreply.github.com>
2021-06-25 14:27:22 -0400

    Update misleading error and give resolution hints
    Previous message sounded like literal branches rather than Gitversion branch "types".  Also gave hints on checking local repo meets Gitversion requirements or to use dynamic repositories.

--- src/GitVersion.Core/Configuration/BranchConfigurationCalculator.cs
+++ src/GitVersion.Core/Configuration/BranchConfigurationCalculator.cs
@@ -132,7 +132,7 @@ namespace GitVersion.Configuration
                 {
                     // TODO We should call the build server to generate this exception, each build server works differently
                     // for fetch issues and we could give better warnings.
-                    throw new InvalidOperationException("Could not find a 'develop' or 'main' branch, neither locally nor remotely.");
+                    throw new InvalidOperationException("Gitversion could not determine which branch to treat as the development branch (default = develop) nor release branch (default = main or master), neither locally nor remotely. Ensure the local clone and checkout match the requirements or considering using 'Gitversion Dynamic Repositories'");
                 }
 
                 log.Warning($"{errorMessage}{System.Environment.NewLine}Falling back to {chosenBranch} branch config");
