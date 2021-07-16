aa6f96a65cfe10ac87a9bae28786f89daa2160e5
Amaury Levé <amaury.leve@gmail.com>
2021-07-16 23:31:55 +0200

    Fix formatting issues

--- src/GitVersion.Core.Tests/Core/GitVersionExecutorTests.cs
+++ src/GitVersion.Core.Tests/Core/GitVersionExecutorTests.cs
@@ -556,15 +556,16 @@ namespace GitVersion.Core.Tests
             return this.sp.GetService<IGitVersionCalculateTool>();
         }
 
-        private static IServiceProvider GetServiceProvider(GitVersionOptions gitVersionOptions, ILog log = null, IGitRepository repository = null, IFileSystem fileSystem = null, IEnvironment environment = null) => ConfigureServices(services =>
-                                                                                                                                                                                                                                {
-                                                                                                                                                                                                                                    if (log != null) services.AddSingleton(log);
-                                                                                                                                                                                                                                    if (fileSystem != null) services.AddSingleton(fileSystem);
-                                                                                                                                                                                                                                    if (repository != null) services.AddSingleton(repository);
-                                                                                                                                                                                                                                    if (environment != null) services.AddSingleton(environment);
-                                                                                                                                                                                                                                    var options = Options.Create(gitVersionOptions);
-                                                                                                                                                                                                                                    services.AddSingleton(options);
-                                                                                                                                                                                                                                    services.AddSingleton(RepositoryExtensions.ToGitRepositoryInfo(options));
-                                                                                                                                                                                                                                });
+        private static IServiceProvider GetServiceProvider(GitVersionOptions gitVersionOptions, ILog log = null, IGitRepository repository = null, IFileSystem fileSystem = null, IEnvironment environment = null) =>
+            ConfigureServices(services =>
+            {
+                if (log != null) services.AddSingleton(log);
+                if (fileSystem != null) services.AddSingleton(fileSystem);
+                if (repository != null) services.AddSingleton(repository);
+                if (environment != null) services.AddSingleton(environment);
+                var options = Options.Create(gitVersionOptions);
+                services.AddSingleton(options);
+                services.AddSingleton(RepositoryExtensions.ToGitRepositoryInfo(options));
+            });
     }
 }
