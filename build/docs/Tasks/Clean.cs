2faa0934db5d3ba3b02bc278d18719c1045180cc
Artur <arturcic@gmail.com>
2021-08-09 10:10:42 +0200

    (build) added PreviewDocs task

---
+++ build/docs/Tasks/Clean.cs
@@ -0,0 +1,19 @@
+using Cake.Common.Diagnostics;
+using Cake.Common.IO;
+using Cake.Frosting;
+using Common.Utilities;
+
+namespace Docs.Tasks
+{
+    [TaskName(nameof(Clean))]
+    [TaskDescription("Cleans the temporary publish location")]
+    public sealed class Clean : FrostingTask<BuildContext>
+    {
+        public override void Run(BuildContext context)
+        {
+            context.Information("Cleaning directories...");
+
+            context.EnsureDirectoryExists(Paths.ArtifactsDocs.Combine("_published"));
+        }
+    }
+}
