b3563fa2659ac827c6edf9484c16b84ea82f3d40
Artur <arturcic@gmail.com>
2021-08-10 13:08:04 +0200

    (build) using Combine method instead of string concatenating

--- build/common/Utilities/Paths.cs
+++ build/common/Utilities/Paths.cs
@@ -6,21 +6,21 @@ namespace Common.Utilities
     {
         public static readonly DirectoryPath Root = "./";
 
-        public static readonly DirectoryPath ToolsDirectory = $"{Root}tools";
-        public static readonly DirectoryPath Artifacts = $"{Root}artifacts";
-        public static readonly DirectoryPath Src = $"{Root}src";
-        public static readonly DirectoryPath Docs = $"{Root}docs";
-        public static readonly DirectoryPath Build = $"{Root}build";
+        public static readonly DirectoryPath ToolsDirectory = Root.Combine("tools");
+        public static readonly DirectoryPath Artifacts = Root.Combine("artifacts");
+        public static readonly DirectoryPath Src = Root.Combine("src");
+        public static readonly DirectoryPath Docs = Root.Combine("docs");
+        public static readonly DirectoryPath Build = Root.Combine("build");
 
-        public static readonly DirectoryPath Nuspec = $"{Build}/nuspec";
+        public static readonly DirectoryPath Nuspec = Build.Combine("nuspec");
 
-        public static readonly DirectoryPath TestOutput = $"{Artifacts}/test-results";
-        public static readonly DirectoryPath Packages = $"{Artifacts}/packages";
-        public static readonly DirectoryPath ArtifactsDocs = $"{Artifacts}/docs";
-        public static readonly DirectoryPath Native = $"{Packages}/native";
+        public static readonly DirectoryPath TestOutput = Artifacts.Combine("test-results");
+        public static readonly DirectoryPath Packages = Artifacts.Combine("packages");
+        public static readonly DirectoryPath ArtifactsDocs = Artifacts.Combine("docs");
 
-        public static readonly DirectoryPath Nuget = $"{Packages}/nuget";
-        public static readonly DirectoryPath ArtifactsBinCmdline = $"{Packages}/prepare/cmdline";
-        public static readonly DirectoryPath ArtifactsBinPortable = $"{Packages}/prepare/portable";
+        public static readonly DirectoryPath Native = Packages.Combine("native");
+        public static readonly DirectoryPath Nuget = Packages.Combine("nuget");
+        public static readonly DirectoryPath ArtifactsBinCmdline = Packages.Combine("prepare").Combine("cmdline");
+        public static readonly DirectoryPath ArtifactsBinPortable = Packages.Combine("prepare").Combine("portable");
     }
 }
