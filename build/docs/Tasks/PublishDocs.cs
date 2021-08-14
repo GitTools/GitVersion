19541dfafa4982ff33d55ae77f4c074baaae9c4e
Artur <arturcic@gmail.com>
2021-08-14 19:22:43 +0200

    (build) make docs publish manually trigger-able

--- build/docs/Tasks/PublishDocs.cs
+++ build/docs/Tasks/PublishDocs.cs
@@ -34,7 +34,7 @@ namespace Docs.Tasks
         {
             var shouldRun = true;
             shouldRun &= context.ShouldRun(context.DirectoryExists(Paths.Docs), "Wyam documentation directory is missing");
-            shouldRun &= context.ShouldRun(context.IsStableRelease || context.IsPreRelease, $"{nameof(PublishDocs)} works only for releases.");
+            shouldRun &= context.ShouldRun(context.IsStableRelease || context.IsPreRelease || context.ForcePublish, $"{nameof(PublishDocs)} works only for releases.");
 
             return shouldRun;
         }
