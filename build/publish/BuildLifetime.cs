c75423f5bf3de2779da20a71c4ffd6af2334be28
Artur <arturcic@gmail.com>
2021-08-14 18:44:48 +0200

    (build) fixed publishing of nuget packages to GitHub packages Repo

--- build/publish/BuildLifetime.cs
+++ build/publish/BuildLifetime.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System;
 using Cake.Common.IO;
 using Common.Utilities;
 using Publish.Utilities;
@@ -15,13 +15,12 @@ namespace Publish
             if (context.Version?.NugetVersion != null)
             {
                 var nugetVersion = context.Version.NugetVersion;
-                context.Packages = new List<NugetPackage>();
 
                 var nugetPackagesFiles = context.GetFiles(Paths.Nuget + "/*.nupkg");
                 foreach (var packageFile in nugetPackagesFiles)
                 {
                     var packageName = packageFile.GetFilenameWithoutExtension().ToString()[..^(nugetVersion.Length + 1)].ToLower();
-                    context.Packages.Add(new NugetPackage(packageName, packageFile, packageName.Contains("Portable")));
+                    context.Packages.Add(new NugetPackage(packageName, packageFile, packageName.Contains("Portable", StringComparison.OrdinalIgnoreCase)));
                 }
             }
             context.StartGroup("Build Setup");
