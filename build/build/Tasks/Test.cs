9080f2cc4418cd2f1cdeab9ff03405e7f809c0db
Artur <arturcic@gmail.com>
2021-08-18 15:15:52 +0200

    (build) publish test results with cake

--- build/build/Tasks/Test.cs
+++ build/build/Tasks/Test.cs
@@ -4,7 +4,7 @@ namespace Build.Tasks
 {
     [TaskName(nameof(Test))]
     [TaskDescription("(CI only) Run the tests and publish the results")]
-    [IsDependentOn(typeof(UnitTest))]
+    [IsDependentOn(typeof(PublishCoverage))]
     public class Test : FrostingTask<BuildContext>
     {
     }
