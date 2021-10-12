d94f35cbdc4de396347c8839f8d064523eb0e388
Artur <arturcic@gmail.com>
2021-10-12 10:46:12 +0200

    added descriptions to the Verbosity enum

--- src/GitVersion.LibGit2Sharp/Git/BranchCollection.cs
+++ src/GitVersion.LibGit2Sharp/Git/BranchCollection.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-
 namespace GitVersion;
 
 internal sealed class BranchCollection : IBranchCollection
--- src/GitVersion.MsBuild.Tests/Mocks/MockEngine.cs
+++ src/GitVersion.MsBuild.Tests/Mocks/MockEngine.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Concurrent;
 using GitVersion.MsBuild.Tests.Helpers;
 using Microsoft.Build.Framework;
