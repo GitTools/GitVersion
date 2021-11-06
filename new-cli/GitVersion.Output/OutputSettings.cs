99ff46e12f4bfdec926a51fdff624363dd850fb8
Artur <arturcic@gmail.com>
2021-11-06 21:46:29 +0100

    (new-cli) - move projects to net6.0

--- new-cli/GitVersion.Output/OutputSettings.cs
+++ new-cli/GitVersion.Output/OutputSettings.cs
@@ -7,7 +7,7 @@ namespace GitVersion.Output;
 [Command("output", "Outputs the version object.")]
 public record OutputSettings : GitVersionSettings
 {
-    public Lazy<string> VersionInfo { get; } = new(() => Console.IsInputRedirected ? Console.ReadLine() : "");
+    public Lazy<string> VersionInfo { get; } = new(() => (Console.IsInputRedirected ? Console.ReadLine() : "")!);
 
     [Option("--input-file", "The input version file")]
     public FileInfo InputFile { get; init; } = default!;
