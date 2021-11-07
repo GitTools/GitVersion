e354ffb61e3eecf39b5120203829adcad5b0feb3
Artur <arturcic@gmail.com>
2021-11-07 20:45:07 +0100

    (new-cli) - moved modules loading to ModulesLoader class

---
+++ new-cli/GitVersion.Cli/ModulesLoader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using GitVersion.Calculation;
+using GitVersion.Configuration;
+using GitVersion.Extensions;
+using GitVersion.Infrastructure;
+using GitVersion.Normalization;
+using GitVersion.Output;
+
+namespace GitVersion;
+
+public class ModulesLoader
+{
+    public static IContainer Load(string[] args)
+    {
+        // TODO: load the list of assemblies from the app working directory, later we might load from nuget
+        var assemblies = new[]
+        {
+            typeof(CommonModule).Assembly,
+            typeof(NormalizeModule).Assembly,
+            typeof(CalculateModule).Assembly,
+            typeof(ConfigModule).Assembly,
+            typeof(OutputModule).Assembly,
+            typeof(CliModule).Assembly
+        };
+
+        var gitVersionModules = assemblies
+            .SelectMany(a => a.GetTypes().Where(t => t.TypeIsGitVersionModule()))
+            .Select(t => (IGitVersionModule)Activator.CreateInstance(t)!)
+            .ToList();
+
+        var serviceProvider = new ContainerRegistrar()
+            .RegisterModules(gitVersionModules)
+            .AddLogging(args)
+            .Build();
+
+        return serviceProvider;
+    }
+}
--- new-cli/GitVersion.Cli/Program.cs
+++ new-cli/GitVersion.Cli/Program.cs
@@ -1,36 +1,8 @@
 ﻿using System;
-using System.Linq;
 using GitVersion;
-using GitVersion.Calculation;
-using GitVersion.Configuration;
-using GitVersion.Extensions;
-using GitVersion.Infrastructure;
-using GitVersion.Normalization;
-using GitVersion.Output;
 
-// TODO: load the list of assemblies from the app working directory, later we might load from nuget
-var assemblies = new[]
-{
-    typeof(CommonModule).Assembly,
-    typeof(NormalizeModule).Assembly,
-    typeof(CalculateModule).Assembly,
-    typeof(ConfigModule).Assembly,
-    typeof(OutputModule).Assembly,
-    typeof(CliModule).Assembly
-};
-
-var gitVersionModules = assemblies
-    .SelectMany(a => a.GetTypes().Where(t => t.TypeIsGitVersionModule()))
-    .Select(t => (IGitVersionModule)Activator.CreateInstance(t)!)
-    .ToList();
-
-using var serviceProvider = new ContainerRegistrar()
-    .RegisterModules(gitVersionModules)
-    .AddLogging(args)
-    .Build();
-
+using var serviceProvider = ModulesLoader.Load(args);
 var app = serviceProvider.GetService<GitVersionApp>();
-
 var result = await app!.RunAsync(args);
 
 if (!Console.IsInputRedirected) Console.ReadKey();
