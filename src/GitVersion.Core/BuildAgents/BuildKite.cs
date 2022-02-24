198d6e61ef19f0186888f1d4c1fdf2a6ac8010ab
Sam Anthonisz <sam.anthonisz@iress.com>
2022-02-24 16:12:47 +1100

    Enable correct PR versioning for Buildkite

--- src/GitVersion.Core/BuildAgents/BuildKite.cs
+++ src/GitVersion.Core/BuildAgents/BuildKite.cs
@@ -21,7 +21,20 @@ public class BuildKite : BuildAgentBase
     public override string[] GenerateSetParameterMessage(string name, string value) =>
         Array.Empty<string>(); // There is no equivalent function in BuildKite.
 
-    public override string? GetCurrentBranch(bool usingDynamicRepos) => Environment.GetEnvironmentVariable("BUILDKITE_BRANCH");
+    public override string? GetCurrentBranch(bool usingDynamicRepos)
+    {        
+        var pullRequest = Environment.GetEnvironmentVariable("BUILDKITE_PULL_REQUEST");
+        if (string.IsNullOrEmpty(pullRequest) || pullRequest == "false")
+        {
+            return Environment.GetEnvironmentVariable("BUILDKITE_BRANCH");
+        }
+        else
+        {
+            // for pull requests BUILDKITE_BRANCH refers to the head, so adjust the 
+            // branch name for pull request versioning to function as expected
+            return string.Format("refs/pull/{0}/head", pullRequest);
+        }
+    }
 
     public override bool PreventFetch() => true;
 }
