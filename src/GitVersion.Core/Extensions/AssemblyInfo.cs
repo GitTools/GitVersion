4263830b8b253d0df9b17ad79bcff6311f4440d8
Artur <arturcic@gmail.com>
2022-07-28 16:12:00 +0200

    use <InternalsVisibleTo /> instead of AssemblyInfo.cs

--- src/GitVersion.Core/Extensions/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("GitVersion.Core.Tests")]
-[assembly: InternalsVisibleTo("GitVersion.App.Tests")]
-[assembly: InternalsVisibleTo("GitVersion.MsBuild.Tests")]
