29b7b31534ace9681c0ff27b8668aa97d4e85429
Michael Denny <micdenny@gmail.com>
2022-09-13 13:46:27 +0200

    improve legibility and consistency

--- src/GitVersion.Core/Configuration/BranchConfigurationCalculator.cs
+++ src/GitVersion.Core/Configuration/BranchConfigurationCalculator.cs
@@ -89,7 +89,8 @@ public class BranchConfigurationCalculator : IBranchConfigurationCalculator
             {
                 excludedInheritBranches.Add(excludedBranch);
             }
-            var branchesToEvaluate = this.repositoryStore.ExcludingBranches(excludedInheritBranches, excludeRemotes: true)
+            var branchesToEvaluate = this.repositoryStore
+                .ExcludingBranches(excludedInheritBranches, excludeRemotes: true)
                 .Distinct(new LocalRemoteBranchEqualityComparer())
                 .ToList();
 
