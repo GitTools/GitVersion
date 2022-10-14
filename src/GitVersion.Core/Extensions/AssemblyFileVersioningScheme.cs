ec543ff159c15c503ad449a2280c85a226667b29
Hardy Hobeck <hardy.hobeck.extern@rwe.com>
2022-10-14 13:53:08 +0200

    Move AssemblyFileVersioningScheme and AssemblyVersioningScheme to separate files.

---
+++ src/GitVersion.Core/Extensions/AssemblyFileVersioningScheme.cs
@@ -0,0 +1,10 @@
+﻿namespace GitVersion.Extensions;
+
+public enum AssemblyFileVersioningScheme
+{
+    MajorMinorPatchTag,
+    MajorMinorPatch,
+    MajorMinor,
+    Major,
+    None
+}
---
+++ src/GitVersion.Core/Extensions/AssemblyVersioningScheme.cs
@@ -0,0 +1,10 @@
+﻿namespace GitVersion.Extensions;
+
+public enum AssemblyVersioningScheme
+{
+    MajorMinorPatchTag,
+    MajorMinorPatch,
+    MajorMinor,
+    Major,
+    None
+}
