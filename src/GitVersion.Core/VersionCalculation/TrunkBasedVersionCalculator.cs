3083f16a2ecff8f4dc82b324ccf85abc56531b86
Hardy Hobeck <Hardy.Hobeck.extern@rwe.com>
2023-04-06 15:57:00 +0200

    Give NotImplementedException a message

--- src/GitVersion.Core/VersionCalculation/TrunkBasedVersionCalculator.cs
+++ src/GitVersion.Core/VersionCalculation/TrunkBasedVersionCalculator.cs
@@ -3,5 +3,6 @@ namespace GitVersion.VersionCalculation;
 internal sealed class TrunkBasedVersionCalculator : ITrunkBasedVersionCalculator
 {
     // TODO: Please implement trunk based version here and remove MainlineVersionCalculator.
-    public SemanticVersion Calculate(NextVersion nextVersion) => throw new NotImplementedException();
+    public SemanticVersion Calculate(NextVersion nextVersion) =>
+        throw new NotImplementedException("Trunk based version calculation is not yet implemented. Use Mainline mode instead.");
 }
