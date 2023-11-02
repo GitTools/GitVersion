9364cb0851914a806f9afd40a51d2694908cdbfa
Noah Sherwin <noahrsherwin@gmail.com>
2023-11-02 07:49:44 -0700

    refactor(GitVersion.Configuration): add .NET 7 StringSyntaxAttribute for CommitDateFormat

--- src/GitVersion.Configuration/GitVersionConfiguration.cs
+++ src/GitVersion.Configuration/GitVersionConfiguration.cs
@@ -89,6 +89,9 @@ internal sealed record GitVersionConfiguration : BranchConfiguration, IGitVersio
     [JsonPropertyName("commit-date-format")]
     [JsonPropertyDescription("The format to use when calculating the commit date. Defaults to 'yyyy-MM-dd'. See [Standard Date and Time Format Strings](https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings) and [Custom Date and Time Format Strings](https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings).")]
     [JsonPropertyDefault("'yyyy-MM-dd'")]
+#if NET7_0_OR_GREATER
+    [System.Diagnostics.CodeAnalysis.StringSyntax("DateTimeFormat")] // See https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.codeanalysis.stringsyntaxattribute, https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.codeanalysis.stringsyntaxattribute.datetimeformat?view=net-7.0#system-diagnostics-codeanalysis-stringsyntaxattribute-datetimeformat
+#endif
     public string? CommitDateFormat { get; internal set; }
 
     [JsonPropertyName("merge-message-formats")]
