0a44b8da5088b8e09ee4a7e6506d3d8b844ef07a
Artur Stolear <arturcic@gmail.com>
2023-12-01 02:02:21 +0100

    (ci) removed test_arm64_artifacts input for  _artifacts_linux

--- build/artifacts/BuildContext.cs
+++ build/artifacts/BuildContext.cs
@@ -8,10 +8,6 @@ public class BuildContext : BuildContextBase
 
     public bool IsDockerOnLinux { get; set; }
 
-    public bool TestArm64Artifacts { get; set; }
-
-    public Architecture? Architecture { get; set; }
-
     public IEnumerable<DockerImage> Images { get; set; } = new List<DockerImage>();
 
     public BuildContext(ICakeContext context) : base(context)
--- build/artifacts/BuildLifetime.cs
+++ build/artifacts/BuildLifetime.cs
@@ -10,9 +10,7 @@ public class BuildLifetime : BuildLifetimeBase<BuildContext>
         base.Setup(context, info);
 
         context.IsDockerOnLinux = context.DockerCustomCommand("info --format '{{.OSType}}'").First().Replace("'", string.Empty) == "linux";
-        context.TestArm64Artifacts = context.EnvironmentVariable("TEST_ARM64_ARTIFACTS", false);
 
-        context.Architecture = context.HasArgument(Arguments.Architecture) ? context.Argument<Architecture>(Arguments.Architecture) : null;
         var dockerRegistry = context.Argument(Arguments.DockerRegistry, DockerRegistry.DockerHub);
         var dotnetVersion = context.Argument(Arguments.DockerDotnetVersion, string.Empty).ToLower();
         var dockerDistro = context.Argument(Arguments.DockerDistro, string.Empty).ToLower();
@@ -20,12 +18,13 @@ public class BuildLifetime : BuildLifetimeBase<BuildContext>
         var versions = string.IsNullOrWhiteSpace(dotnetVersion) ? Constants.VersionsToBuild : new[] { dotnetVersion };
         var distros = string.IsNullOrWhiteSpace(dockerDistro) ? Constants.DockerDistrosToBuild : new[] { dockerDistro };
 
-        var archs = context.Architecture.HasValue ? new[] { context.Architecture.Value } : Constants.ArchToBuild;
+        var architectures = context.HasArgument(Arguments.Architecture) ? context.Arguments<Architecture>(Arguments.Architecture) : Constants.ArchToBuild;
+
 
         var registry = dockerRegistry == DockerRegistry.DockerHub ? Constants.DockerHubRegistry : Constants.GitHubContainerRegistry;
         context.Images = from version in versions
                          from distro in distros
-                         from arch in archs
+                         from arch in architectures
                          select new DockerImage(distro, version, arch, registry, true);
 
         context.StartGroup("Build Setup");
