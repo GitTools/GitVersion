63127c397e55e1c94ef86fb6de702e028ac02755
Noah Sherwin <noahrsherwin@gmail.com>
2023-12-04 02:07:14 -0800

    refactor(GitVersion.Core): add configuration defaults to ConfigurationConstants

--- src/GitVersion.Core/Configuration/ConfigurationConstants.cs
+++ src/GitVersion.Core/Configuration/ConfigurationConstants.cs
@@ -1,10 +1,24 @@
+using GitVersion.Extensions;
+
 namespace GitVersion.Configuration;
 
 internal static class ConfigurationConstants
 {
+    public const AssemblyVersioningScheme DefaultAssemblyVersioningScheme = AssemblyVersioningScheme.MajorMinorPatch;
+    internal const string NameOfDefaultAssemblyVersioningScheme = nameof(AssemblyVersioningScheme.MajorMinorPatch);
+    public const AssemblyFileVersioningScheme DefaultAssemblyFileVersioningScheme = AssemblyFileVersioningScheme.MajorMinorPatch;
+    internal const string NameOfDefaultAssemblyFileVersioningScheme = nameof(AssemblyFileVersioningScheme.MajorMinorPatch);
+    public const string DefaultAssemblyInformationalFormat = "{InformationalVersion}";
     public const string DefaultTagPrefix = "[vV]?";
     public const string DefaultVersionInBranchPattern = @"(?<version>[vV]?\d+(\.\d+)?(\.\d+)?).*";
+    public const int DefaultTagPreReleaseWeight = 60000;
+    internal const string StringDefaultTagPreReleaseWeight = "60000";
+    public const string DefaultCommitDateFormat = "yyyy-MM-dd";
     public const string BranchNamePlaceholder = "{BranchName}";
+    public const bool DefaultUpdateBuildNumber = true;
+    internal const string StringDefaultUpdateBuildNumber = "true";
+    public const SemanticVersionFormat DefaultSemanticVersionFormat = SemanticVersionFormat.Strict;
+    internal const string StringDefaultSemanticVersionFormat = nameof(SemanticVersionFormat.Strict);
 
     public const string MainBranchKey = "main";
     public const string MasterBranchKey = "master";
