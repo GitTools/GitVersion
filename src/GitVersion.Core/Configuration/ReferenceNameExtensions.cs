ec20afc770756b0c778079d41577d20552d4a6bf
HHobeck <HHobeck@gmail.com>
2024-04-18 08:05:37 +0200

    Move TryGetSemanticVersion to ReferenceNameExtensions class

---
+++ src/GitVersion.Core/Configuration/ReferenceNameExtensions.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics.CodeAnalysis;
+using GitVersion.Git;
+
+namespace GitVersion.Configuration;
+
+public static class ReferenceNameExtensions
+{
+    public static bool TryGetSemanticVersion(
+            this ReferenceName source, [NotNullWhen(true)] out (SemanticVersion Value, string? Name) result, IGitVersionConfiguration configuration)
+        => source.TryGetSemanticVersion(out result, configuration.VersionInBranchRegex, configuration.TagPrefix, configuration.SemanticVersionFormat);
+
+    public static bool TryGetSemanticVersion(
+            this ReferenceName source, [NotNullWhen(true)] out (SemanticVersion Value, string? Name) result, EffectiveConfiguration configuration)
+        => source.TryGetSemanticVersion(out result, configuration.VersionInBranchRegex, configuration.TagPrefix, configuration.SemanticVersionFormat);
+}
