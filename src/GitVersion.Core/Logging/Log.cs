9515bc3a9874f82f0498d2386ff7dea18fbdc32b
Artur Stolear <artur.stolear@gmail.com>
2024-05-22 07:30:32 +0200

    minor impevements

--- src/GitVersion.Core/Logging/Log.cs
+++ src/GitVersion.Core/Logging/Log.cs
@@ -9,7 +9,8 @@ internal sealed class Log(params ILogAppender[] appenders) : ILog
     private IEnumerable<ILogAppender> appenders = appenders;
     private readonly Regex obscurePasswordRegex = new("(https?://)(.+)(:.+@)", RegexOptions.Compiled);
     private readonly StringBuilder sb = new();
-    private string indent = string.Empty;
+    private string currentIndentation = string.Empty;
+    private const string Indentation = "  ";
 
     public Log() : this([])
     {
@@ -36,27 +37,30 @@ internal sealed class Log(params ILogAppender[] appenders) : ILog
 
     public IDisposable IndentLog(string operationDescription)
     {
-        var start = DateTime.Now;
+        var start = TimeProvider.System.GetTimestamp();
         Write(Verbosity.Normal, LogLevel.Info, $"-< Begin: {operationDescription} >-");
-        this.indent += "  ";
+        this.currentIndentation += Indentation;
 
         return Disposable.Create(() =>
         {
-            var length = this.indent.Length - 2;
-            this.indent = length > 0 ? this.indent[..length] : "";
-            Write(Verbosity.Normal, LogLevel.Info, string.Format(CultureInfo.InvariantCulture, "-< End: {0} (Took: {1:N}ms) >-", operationDescription, DateTime.Now.Subtract(start).TotalMilliseconds));
+            var length = this.currentIndentation.Length - Indentation.Length;
+            this.currentIndentation = length > 0 ? this.currentIndentation[..length] : "";
+            var end = TimeProvider.System.GetTimestamp();
+            var duration = TimeProvider.System.GetElapsedTime(start, end).TotalMilliseconds;
+            Write(Verbosity.Normal, LogLevel.Info, string.Format(CultureInfo.InvariantCulture, "-< End: {0} (Took: {1:N}ms) >-", operationDescription, duration));
         });
     }
 
     public void Separator() => Write(Verbosity.Normal, LogLevel.Info, "-------------------------------------------------------");
 
-    public void AddLogAppender(ILogAppender logAppender) => this.appenders = this.appenders.Concat(new[] { logAppender });
+    public void AddLogAppender(ILogAppender logAppender) => this.appenders = this.appenders.Concat([logAppender]);
 
     public override string ToString() => this.sb.ToString();
 
     private string FormatMessage(string message, string level)
     {
         var obscuredMessage = this.obscurePasswordRegex.Replace(message, "$1$2:*******@");
-        return string.Format(CultureInfo.InvariantCulture, "{0}{1} [{2:MM/dd/yy H:mm:ss:ff}] {3}", this.indent, level, DateTime.Now, obscuredMessage);
+        var timestamp = $"{DateTime.Now:yy-MM-dd H:mm:ss:ff}";
+        return string.Format(CultureInfo.InvariantCulture, "{0}{1} [{2}] {3}", this.currentIndentation, level, timestamp, obscuredMessage);
     }
 }
