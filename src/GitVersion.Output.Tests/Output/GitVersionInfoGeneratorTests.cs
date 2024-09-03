a25d30e66115caf4bb5d0013b19370b879c475ab
Alexander Rose <arose@haprotec.de>
2024-09-03 09:13:58 +0200

    create regression test

--- src/GitVersion.Output.Tests/Output/GitVersionInfoGeneratorTests.cs
+++ src/GitVersion.Output.Tests/Output/GitVersionInfoGeneratorTests.cs
@@ -49,4 +49,44 @@ public class GitVersionInfoGeneratorTests : TestBase
 
         DirectoryHelper.DeleteDirectory(directory);
     }
+
+    /// <summary>
+    /// Regression test for issue #4196 (https://github.com/GitTools/GitVersion/issues/4196)
+    /// </summary>
+    [TestCase]
+    public void ShouldProperlyOutputNamespaceDeclaration()
+    {
+        const string fileExtension = "cs";
+        const string targetNamespace = "My.Custom.Namespace";
+
+        var directory = PathHelper.Combine(PathHelper.GetTempPath(), "GitVersionInfoGeneratorTests", Guid.NewGuid().ToString());
+        if (!Directory.Exists(directory))
+            Directory.CreateDirectory(directory);
+        var fileName = "GitVersionInformation.g." + fileExtension;
+        var fullPath = PathHelper.Combine(directory, fileName);
+
+        var semanticVersion = new SemanticVersion
+        {
+            Major = 1,
+            Minor = 2,
+            Patch = 3,
+            PreReleaseTag = "unstable4",
+            BuildMetaData = new("versionSourceSha", 5,
+                "feature1", "commitSha", "commitShortSha", DateTimeOffset.Parse("2014-03-06 23:59:59Z"), 0)
+        };
+
+        var sp = ConfigureServices();
+
+        var fileSystem = sp.GetRequiredService<IFileSystem>();
+        var variableProvider = sp.GetRequiredService<IVariableProvider>();
+
+        var variables = variableProvider.GetVariablesFor(semanticVersion, EmptyConfigurationBuilder.New.Build(), 0);
+        using var generator = sp.GetRequiredService<IGitVersionInfoGenerator>();
+
+        generator.Execute(variables, new(directory, fileName, fileExtension, targetNamespace));
+
+        fileSystem.ReadAllText(fullPath).ShouldMatchApproved(c => c.SubFolder(PathHelper.Combine("Approved", fileExtension)));
+
+        DirectoryHelper.DeleteDirectory(directory);
+    }
 }
