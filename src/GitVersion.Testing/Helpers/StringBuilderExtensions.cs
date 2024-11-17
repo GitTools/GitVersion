7eb9399797a708948c65875aac5b4a2f61ea5c02
Artur Stolear <artur.stolear@gmail.com>
2024-11-17 13:49:46 +0100

    cleanup code targeting  < net8.0

--- src/GitVersion.Testing/Helpers/StringBuilderExtensions.cs
+++ src/GitVersion.Testing/Helpers/StringBuilderExtensions.cs
@@ -1,15 +1,11 @@
-#if NET7_0_OR_GREATER
 using System.Diagnostics.CodeAnalysis;
-#endif
 
 namespace GitVersion.Testing.Internal;
 
 internal static class StringBuilderExtensions
 {
     public static void AppendLineFormat(this StringBuilder stringBuilder,
-#if NET7_0_OR_GREATER
                                         [StringSyntax(StringSyntaxAttribute.CompositeFormat)]
-#endif
                                         string format,
                                         params object?[] args)
     {
