7e3e268917f64fac82bc93fc732714c1593c4346
Stefan Schick <s.schick@gebhardt-group.com>
2025-01-31 12:53:22 +0100

    Implement custom deserializer

--- src/GitVersion.Core/VersionCalculation/VersionStrategies.cs
+++ src/GitVersion.Core/VersionCalculation/VersionStrategies.cs
@@ -1,7 +1,6 @@
 namespace GitVersion.VersionCalculation;
 
 [Flags]
-[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum VersionStrategies
 {
     None = 0,
---
+++ src/GitVersion.Core/VersionCalculation/VersionStrategiesConverter.cs
@@ -0,0 +1,34 @@
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace GitVersion.VersionCalculation;
+
+public class VersionStrategiesConverter : IYamlTypeConverter
+{
+    public static readonly IYamlTypeConverter Instance = new VersionStrategiesConverter();
+
+    public bool Accepts(Type type)
+    {
+        return type == typeof(VersionStrategies[]);
+    }
+
+    public object? ReadYaml(IParser parser, Type type, ObjectDeserializer rootDeserializer)
+    {
+        string data = parser.Consume<Scalar>().Value;
+
+        var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(UnderscoredNamingConvention.Instance)  // see height_in_inches in sample yml 
+            .Build();
+
+        VersionStrategies[] strategies = deserializer.Deserialize<VersionStrategies[]>(data);
+
+        return strategies;
+    }
+
+    public void WriteYaml(IEmitter emitter, object? value, Type type, ObjectSerializer serializer)
+    {
+        // Convert from an object to text during serialization.
+    }
+}
