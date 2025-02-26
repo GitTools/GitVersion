7eabdfd199f12dec29006c0f2f0ae1f16ca237a5
Bi0T1N <Bi0T1N@users.noreply.github.com>
2025-02-26 22:13:28 +0100

    Add missing whitespace after foreach

--- src/GitVersion.Output/OutputGenerator/OutputGenerator.cs
+++ src/GitVersion.Output/OutputGenerator/OutputGenerator.cs
@@ -48,7 +48,7 @@ internal sealed class OutputGenerator(
                 dotEnvEntries.Add(prefixedKey + "=" + environmentValue);
             }
 
-            foreach(var dotEnvEntry in dotEnvEntries)
+            foreach (var dotEnvEntry in dotEnvEntries)
             {
                 this.console.WriteLine(dotEnvEntry);
             }
