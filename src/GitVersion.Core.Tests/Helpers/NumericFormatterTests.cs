16f557cb9aa8fd5385df4621aa4fa9af7ec544f2
9swampy <github@prims.com>
2025-06-27 09:06:43 +0100

    Support C# format strings in config

---
+++ src/GitVersion.Core.Tests/Helpers/NumericFormatterTests.cs
@@ -0,0 +1,38 @@
+﻿using GitVersion.Helpers;
+
+namespace GitVersion.Tests.Helpers;
+
+[TestFixture]
+public class NumericFormatterTests
+{
+    [Test]
+    public void Priority_ShouldBe1() => new NumericFormatter().Priority.ShouldBe(1);
+    [Test]
+    public void TryFormat_NullValue_ReturnsFalse()
+    {
+        var sut = new NumericFormatter();
+        var result = sut.TryFormat(null, "n", out var formatted);
+        result.ShouldBeFalse();
+        formatted.ShouldBeEmpty();
+    }
+
+    [TestCase("1234.5678", "n", "1,234.57")]
+    [TestCase("1234.5678", "f2", "1234.57")]
+    [TestCase("1234.5678", "f0", "1235")]
+    [TestCase("1234.5678", "g", "1234.5678")]
+    public void TryFormat_ValidFormats_ReturnsExpectedResult(string input, string format, string expected)
+    {
+        var sut = new NumericFormatter();
+        var result = sut.TryFormat(input, format, out var formatted);
+        result.ShouldBeTrue();
+        formatted.ShouldBe(expected);
+    }
+    [Test]
+    public void TryFormat_UnsupportedFormat_ReturnsFalse()
+    {
+        var sut = new NumericFormatter();
+        var result = sut.TryFormat(1234.5678, "z", out var formatted);
+        result.ShouldBeFalse();
+        formatted.ShouldBeEmpty();
+    }
+}
