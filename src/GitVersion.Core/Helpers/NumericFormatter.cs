9a186341ee28104b651c9d3626b3878a02f38515
9swampy <github@prims.com>
2025-06-27 09:06:43 +0100

    Support C# format strings in config

---
+++ src/GitVersion.Core/Helpers/NumericFormatter.cs
@@ -0,0 +1,48 @@
+using System.Globalization;
+
+namespace GitVersion.Helpers;
+
+internal class NumericFormatter : IValueFormatter
+{
+    public int Priority => 1;
+
+    public bool TryFormat(object? value, string format, out string result)
+    {
+        result = string.Empty;
+
+        if (value is not string s)
+        {
+            return false;
+        }
+
+        // Integer formatting
+        if (format.All(char.IsDigit) && int.TryParse(s, out var i))
+        {
+            result = i.ToString(format, CultureInfo.InvariantCulture);
+            return true;
+        }
+
+        // Hexadecimal formatting
+        if ((format.StartsWith('X') || format.StartsWith('x')) && int.TryParse(s, out var hex))
+        {
+            result = hex.ToString(format, CultureInfo.InvariantCulture);
+            return true;
+        }
+
+        // Floating point formatting
+        if ("FEGNCP".Contains(char.ToUpperInvariant(format[0])) && double.TryParse(s, out var d))
+        {
+            result = d.ToString(format, CultureInfo.InvariantCulture);
+            return true;
+        }
+
+        // Decimal formatting
+        if (decimal.TryParse(s, NumberStyles.Any, CultureInfo.InvariantCulture, out var dec))
+        {
+            result = dec.ToString(format, CultureInfo.InvariantCulture);
+            return true;
+        }
+
+        return false;
+    }
+}
