6f3f5fd28c0f538b02eb67a953c60b6c14167869
Artur Stolear <artur.stolear@gmail.com>
2025-08-29 11:11:51 +0200

    refactor: Enhance numeric parsing in NumericFormatter

--- src/GitVersion.Core/Formatting/NumericFormatter.cs
+++ src/GitVersion.Core/Formatting/NumericFormatter.cs
@@ -14,21 +14,21 @@ internal class NumericFormatter : InvariantFormatter, IValueFormatter
             return false;
 
         // Integer formatting
-        if (format.All(char.IsDigit) && int.TryParse(s, out var i))
+        if (format.All(char.IsDigit) && int.TryParse(s, NumberStyles.Integer, cultureInfo, out var i))
         {
             result = i.ToString(format, cultureInfo);
             return true;
         }
 
         // Hexadecimal formatting
-        if (format.StartsWith("X", StringComparison.OrdinalIgnoreCase) && int.TryParse(s, out var hex))
+        if (format.StartsWith("X", StringComparison.OrdinalIgnoreCase) && int.TryParse(s, NumberStyles.Integer, cultureInfo, out var hex))
         {
             result = hex.ToString(format, cultureInfo);
             return true;
         }
 
         // Floating point formatting
-        if ("FEGNCP".Contains(char.ToUpperInvariant(format[0])) && double.TryParse(s, out var d))
+        if ("FEGNCP".Contains(char.ToUpperInvariant(format[0])) && double.TryParse(s, NumberStyles.Float | NumberStyles.AllowThousands, cultureInfo, out var d))
         {
             result = d.ToString(format, cultureInfo);
             return true;
