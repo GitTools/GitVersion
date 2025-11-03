dd48544a1f11d680ea31fa6a4623ca9b5ed3457a
Artur Stolear <artur.stolear@gmail.com>
2025-11-03 14:38:16 +0100

    Refactors SequenceDiagram and related methods
    Replaces direct field access with a public property `DiagramBuilder` in `SequenceDiagram`. Updates related methods to use this property for better encapsulation and maintainability.

--- src/GitVersion.Core.Tests/IntegrationTests/RepositoryFixtureExtensions.cs
+++ src/GitVersion.Core.Tests/IntegrationTests/RepositoryFixtureExtensions.cs
@@ -7,33 +7,15 @@ internal static class RepositoryFixtureExtensions
     public static void MakeACommit(this RepositoryFixtureBase fixture, string commitMsg)
     {
         fixture.Repository.MakeACommit(commitMsg);
-        var diagramBuilder = (StringBuilder?)typeof(SequenceDiagram)
-            .GetField("diagramBuilder", BindingFlags.Instance | BindingFlags.NonPublic)
-            ?.GetValue(fixture.SequenceDiagram);
 
-        var participant = GetParticipant(fixture.Repository.Head.FriendlyName);
-        if (participant != null)
-        {
-            AddTheCommitMessage(fixture, commitMsg, diagramBuilder, participant);
-        }
-
-        string? GetParticipant(string participantName) =>
-            (string?)typeof(SequenceDiagram).GetMethod("GetParticipant", BindingFlags.Instance | BindingFlags.NonPublic)
-                ?.Invoke(fixture.SequenceDiagram,
-                [
-                    participantName
-                ]);
-    }
-
-    private static void AddTheCommitMessage(RepositoryFixtureBase fixture, string commitMsg, StringBuilder? diagramBuilder, string participant)
-    {
+        var participant = fixture.SequenceDiagram.GetParticipant(fixture.Repository.Head.FriendlyName);
         if (commitMsg.Length < 40)
         {
-            diagramBuilder?.AppendLineFormat("{0} -> {0}: Commit '{1}'", participant, commitMsg);
+            fixture.SequenceDiagram.DiagramBuilder.AppendLineFormat("{0} -> {0}: Commit '{1}'", participant, commitMsg);
         }
         else
         {
-            var formattedCommitMsg = string.Join(System.Environment.NewLine, $"Commit '{commitMsg}'".SplitIntoLines(60));
+            var formattedCommitMsg = string.Join(SysEnv.NewLine, $"Commit '{commitMsg}'".SplitIntoLines(60));
             fixture.SequenceDiagram.NoteOver(formattedCommitMsg, participant);
         }
     }
--- src/GitVersion.Testing/Fixtures/SequenceDiagram.cs
+++ src/GitVersion.Testing/Fixtures/SequenceDiagram.cs
@@ -9,31 +9,32 @@ namespace GitVersion.Testing;
 public class SequenceDiagram
 {
     private readonly Dictionary<string, string> participants = [];
-    private readonly StringBuilder diagramBuilder;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="T:SequenceDiagram"/> class.
     /// </summary>
     public SequenceDiagram()
     {
-        this.diagramBuilder = new StringBuilder();
-        this.diagramBuilder.AppendLine("@startuml");
+        this.DiagramBuilder = new StringBuilder();
+        this.DiagramBuilder.AppendLine("@startuml");
     }
+
+    public StringBuilder DiagramBuilder { get; }
 
     /// <summary>
     /// Activates a branch/participant in the sequence diagram
     /// </summary>
-    public void Activate(string branch) => this.diagramBuilder.AppendLineFormat("activate {0}", GetParticipant(branch));
+    public void Activate(string branch) => this.DiagramBuilder.AppendLineFormat("activate {0}", GetParticipant(branch));
 
     /// <summary>
     /// Deactivates a branch/participant in the sequence diagram
     /// </summary>
-    public void Deactivate(string branch) => this.diagramBuilder.AppendLineFormat("deactivate {0}", GetParticipant(branch));
+    public void Deactivate(string branch) => this.DiagramBuilder.AppendLineFormat("deactivate {0}", GetParticipant(branch));
 
     /// <summary>
     /// Destroys a branch/participant in the sequence diagram
     /// </summary>
-    public void Destroy(string branch) => this.diagramBuilder.AppendLineFormat("destroy {0}", GetParticipant(branch));
+    public void Destroy(string branch) => this.DiagramBuilder.AppendLineFormat("destroy {0}", GetParticipant(branch));
 
     /// <summary>
     /// Creates a participant in the sequence diagram
@@ -43,21 +44,21 @@ public class SequenceDiagram
         var cleanParticipant = ParticipantSanitizer.SanitizeParticipant(@as ?? participant);
         this.participants.Add(participant, cleanParticipant);
         if (participant == cleanParticipant)
-            this.diagramBuilder.AppendLineFormat("participant {0}", participant);
+            this.DiagramBuilder.AppendLineFormat("participant {0}", participant);
         else
-            this.diagramBuilder.AppendLineFormat("participant \"{0}\" as {1}", participant, cleanParticipant);
+            this.DiagramBuilder.AppendLineFormat("participant \"{0}\" as {1}", participant, cleanParticipant);
     }
 
     /// <summary>
     /// Appends a divider with specified text to the sequence diagram
     /// </summary>
-    public void Divider(string text) => this.diagramBuilder.AppendLineFormat("== {0} ==", text);
+    public void Divider(string text) => this.DiagramBuilder.AppendLineFormat("== {0} ==", text);
 
     /// <summary>
     /// Appends a note over one or many participants to the sequence diagram
     /// </summary>
     public void NoteOver(string noteText, string startParticipant, string? endParticipant = null, string? prefix = null, string? color = null) =>
-        this.diagramBuilder.AppendLineFormat(
+        this.DiagramBuilder.AppendLineFormat(
             prefix + """
                      note over {0}{1}{2}
                        {3}
@@ -71,7 +72,7 @@ public class SequenceDiagram
     /// <summary>
     /// Appends applying a tag to the specified branch/participant to the sequence diagram
     /// </summary>
-    public void ApplyTag(string tag, string toBranch) => this.diagramBuilder.AppendLineFormat("{0} -> {0}: tag {1}", GetParticipant(toBranch), tag);
+    public void ApplyTag(string tag, string toBranch) => this.DiagramBuilder.AppendLineFormat("{0} -> {0}: tag {1}", GetParticipant(toBranch), tag);
 
     /// <summary>
     /// Appends branching from a branch to another branch, @as can override the participant name
@@ -80,11 +81,11 @@ public class SequenceDiagram
     {
         if (!this.participants.ContainsKey(branchName))
         {
-            this.diagramBuilder.Append("create ");
+            this.DiagramBuilder.Append("create ");
             Participant(branchName, @as);
         }
 
-        this.diagramBuilder.AppendLineFormat(
+        this.DiagramBuilder.AppendLineFormat(
             "{0} -> {1}: branch from {2}",
             GetParticipant(currentName),
             GetParticipant(branchName), currentName);
@@ -97,32 +98,32 @@ public class SequenceDiagram
     {
         if (!this.participants.ContainsKey(branchName))
         {
-            this.diagramBuilder.Append("create ");
+            this.DiagramBuilder.Append("create ");
             Participant(branchName, @as);
         }
 
-        this.diagramBuilder.AppendLineFormat("{0} -> {1}: branch from tag ({2})", GetParticipant(onBranch), GetParticipant(branchName), fromTag);
+        this.DiagramBuilder.AppendLineFormat("{0} -> {1}: branch from tag ({2})", GetParticipant(onBranch), GetParticipant(branchName), fromTag);
     }
 
     /// <summary>
     /// Appends a commit on the target participant/branch to the sequence diagram
     /// </summary>
-    public void MakeACommit(string toParticipant) => this.diagramBuilder.AppendLineFormat("{0} -> {0}: commit", GetParticipant(toParticipant));
+    public void MakeACommit(string toParticipant) => this.DiagramBuilder.AppendLineFormat("{0} -> {0}: commit", GetParticipant(toParticipant));
 
     /// <summary>
     /// Append a merge to the sequence diagram
     /// </summary>
-    public void Merge(string from, string to) => this.diagramBuilder.AppendLineFormat("{0} -> {1}: merge", GetParticipant(from), GetParticipant(to));
+    public void Merge(string from, string to) => this.DiagramBuilder.AppendLineFormat("{0} -> {1}: merge", GetParticipant(from), GetParticipant(to));
 
-    private string GetParticipant(string branch) => this.participants.GetValueOrDefault(branch, branch);
+    public string GetParticipant(string branch) => this.participants.GetValueOrDefault(branch, branch);
 
     /// <summary>
     /// Ends the sequence diagram
     /// </summary>
-    public void End() => this.diagramBuilder.AppendLine("@enduml");
+    public void End() => this.DiagramBuilder.AppendLine("@enduml");
 
     /// <summary>
     /// returns the plantUML representation of the Sequence Diagram
     /// </summary>
-    public string GetDiagram() => this.diagramBuilder.ToString();
+    public string GetDiagram() => this.DiagramBuilder.ToString();
 }
