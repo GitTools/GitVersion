86c66453aac6dd520e92f5cff290e92594bf3940
Artur Stolear <artur.stolear@gmail.com>
2025-11-18 00:39:12 +0100

    enable testing with Full Framework
    This enabled testing the GitVersion.MsBuild with the full .NET Framework's version of msbuild

--- build/artifacts/Tasks/ArtifactsMsBuildFullTest.cs
+++ build/artifacts/Tasks/ArtifactsMsBuildFullTest.cs
@@ -19,8 +19,12 @@ public class ArtifactsMsBuildFullTest : FrostingTask<BuildContext>
         if (context.Version == null)
             return;
         var version = context.Version.NugetVersion;
+        var fullSemVer = context.Version.GitVersion.FullSemVer;
 
         var nugetSource = context.MakeAbsolute(Paths.Nuget).FullPath;
+
+        const int toolVersionValue = 11; // Workaround for now. It should be removed when https://github.com/cake-build/cake/issues/4658 is merged
+        var isMsBuildToolVersionValid = Enum.IsDefined(typeof(MSBuildToolVersion), toolVersionValue);
 
         context.Information("\nTesting msbuild task with dotnet build\n");
         foreach (var netVersion in Constants.DotnetVersions)
@@ -40,7 +44,27 @@ public class ArtifactsMsBuildFullTest : FrostingTask<BuildContext>
             });
 
             var exe = Paths.Integration.Combine("build").Combine(framework).CombineWithFilePath("app.dll");
-            context.ValidateOutput("dotnet", exe.FullPath, context.Version.GitVersion.FullSemVer);
+            context.ValidateOutput("dotnet", exe.FullPath, fullSemVer);
+
+            if (!isMsBuildToolVersionValid) continue;
+
+            const MSBuildToolVersion toolVersion = (MSBuildToolVersion)toolVersionValue;
+            context.Information("\nTesting msbuild task with msbuild (for full framework)\n");
+
+            var msBuildSettings = new MSBuildSettings
+            {
+                Verbosity = Verbosity.Minimal,
+                ToolVersion = toolVersion,
+                Restore = true
+            };
+
+            msBuildSettings.WithProperty("GitVersionMsBuildVersion", version);
+            msBuildSettings.WithProperty("RestoreSource", nugetSource);
+
+            context.MSBuild(projPath.FullPath, msBuildSettings);
+
+            var fullExe = Paths.Integration.Combine("build").CombineWithFilePath("app.exe");
+            context.ValidateOutput(fullExe.FullPath, null, fullSemVer);
         }
     }
 }
