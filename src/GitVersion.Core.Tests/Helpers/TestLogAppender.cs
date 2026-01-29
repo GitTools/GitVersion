7eea248def88b48d04b8a051dbad54de999ee729
Artur Stolear <artur.stolear@gmail.com>
2026-01-29 02:13:58 +0100

    test(core): update core tests for ILogger<T> migration
    Update test infrastructure:
    - GitVersionCoreTestModule uses TestLoggerFactory
    - TestConsoleAdapter moved to GitVersion namespace
    - Remove TestLogAppender (replaced by TestLogger)
    - Remove LoggerTest (legacy ILog tests)
    - Update GitVersionExecutorTests and RepositoryStoreTests

--- src/GitVersion.Core.Tests/Helpers/TestLogAppender.cs
+++
@@ -1,8 +0,0 @@
-using GitVersion.Logging;
-
-namespace GitVersion.Core.Tests.Helpers;
-
-public class TestLogAppender(Action<string> logAction) : ILogAppender
-{
-    public void WriteTo(LogLevel level, string message) => logAction(message);
-}
