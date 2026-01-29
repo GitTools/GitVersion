0764d25226a2ccb66c057777ef93252ab10beb29
Artur Stolear <artur.stolear@gmail.com>
2026-01-29 02:12:01 +0100

    refactor(core): remove legacy ILog logging infrastructure
    Remove obsolete logging types replaced by Microsoft.Extensions.Logging:
    - ILog, ILogAppender interfaces
    - Log, NullLog, ConsoleAppender implementations
    - LogLevel enum (replaced by Microsoft.Extensions.Logging.LogLevel)
    - LogAction delegate and LogExtensions
    - Old IConsole and ConsoleAdapter (moved to Console folder)

--- src/GitVersion.Core/Logging/LogAction.cs
+++
@@ -1,7 +0,0 @@
-namespace GitVersion.Logging;
-
-/// <summary>A delegate that receives a <see cref="LogActionEntry"/> callback used to lazily compose a log message.</summary>
-public delegate void LogAction(LogActionEntry actionEntry);
-
-/// <summary>A delegate that formats and writes a log message using the supplied format string and arguments.</summary>
-public delegate void LogActionEntry(string format, params object[] args);
