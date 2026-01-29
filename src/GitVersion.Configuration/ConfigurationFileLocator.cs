cc096a155609a2791e36a7c46acf4f523987920a
Artur Stolear <artur.stolear@gmail.com>
2026-01-29 02:13:28 +0100

    refactor(configuration): migrate from ILog to ILogger<T>
    Update ConfigurationFileLocator and ConfigurationProvider to use
    ILogger<T> instead of the legacy ILog interface.

--- src/GitVersion.Configuration/ConfigurationFileLocator.cs
+++ src/GitVersion.Configuration/ConfigurationFileLocator.cs
@@ -1,13 +1,12 @@
 using System.IO.Abstractions;
 using GitVersion.Extensions;
 using GitVersion.Helpers;
-using GitVersion.Logging;
 
 namespace GitVersion.Configuration;
 
 internal class ConfigurationFileLocator(
     IFileSystem fileSystem,
-    ILog log,
+    ILogger<ConfigurationFileLocator> logger,
     IOptions<GitVersionOptions> options)
     : IConfigurationFileLocator
 {
@@ -25,7 +24,7 @@ internal class ConfigurationFileLocator(
     ];
 
     private readonly IFileSystem fileSystem = fileSystem.NotNull();
-    private readonly ILog log = log.NotNull();
+    private readonly ILogger<ConfigurationFileLocator> logger = logger.NotNull();
     private readonly IOptions<GitVersionOptions> options = options.NotNull();
 
     private string? ConfigurationFile => this.options.Value.ConfigurationInfo.ConfigurationFile;
@@ -50,7 +49,7 @@ internal class ConfigurationFileLocator(
         var customConfigurationFile = GetCustomConfigurationFilePathIfEligable(directoryPath);
         if (!string.IsNullOrWhiteSpace(customConfigurationFile))
         {
-            this.log.Info($"Found configuration file at '{customConfigurationFile}'");
+            this.logger.LogInformation($"Found configuration file at '{customConfigurationFile}'");
             return customConfigurationFile;
         }
 
@@ -62,14 +61,14 @@ internal class ConfigurationFileLocator(
         var files = this.fileSystem.Directory.GetFiles(directoryPath);
         foreach (var fileName in this.SupportedConfigFileNames)
         {
-            this.log.Debug($"Trying to find configuration file {fileName} at '{directoryPath}'");
+            this.logger.LogDebug($"Trying to find configuration file {fileName} at '{directoryPath}'");
             var matchingFile = files.FirstOrDefault(file => string.Equals(FileSystemHelper.Path.GetFileName(file), fileName, StringComparison.OrdinalIgnoreCase));
-            if (matchingFile == null)
+if (matchingFile == null)
             {
                 continue;
             }
 
-            this.log.Info($"Found configuration file at '{matchingFile}'");
+            this.logger.LogInformation($"Found configuration file at '{matchingFile}'");
             return matchingFile;
         }
 
