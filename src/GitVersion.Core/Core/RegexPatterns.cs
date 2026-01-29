26e34daa1563cfb7e69d300cb0ea56f00f52130a
Artur Stolear <artur.stolear@gmail.com>
2026-01-29 13:13:51 +0100

    refactor(logging): Optimize password obscuring regex
    Moves the password obscuring regex to a static, pre-compiled instance for improved performance and clarifies its usage. Suppresses a false positive SonarQube warning for the masked password constant.

--- src/GitVersion.Core/Core/RegexPatterns.cs
+++ src/GitVersion.Core/Core/RegexPatterns.cs
@@ -14,7 +14,7 @@ internal static partial class RegexPatterns
     private const string SwitchArgumentRegexPattern = @"/\w+:";
 
     [StringSyntax(StringSyntaxAttribute.Regex)]
-    public const string ObscurePasswordRegexPattern = "(https?://)(.+)(:.+@)";
+    private const string ObscurePasswordRegexPattern = "(https?://)(.+)(:.+@)";
 
     [StringSyntax(StringSyntaxAttribute.Regex)]
     private const string ExpandTokensRegexPattern =
