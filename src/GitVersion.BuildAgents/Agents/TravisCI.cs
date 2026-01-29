ee312e8200846917c5869850d8ec6a004b24d29b
Artur Stolear <artur.stolear@gmail.com>
2026-01-29 02:13:21 +0100

    refactor(agents): migrate build agents from ILog to ILogger<T>
    Update all build agent implementations to use ILogger<T>:
    - AppVeyor, AzurePipelines, BitBucketPipelines, BuildKite
    - CodeBuild, ContinuaCi, Drone, EnvRun
    - GitHubActions, GitLabCi, Jenkins, MyGet
    - SpaceAutomation, TeamCity, TravisCI

--- src/GitVersion.BuildAgents/Agents/TravisCI.cs
+++ src/GitVersion.BuildAgents/Agents/TravisCI.cs
@@ -1,10 +1,9 @@
 using System.IO.Abstractions;
-using GitVersion.Logging;
 using GitVersion.OutputVariables;
 
 namespace GitVersion.Agents;
 
-internal class TravisCi(IEnvironment environment, ILog log, IFileSystem fileSystem) : BuildAgentBase(environment, log, fileSystem)
+internal class TravisCi(IEnvironment environment, ILogger<TravisCi> logger, IFileSystem fileSystem) : BuildAgentBase(environment, logger, fileSystem)
 {
     public const string EnvironmentVariableName = "TRAVIS";
     protected override string EnvironmentVariable => EnvironmentVariableName;
