44f60b5c6139ded6347eef6ac437fee8b9aaad6a
Artur Stolear <artur.stolear@gmail.com>
2026-03-18 16:35:55 +0100

    build: Enhance mark-shipped script for public API changes

--- src/mark-shipped.ps1
+++ src/mark-shipped.ps1
@@ -7,36 +7,44 @@ Set-StrictMode -version 2.0
 $ErrorActionPreference = "Stop"
 
 function MarkShipped([string]$dir) {
+    $nullableHeader = "#nullable enable";
     $shippedFilePath = Join-Path $dir "PublicAPI.Shipped.txt"
     $shipped = @()
     $shipped += Get-Content $shippedFilePath
 
     $unshippedFilePath = Join-Path $dir "PublicAPI.Unshipped.txt"
     $unshipped = Get-Content $unshippedFilePath
+    $added = @()
     $removed = @()
     $removedPrefix = "*REMOVED*";
-    Write-Host "Processing $dir"
 
     foreach ($item in $unshipped) {
-        if ($item.Length -gt 0) {
+        if ($item.Length -gt 0 -and $item -ne $nullableHeader) {
             if ( $item.StartsWith($removedPrefix)) {
                 $item = $item.Substring($removedPrefix.Length)
                 $removed += $item
             }
             else {
                 $shipped += $item
+                $added += $item
             }
         }
     }
 
+    $changeCount = $added.Count + $removed.Count
+    Write-Host ("{0,-6} Processed {1}" -f "[$changeCount]", $unshippedFilePath)
+
     $shipped | Sort-Object -Unique | Where-Object { -not $removed.Contains($_) } | Out-File $shippedFilePath -Encoding Ascii
-    "#nullable enable" | Out-File $unshippedFilePath -Encoding Ascii
+    $nullableHeader | Out-File $unshippedFilePath -Encoding Ascii
 }
 
 try {
     foreach ($file in Get-ChildItem -re -in "PublicApi.Shipped.txt") {
         $dir = Split-Path -parent $file
         MarkShipped $dir
+    }
+    if ($null -ne (git status --porcelain)) {
+        Write-Host "Changes detected, committing and pushing changes"
     }
 }
 catch {
