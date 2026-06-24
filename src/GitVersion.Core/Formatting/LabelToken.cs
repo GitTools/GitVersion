b00ef16cb80ae055ba2031e8a1d31e2ef8ec620f
Robert Coltheart <robert.coltheart@gmail.com>
2026-06-25 07:58:11 +1000

    fix: change label token to be record

--- src/GitVersion.Core/Formatting/LabelToken.cs
+++ src/GitVersion.Core/Formatting/LabelToken.cs
@@ -1,17 +1,3 @@
 namespace GitVersion.Formatting;
 
-internal class LabelToken
-{
-    public LabelToken(string name, LabelTokenType type, string? format = null)
-    {
-        Name = name;
-        Type = type;
-        Format = format;
-    }
-
-    public string Name { get; }
-
-    public LabelTokenType Type { get; }
-
-    public string? Format { get; }
-}
+internal record LabelToken(string Name, LabelTokenType Type, string? Format = null);
